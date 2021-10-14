5b7fa9d10391f51b247e1a6287cdf33d01d8ea5c
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 10:08:49 +0200

    Unit 02

---
+++ Unit_02/Ex2.java
@@ -0,0 +1,17 @@
+/*
+ * @author David Antúnez Pérez 1º DAM
+ * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ * pantalla de tal forma que el resultado del programa sea el mismo que en el
+ * ejercicio 1 del capítulo 1.
+ * 
+ */
+
+
+public class Ex2 {
+  
+  public static void main (String[] args) {
+    String name = "David Antúnez Pérez";
+    System.out.println(name);
+  }
+}
+
