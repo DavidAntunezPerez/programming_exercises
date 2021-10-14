a96cab66607925c1a71316166217846a2664724b
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 10:42:46 +0200

    Unit 02: Variables, Operations

---
+++ Unit_02/Ex7.java
@@ -0,0 +1,19 @@
+/**
+ *@author David Antúnez Pérez
+ * 
+ * Escribe un programa que declare variables de tipo char y de tipo String. Intenta
+ * mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia
+ * de Java (con un solo println) ¿es posible?
+ * 
+ */
+
+
+public class Ex7 {
+  
+  public static void main (String[] args) {
+    char number = '1';
+    String yes = "yes";
+    System.out.println(  yes + number );
+  }
+}
+
