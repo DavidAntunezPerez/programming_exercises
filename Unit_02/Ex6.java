1409387ed6f723cf8dc228f35668c090ee1ce05b
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 10:35:42 +0200

    Unit 02: Variables, Objects

---
+++ Unit_02/Ex6.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author David Antúnez Pérez
+ * Escribe un programa que calcule el total de una factura a partir de la base
+ * imponible (precio sin IVA). La base imponible estará almacenada en una
+ * variable.
+ * 
+ */
+
+
+public class Ex6 {
+  
+  public static void main (String[] args) {
+    double sinIva = 20d;
+    double conIva = sinIva * 1.21;
+    System.out.println("El precio sin IVA es: " + sinIva);
+    System.out.println("El precio con IVA es: " + conIva);
+	}
+}
+
