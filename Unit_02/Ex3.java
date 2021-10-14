394ca805a502fb39a3e90e46ca063efb5ba8609c
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 10:15:19 +0200

    Unit 02: Variables, Operations

---
+++ Unit_02/Ex3.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author David Antúnez Pérez
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. 
+ * Muestra los valores de esas variables por pantalla de tal forma que el
+ * resultado del programa sea el mismo que en el ejercicio 2.
+ * 
+ */
+
+
+public class Ex3 {
+  
+  public static void main (String[] args) {
+    String name = "David Antúnez Pérez";
+    String direction = "Calle Real, 1";
+    long telephone = 600122044l;
+    System.out.println( name + ", " + direction + ", " + telephone);
+  }
+}
+
