0eca9fc776796adcdbef1580af645a4bfe777b72
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 09:05:51 +0200

    started Unit2:Variables.

---
+++ Unit_02/TextChains.java
@@ -0,0 +1,33 @@
+/*
+ * CadenasDeTexto.java
+ * 
+ * Copyright 2021 an2 <an2@an2-VirtualBox>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class TextChains {
+  
+  public static void main (String[] args) {
+    String timeToday = "cloudy";
+    
+    System.out.println("It's " + timeToday);
+  }
+}
+
