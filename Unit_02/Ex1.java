58dfa1b50be650234728842218a379303972f784
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 10:04:17 +0200

    Unit02: Variables, Operations

---
+++ Unit_02/Ex1.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021 
+ * @author David Antúnez Pérez 1ºDAM
+ * Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+el valor de cada variable, la suma, la resta, la división y la multiplicación.
+
+ */
+
+
+public class Ex1 {
+  
+	public static void main (String[] args) {
+    int x = 144;
+    int y = 999;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(x + y);
+    System.out.println(x - y);
+    System.out.println(x * y);
+    System.out.println(x / y);
+  }
+}
+
