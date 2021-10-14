a024066dab4c160c0b9a486d785db2586e03aa10
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-14 10:24:20 +0200

    Unit 02: Variables, Operations

---
+++ Unit_02/Ex4.java
@@ -0,0 +1,17 @@
+/**
+ *@author David Antúnez Pérez
+ * Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
+ * convertir deberá estar almacenada en una variable.
+ * 
+ */
+
+
+public class Ex4 {
+  
+  public static void main (String[] args) {
+    double numEuros = 1;
+    double numPesetas = numEuros * 166.386;
+    System.out.println( numEuros + " euros son " + numPesetas + " pesetas" );
+  }
+}
+
