f9a184e096b38c152f22abe224940fb1858dad83
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-18 18:56:20 +0200

    Unit03: Datalecture from keyboard

---
+++ Unit_03/Ex3.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author David Antúnez Pérez
+ * Conversor Pesetas a Euros
+ * 
+ */
+
+
+public class Ex3 {
+  
+  public static void main (String[] args) {
+    String line;
+    System.out.println("Selecciona la cantidad de pesetas a convertir: ");
+    line = System.console().readLine();
+    int euros;
+    euros = Integer.parseInt(line);
+    double total; 
+    total = euros * 0.006;
+    System.out.println(euros + " pesetas equivalen a " + total + " € .");
+    
+  }
+}
