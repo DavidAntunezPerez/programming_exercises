d70b5d21150240dfbfcab55c68eea7b5a23c3e16
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-18 19:18:48 +0200

    Unit03: Datalecture from keyboard

---
+++ Unit_03/Ex5.java
@@ -0,0 +1,26 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ * 
+ */
+
+
+public class Ex5 {
+  
+  public static void main (String[] args) {
+     String line;
+     System.out.println("Triangle height: ");
+     line = System.console().readLine();
+     double number1;
+     number1 = Integer.parseInt(line);
+     System.out.println("Triangle base: ");
+     line = System.console().readLine();
+     double number2;
+     number2 = Integer.parseInt(line);
+     System.out.println( " Triangle height is: " + number1 );
+     System.out.println( " Triangle base is: " + number2);
+     double result = (number1 * number2) / 2;
+     System.out.println( " The area of the triangle is: " + result);
+  }
+}
+
