0fdefc3fd0908efb3d5bb369d4aa7369829d49c2
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-18 10:27:18 +0200

    Unit03: Datalecture from keyboard

---
+++ Unit_03/Ex2.java
@@ -0,0 +1,23 @@
+/**
+ *Realiza un conversor de euros a pesetas. La cantidad de euros que se quiere
+convertir debe ser introducida por teclado.
+ * @author David Antúnez Pérez
+ * 
+ */
+
+
+public class Ex2 {
+  
+  public static void main (String[] args) {
+    String line;
+    System.out.println("Selecciona la cantidad de euros a convertir: ");
+    line = System.console().readLine();
+    int euros;
+    euros = Integer.parseInt(line);
+    double total; 
+    total = euros * 166.386;
+    System.out.println(euros + "€ equivale a " + total + " pesetas.");
+    
+  }
+}
+
