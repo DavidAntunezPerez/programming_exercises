63afc8a27729c26c9d5488935a87eaf14bbc108f
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-18 10:06:02 +0200

    Unit03 Try

---
+++ Unit_03/Unit3Try2.java
@@ -0,0 +1,20 @@
+public class Unit3Try2
+ {
+  
+  public static void main (String[] args) {
+     String line;
+     System.out.println("Please, put a number: ");
+     line = System.console().readLine();
+     int number1;
+     number1 = Integer.parseInt(line);
+     System.out.println("Put another number, please: ");
+     line = System.console().readLine();
+     int number2;
+     number2 = Integer.parseInt(line);
+     int total;
+     total = (2 * number1) + number2;
+     System.out.print( " First number is: " + number1 );
+     System.out.print( " Second number is: " + number2 );
+     
+  }
+}
