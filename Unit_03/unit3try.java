aabd36f1b77f6ba215182a36043a2a90b91918f4
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-18 09:56:30 +0200

    Unit03 Try

---
+++ Unit_03/unit3try.java
@@ -0,0 +1,9 @@
+
+public class unit3try {
+  
+  public static void main (String[] args) {
+    System.out.println("Introduzca el nombre de la fruta");
+    String fruta = System.console().readLine();
+  }
+}
+
