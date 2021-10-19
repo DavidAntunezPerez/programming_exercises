9d3cdb697440d5e00946af0af50fed7cfb6cf916
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-19 09:49:40 +0200

    Unit 03: Datalecture from keyboard

--- Unit_03/Ex10.java
+++ Unit_03/Ex10.java
@@ -8,13 +8,13 @@ public class Ex10 {
   
   public static void main (String[] args) {
     String line;
-    System.out.print(" Number of Kb: ");
+    System.out.print(" Number of MB: ");
     line = System.console().readLine();
     double number1;
     number1 = Integer.parseInt(line);
-    System.out.println( " Selected value: " + number1 + "kB");
-    double result = number1 / 1000;
-    System.out.println( " Value converted: " + result + "MB");
+    System.out.println( " Selected value: " + number1 + "MB");
+    double result = number1 * 1000;
+    System.out.println( " Value converted: " + result + "kB");
   }
 }
 
