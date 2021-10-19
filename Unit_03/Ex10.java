428bd5bf5a227d542651461c04a073f2eb58aa9a
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-19 09:45:30 +0200

    Unit 03: Datalecture from keyboard

---
+++ Unit_03/Ex10.java
@@ -0,0 +1,20 @@
+/**
+ * @author David Antúnez Pérez
+ * Realiza un conversor de Mb a Kb
+ */
+
+
+public class Ex10 {
+  
+  public static void main (String[] args) {
+    String line;
+    System.out.print(" Number of Kb: ");
+    line = System.console().readLine();
+    double number1;
+    number1 = Integer.parseInt(line);
+    System.out.println( " Selected value: " + number1 + "kB");
+    double result = number1 / 1000;
+    System.out.println( " Value converted " + result + "MB");
+  }
+}
+
