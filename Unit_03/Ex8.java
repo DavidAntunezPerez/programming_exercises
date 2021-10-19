d1b9129a9616594febab000f78a055c3cca3f672
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-19 09:25:26 +0200

    Unit 03: Datalecture from keyboard

---
+++ Unit_03/Ex8.java
@@ -0,0 +1,21 @@
+/**
+ *@author David Antúnez Pérez
+ * Escribe un programa que calcule el salario semanal de un empleado en base a
+ * las horas trabajadas, a razón de 12 euros la hora
+ */
+
+
+public class Ex8 {
+  
+  public static void main (String[] args) {
+    String line;
+     System.out.print(" Number of hours of work: ");
+     line = System.console().readLine();
+     double hoursWork;
+     hoursWork = Integer.parseInt(line);
+     double salary = hoursWork * 12;
+     System.out.print(" The salary obtained is: " + salary);
+    
+  }
+}
+
