a348cbb56004607eaeb64687c7a11314401125a1
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-21 09:18:27 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Others/BooleanUse.java
@@ -0,0 +1,14 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ */
+
+
+public class BooleanUse {
+  
+  public static void main (String[] args) {
+    boolean x = 5 > 7;
+    System.out.println(x);
+  }
+}
+
