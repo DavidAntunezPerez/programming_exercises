bfa2922cf0963df5dd4f11b77c79ec9e60cf3a7a
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-21 09:27:09 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Others/IfUse.java
@@ -0,0 +1,21 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ * 
+ */
+
+
+public class IfUse {
+  
+  public static void main (String[] args) {
+    String fruit = "Orange";
+    
+    if ("Orange".equals(fruit)){
+      System.out.println("Orange");
+      } 
+      else {
+        System.out.println("Not Orange");
+        }
+  }
+}
+
