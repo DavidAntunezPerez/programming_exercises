19fa91065bec443328b57d8e0a8a1729b282d7a0
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-21 11:16:03 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Ex2.java
@@ -0,0 +1,16 @@
+/**
+ * @author David Antúnez Pérez
+ * Realiza un programa que pida una hora por teclado y que muestre luego buenos
+ * días, buenas tardes o buenas noches según la hora. Se utilizarán los tramos de
+ * 6 a 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en cuenta las
+ * horas, los minutos no se deben introducir por teclado.
+ */
+
+
+public class Ex2 {
+  
+  public static void main (String[] args) {
+    
+  }
+}
+
