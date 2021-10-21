d8325329b6e3b51a5f83fecdbe34bc25c87e4291
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-21 19:35:44 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Ex5.java
@@ -0,0 +1,27 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ * 
+ */
+
+
+public class Ex5 {
+  
+  public static void main (String[] args) {
+      System.out.println(" First degree ecuations calculator.");
+      System.out.println(" This calculator will solve first degree ecuations estructured as: ax+b=0 ");
+      String line;
+      System.out.print(" Select a value: ");
+      line = System.console().readLine();
+      double a;
+      a = Integer.parseInt(line);
+      String line2;
+      System.out.print(" Select b value: ");
+      line2 = System.console().readLine();
+      double b;
+      b = Integer.parseInt(line2);
+      double result = -b / a;
+      System.out.println(" x = " + result);
+  }
+}
+
---
+++ Unit_04/Ex6.java
@@ -0,0 +1,21 @@
+/**
+ *@author David Antúnez Pérez
+ * 
+ * 
+ * 
+ */
+
+
+public class Ex6 {
+  
+  public static void main (String[] args) {
+      String line;
+      System.out.print(" Selecciona la altura a la que se encuentra el objeto (en metros): ");
+      line = System.console().readLine();
+      double height;
+      height = Integer.parseInt(line);
+      double result = Math.sqrt((2 * height) / 9.81);
+      System.out.print("El tiempo que tardará en caer el objeto desde " + height + " metros será de " + result + " segundos.");
+  }
+}
+
