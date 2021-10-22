26563951a8e105388162dd227f34f9789cf2bca7
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-22 12:21:16 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Ex8.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ * @author David Antúnez Pérez
+ * 
+ */
+
+public class Ex8 {
+  
+  public static void main (String[] args) {
+     System.out.println(" CALCULADORA DE MEDIA DE TRES NOTAS");
+     System.out.println("-----------------------------------");
+      String line;
+      System.out.print(" Escriba la primera nota: ");
+      line = System.console().readLine();
+      double a;
+      a = Integer.parseInt(line);
+      String line2;
+      System.out.print(" Escriba la segunda nota: ");
+      line2 = System.console().readLine();
+      double b;
+      b = Integer.parseInt(line2);
+      String line3;
+      System.out.print(" Escriba la primera nota: ");
+      line3 = System.console().readLine();
+      double c;
+      c = Integer.parseInt(line3);
+      double result = (a + b + c) / 3;
+      System.out.println(" La media entre las notas " + a + ", " + b + " y " + c + " es " + result + ".");
+      
+      if ((result > 6)&&(result < 9)){
+        System.out.println(" Has sacado una media de notable. Esfuerzate un poco más y llegarás al sobresaliente.");
+        }
+        
+      else if ((result>=5)&&(result<=6)){
+        System.out.println(" Has sacado una media de suficiente. Vas muy justo, esfuerzate más.");
+        }
+        
+        else if(result<5){
+          System.out.println(" Has sacado una media de insuficiente, estudia más.");
+          }
+          else{
+          System.out.println(" ¡Has sacado una media de sobresaliente! ¡Sigue asi!");
+          }
+  }
+}
+
+
