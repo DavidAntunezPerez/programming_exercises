3649e7ca5aa1c18b082259b0b802b7988e61f79b
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-23 20:18:21 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Ex15.java
@@ -0,0 +1,41 @@
+/**
+ * @author David Antúnez Pérez
+ */
+
+
+public class Ex15 {
+  
+  public static void main (String[] args) {
+    System.out.println("CREA TU PIRÁMIDE A MEDIDA");
+    System.out.println("--------------------------");
+    System.out.println("- Carácter que rellena la pirámide");
+    System.out.printf(" 1) * \n 2) & \n 3) $ \n");
+    String line;
+      System.out.print(" Seleccione una opción: ");
+      line = System.console().readLine();
+      int num;
+      num = Integer.parseInt(line);
+      System.out.println("- Dirección de la pirámide");
+      System.out.printf(" 1) Arriba \n 2) Abajo \n 3) Izquierda \n 4) Derecha \n");
+      String line;
+      System.out.print(" Seleccione una opción: ");
+      line = System.console().readLine();
+      int dir;
+      dir = Integer.parseInt(line);
+      switch (num){
+        case 1:
+        
+        break
+        case 2:
+        
+        break
+        case 3:
+        
+        break
+        }
+      
+      
+      
+  }
+}
+
