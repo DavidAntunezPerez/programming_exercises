7b59c9883468abe3f93e25705a55dcc622f343b5
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-25 09:55:33 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Ex18.java
@@ -0,0 +1,28 @@
+/**
+ * @author David Antúnez Pérez
+ */
+
+
+public class Ex18 {
+  
+  public static void main (String[] args) {
+          System.out.print("INTRODUCE UN NÚMERO: ");
+      int num;
+      String line;
+      line = System.console().readLine();
+      num = Integer.parseInt(line);
+      if (num <=9){
+        int result = num%10
+        System.out.println(" La primera cifra es: " + result);
+        }
+      else if(){
+        
+        }
+      else if (){
+        
+        }
+          else{
+          System.out.println("No se puede introducir un número de más de 5 cifras"); 
+          }
+  }
+}
