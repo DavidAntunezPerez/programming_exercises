155a5e973978e0a55f790b150efd01670b74adce
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-25 12:51:13 +0200

    Unit 04: Conditional sentence

---
+++ Unit_04/Ex19.java
@@ -0,0 +1,48 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ */
+
+
+public class Ex19 {
+  
+  public static void main (String[] args) {
+    int num, cif = 0;
+    System.out.print("INTRODUCE UN NÚMERO (máximo 5 cifras): ");
+      String line;
+      line = System.console().readLine();
+      num = Integer.parseInt(line);
+      if((num < 10)&&(num > -10)) {
+        cif = 1;
+        }
+      else if ((num >= 10)&&(num <100)){
+        cif = 2;
+        }
+      else if ((num >= 100)&&(num <1000)){
+        cif = 3;
+        }
+      else if ((num >= 1000)&&(num < 10000)){
+        cif = 4;
+        }
+      else if ((num >= 10000)&&(num < 100000)){
+        cif = 5;
+        }
+      else if((num >= -10)&&(num <-100)) {
+        cif = 2;
+        }
+      else if((num >= -100)&&(num <-1000)) {
+        cif = 3;
+        }
+      else if((num >= -1000)&&(num <-10000)) {
+        cif = 4;
+        }
+      else if((num >= -10000)&&(num <-100000)) {
+        cif = 5;
+        }
+      System.out.println("El número dígitos del número es "+ cif);
+    
+    
+  }
+}
+
+
