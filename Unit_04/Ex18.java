3e94349e487a3e57935ad15628218ff410596e3f
David Antúnez Pérez <antunezdavid2003@gmail.com>
2021-10-25 10:36:08 +0200

    Unit 04: Conditional sentence

--- Unit_04/Ex18.java
+++ Unit_04/Ex18.java
@@ -7,22 +7,24 @@ public class Ex18 {
   
   public static void main (String[] args) {
           System.out.print("INTRODUCE UN NÚMERO: ");
-      int num;
-      String line;
-      line = System.console().readLine();
-      num = Integer.parseInt(line);
-      if (num <=9){
-        int result = num%10
-        System.out.println(" La primera cifra es: " + result);
-        }
-      else if(){
-        
-        }
-      else if (){
-        
-        }
-          else{
-          System.out.println("No se puede introducir un número de más de 5 cifras"); 
-          }
+        String line;
+        int cif;
+        line = System.console().readLine();
+        int num = Integer.parseInt(System.console().readLine());
+    if(( num >= 100 ) && ( num < 1000 )) {
+      cif = num / 100;
+    }
+    else if (( num >= 1000 ) && ( num < 10000 )) {
+      cif = num / 1000;
+    }
+    else if ( num >= 10000 ) {
+    cif = num / 10000;
+    }
+   
+    System.out.println("La primera cifra del número es " + cif);
+    
+    
+    }
   }
-}
+
+
