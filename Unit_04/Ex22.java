8741d4d47d1b058b5a23b05d6bc79562378006ab
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-10-30 14:13:16 +0200

    Unit 04: Conditional Sentence

--- Unit_04/Ex22.java
+++ Unit_04/Ex22.java
@@ -8,6 +8,31 @@
  */
 public class Ex22 {
     public static void main(String[] args) {
-        
+        System.out.print("Introduzca un día de la semana (de Lunes a Viernes) ");
+        String day = System.console().readLine();
+        System.out.print("Introduzca la hora (Excluyendo minutos) ");
+        int hour = Integer.parseInt(System.console().readLine());
+        System.out.print("Introduzca los minutos: ");
+        int min = Integer.parseInt(System.console().readLine());
+        int numDay;
+        switch(day){
+            case "Lunes" ,"LUNES","lunes":
+                 numDay = 1;
+            break;
+            case "Martes", "martes", "MARTES":
+                numDay = 2;
+            break;
+            case "Miercoles", "Miércoles", "MIERCOLES", "MIÉRCOLES", "miercoles", "miércoles":
+                numDay = 3;
+            break;
+            case "Jueves", "jueves", "JUEVES":
+                numDay = 4;
+            break;
+            case "viernes", "Viernes", "VIERNES":
+                numDay = 5;
+            break;
+            default:
+            System.out.println("Por favor, introduzca los datos correctamente.");
+        }
  }
 }
