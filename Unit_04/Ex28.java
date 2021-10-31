b1909502d3d515afe4d000fe15988a395aaaf119
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-01 00:41:24 +0100

    Conditional Sentence

--- Unit_04/Ex28.java
+++ Unit_04/Ex28.java
@@ -1,3 +1,9 @@
+/**
+ * @author David Antunez Perez
+ * Implementa el juego piedra, papel y tijera. Primero, el usuario 1 introduce
+ * su jugada y luego el usuario 2. Si alguno de los usuarios introduce una opción
+ * incorrecta, el programa deberá mostrar un mensaje de error.
+ */
 public class Ex28 {
     public static void main(String[] args) {
         
