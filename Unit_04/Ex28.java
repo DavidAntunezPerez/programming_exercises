81e519e4f8ecd75d30f5334df08c0265f5e70cb0
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-01 13:29:23 +0100

    Conditional Sentence

--- Unit_04/Ex28.java
+++ Unit_04/Ex28.java
@@ -6,6 +6,65 @@
  */
 public class Ex28 {
     public static void main(String[] args) {
-        
+        System.out.println("PIEDRA, PAPEL O TIJERAS");
+        System.out.println("-----------------------");
+        System.out.print("Turno del jugador 1 (Introduzca piedra, papel o tijeras): ");
+            String turn1 = System.console().readLine();
+         System.out.print("Turno del jugador 2 (Introduzca piedra, papel o tijeras): ");
+            String turn2 = System.console().readLine();
+        String result = "";
+        switch (turn1){
+            case "piedra":
+            switch (turn2){
+                case "piedra":
+                result="Empate.";
+                break;
+                case "papel":
+                result="Gana el jugador 2";
+                break;
+                case "tijeras":
+                result="Gana el jugador 1";
+                break;
+                default:
+                result = "ERROR, realiza un movimiento válido";
+
+            }
+            break;
+            case "papel":
+                switch (turn2){
+                    case "piedra":
+                    result="Gana el jugador 1";
+                    break;
+                    case "papel":
+                    result="Empate";
+                    break;
+                    case "tijeras":
+                    result="Gana el jugador 2";
+                    break;
+                    default:
+                    result = "ERROR, realiza un movimiento válido";
+
+                }
+            break;
+            case "tijeras":
+                switch (turn2){
+                    case "piedra":
+                    result="Gana el jugador 2";
+                    break;
+                    case "papel":
+                    result="Gana el jugador 1";
+                    break;
+                    case "tijeras":
+                    result="Empate";
+                    break;
+                    default:
+                    result = "ERROR, realiza un movimiento válido";
+                }
+            break;
+            default:
+            result = "ERROR, realiza un movimiento válido";
+
+        }
+        System.out.println(result);
     }
 }
