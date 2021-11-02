1335c548eead0fbbf883541f141060709cb7fbeb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-02 09:40:26 +0100

    Conditional Sentence

--- Unit_04/Ex12.java
+++ Unit_04/Ex12.java
@@ -15,7 +15,7 @@ public class Ex12 {
     String ans1;
     int result = 0;
       System.out.print(" Selecciona la respuesta: ");
-      ans1 = System.console().readLine();
+      ans1 = System.console().readLine().toLowerCase();
         switch (ans1){
         case "a":
         System.out.printf(" ¡Respuesta correcta! \n IMS es una base de datos jerárquica creada por IBM \n");
@@ -34,7 +34,7 @@ public class Ex12 {
     System.out.println(" a) Airbag \n b) Guantes \n c) Frenos ABS ");
     String ans2;
       System.out.print(" Selecciona la respuesta: ");
-      ans2 = System.console().readLine();
+      ans2 = System.console().readLine().toLowerCase();
         switch (ans2){
         case "c":
         System.out.printf("¡Respuesta correcta! \n Los frenos ABS es un sistema de seguridad presente \n en coches que previene el bloqueo de ruedas \n");
@@ -53,7 +53,7 @@ public class Ex12 {
     System.out.println(" a) JRE \n b) VirtualBox \n c) Kubernetes ");
     String ans3;
       System.out.print(" Selecciona la respuesta: ");
-      ans3 = System.console().readLine();
+      ans3 = System.console().readLine().toLowerCase();
       switch (ans3){
         case "a":
         System.out.printf("¡Respuesta correcta! \nJRE (Java Runtime Enviroment) es un entorno de ejecución que proporciona \nlas blibliotecas de clases y otros recursos que un programa Java requiera ejecutar.\n");
@@ -70,7 +70,7 @@ public class Ex12 {
     System.out.println(" a) Tim Berners-Lee \n b) Marc Andreessen \n c) Steve Jobs  ");
     String ans4;
       System.out.print(" Selecciona la respuesta: ");
-      ans4 = System.console().readLine();
+      ans4 = System.console().readLine().toLowerCase();
       switch (ans4){
         case "a":
         System.out.printf("¡Respuesta correcta! \nTim Berners-Lee es un reconocido e importante científico informático de origen británico\n al que se le atribulle la invención de la WWW y del lenguaje de marcas HTML. \n");
@@ -88,7 +88,7 @@ public class Ex12 {
     System.out.println(" a) 22.04.03 \n b) 22.10 \n c) 22.01 ");
     String ans5;
       System.out.print(" Selecciona la respuesta: ");
-      ans5 = System.console().readLine();
+      ans5 = System.console().readLine().toLowerCase();
       switch (ans5){
         case "b":
         System.out.printf("¡Respuesta correcta! \nEl OS Ubuntu 22.10 fue lanzado en 14/10/21 bajo el nombre de Impish Indri\n");
@@ -106,7 +106,7 @@ public class Ex12 {
     System.out.println(" a) Sirve para compilar un archivo .java para poder ejecutarlo posteriormente \n b) Un tipo de datos primitivo que puede almacenar valores verdaderos o falsos.\n c) Se utiliza para analizar los errores de un archivo .java para compilarlos ");
     String ans6;
       System.out.print(" Selecciona la respuesta: ");
-      ans6 = System.console().readLine();
+      ans6 = System.console().readLine().toLowerCase();
       switch (ans6){
         case "b":
         System.out.printf("¡Respuesta correcta! \n Una boolean o variable booleana es capaz de \nverificar si un hecho es verdadero o falso en Java\n");
@@ -124,7 +124,7 @@ public class Ex12 {
     System.out.println(" a) Socket \n b) PSU \n c) PCIExpress ");
     String ans7;
       System.out.print(" Selecciona la respuesta: ");
-      ans7 = System.console().readLine();
+      ans7 = System.console().readLine().toLowerCase();
         switch (ans7){
           case "b":
           System.out.printf("¡Respuesta correcta! \nLa PSU o Power Supply Unit se encarga de suministrar la energía electrica de los componentes del ordenador. \n");
@@ -142,7 +142,7 @@ public class Ex12 {
     System.out.println(" a) C \n b) C++ \n c) SQL ");
     String ans8;
       System.out.print(" Selecciona la respuesta: ");
-      ans8 = System.console().readLine();
+      ans8 = System.console().readLine().toLowerCase();
       switch (ans8){
         case "b":
         System.out.printf("¡Respuesta correcta! \n C++ es una extensión a C para soportar la programación orientada a objetos (POO).  \n");
@@ -160,7 +160,7 @@ public class Ex12 {
     System.out.println(" a) 5 \n b) 4 \n c) 6 ");
     String ans9;
       System.out.print(" Selecciona la respuesta: ");
-      ans9 = System.console().readLine();
+      ans9 = System.console().readLine().toLowerCase();
       switch (ans9){
         case "a":
         System.out.printf("¡Respuesta correcta! \nHTLM 5 es la última especificación oficial y se espera que continúe evolucionando a lo largo de los próximos años. \n");
@@ -179,7 +179,7 @@ public class Ex12 {
     System.out.println(" a) C \n b) COBOL \n c) Fortran ");
     String ans10;
       System.out.print(" Selecciona la respuesta: ");
-      ans10 = System.console().readLine();
+      ans10 = System.console().readLine().toLowerCase();
       switch (ans10){
         case "c":
         System.out.printf("¡Respuesta correcta! \nEl FORTRAN (FORmula TRANslator) fue el primer lenguaje de programación utilizado universalmente en el mundo de la programación. \n Fue creado en 1957 por John Backus de IBM. \n");
