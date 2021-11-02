bc500b2dbb6738b4702fc9ea0645c692577be38e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-02 10:48:13 +0100

    Conditional Sentence

--- Unit_04/Ex20.java
+++ Unit_04/Ex20.java
@@ -3,41 +3,44 @@
  * @author David Antúnez Pérez
  * 
  */
-
-
 public class Ex20 {
-  public static void main(String[] args) {
-    boolean capicua = false;
-    System.out.print("Introduzca un número entero (de 5 cifras como máximo): ");
-    int n = Integer.parseInt(System.console().readLine());
-    if (n < 10) {
+    public static void main(String[] args) {
+      int n;
+      boolean capicua = false;
+      System.out.print("Por favor, introduzca un número entero (de 5 cifras como máximo): ");
+      n = Integer.parseInt(System.console().readLine());
+// una cifra
+  if (n < 10) {
     capicua = true;
-    }
-    if ((n >= 10) && (n < 100)) {
-      if ((n / 10) == (n % 10)) {
-        capicua = true;
-    }
-   }
-   if ((n >= 1000) && (n < 10000)){
-      if(((n / 1000) == (n % 10)) && ((( n / 100 ) % 10)== (( n / 10) % 10))){
-        capicua=true;
-    }
-   }
-   if ((n >= 1000) && (n < 10000)){
-    if(((n / 1000) == (n % 10)) && ((( n / 100 ) % 10)== (( n / 10) % 10))){
-      capicua=true;
-   }
+  }  
+  // dos cifras
+  if ((n >= 10) && (n < 100)) {
+  if ((n / 10) == (n % 10)) {
+  capicua = true;
   }
-  if (((n / 10000) == (n % 10) ) && ((((n / 1000) % 10)) == ((n / 10) % 10))){
-    if((n/10000) == (n%10)){
-      capicua=true;
-   }
-  if(capicua){
-    System.out.println("El número es capicua.");
   }
-  else{
-    System.out.println("El número no es capicua.");
-   }
+  // tres cifras
+  if ((n >= 100) && (n < 1000)) {
+  if ((n / 100) == (n % 10)) {
+  capicua = true;
+  }
+  }
+  // cuatro cifras
+  if ((n >= 1000) && (n < 10000)) {
+  if (((n / 1000) == (n % 10)) && ((( n / 100 ) % 10)== (( n / 10) % 10))) {
+  capicua = true;
+  }
+  }
+  // cinco cifras
+  if (n >= 10000) {
+  if (((n / 10000) == (n % 10) ) && ((((n / 1000) % 10)) == ((n / 10) % 10))) {
+  capicua = true;
+  }
+  }
+  if (capicua) {
+  System.out.println("El número introducido es capicúa.");
+  } else {
+  System.out.println("El número introducido no es capicúa.");
   }
  }
 }
