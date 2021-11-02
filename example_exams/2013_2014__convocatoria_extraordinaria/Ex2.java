9dc1983ae079307069f0cb7d8506c42bf576e5cb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-02 11:12:59 +0100

    Exam Examples

--- example_exams/2013_2014__convocatoria_extraordinaria/Ex2.java
+++ example_exams/2013_2014__convocatoria_extraordinaria/Ex2.java
@@ -1,5 +1,14 @@
+/**
+ * @author David Antunez Perez
+ * Realiza un programa que trocee un número introducido por teclado en otros dos números. Un trozo se
+    formará con los dígitos de las posiciones pares y otro trozo con los dígitos de las posiciones impares.
+    Se debe conservar el orden de los dígitos. Da igual el trozo que se muestre primero con tal de que
+    cada trozo esté bien formado.
+ */
 public class Ex2 {
     public static void main(String[] args) {
-        
+        System.out.println("Escriba un número: ");
+        int n = Integer.parseInt(System.console().readLine());
+        System.out.println(n);
     }
 }
