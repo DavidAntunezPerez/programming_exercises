72bc577fdf20ab6e0762c810f0ebcae8b6d268ab
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-02 19:36:13 +0100

    Example Exams

--- example_exams/2013_2014__convocatoria_extraordinaria/Exam_Ex2.java
+++ example_exams/2013_2014__convocatoria_extraordinaria/Exam_Ex2.java
@@ -7,7 +7,7 @@
  */
 public class Exam_Ex2 {
     public static void main(String[] args) {
-        System.out.println("Escriba un número: ");
+        System.out.print("Escriba un número: ");
         int n = Integer.parseInt(System.console().readLine());
         System.out.println(n);
         
