f3db54b7341c4e7af961f141282cfcc95751f06f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-02 10:54:16 +0100

    Example Exams

---
+++ example_exams/2013_2014__convocatoria_extraordinaria/Ex1.java
@@ -0,0 +1,11 @@
+/**
+ * Realiza un programa que pinte la letra T por pantalla hecha con asteriscos. El programa pedirá la
+    altura. El palo horizontal de la T tendrá una longitud de la mitad (división entera entre 2) de la altura
+    más uno.
+
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        
+    }
+}
