46566d5b747d05ea4997d1c0b9505682dd36a021
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-05 19:17:31 +0100

    Loops

---
+++ Unit_05/Ex1.java
@@ -0,0 +1,11 @@
+/**
+ * @author David Antunez Perez
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+       for (int n = 0; n <= 500; n++){
+           System.out.println(n);
+       }
+    }
+}
