0886fc23ff029d37f8a0623686ae682047964535
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-05 18:28:04 +0100

    Loops

---
+++ Unit_05/Practice/UseWhile.java
@@ -0,0 +1,13 @@
+/**
+ * @author David Antunez Perez
+ * Aprender el uso de while
+ * while (expresion) {
+    sentencias
+    }
+ */
+
+public class UseWhile {
+    public static void main(String[] args) {
+        
+    }
+}
