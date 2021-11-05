1aa74de11c21e7d9fc0e751308c9d0e3081dd6bf
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-05 19:21:38 +0100

    Loops

---
+++ Unit_05/Ex2.java
@@ -0,0 +1,13 @@
+/**
+ * @author David Antunez Perez
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+ */
+public class Ex2 {
+   public static void main(String[] args) {
+       int n = 0;
+        while (n <= 100){
+           System.out.println(n);
+           n += 5;
+       }
+   } 
+}
