8879a15185296c7be9e84615621493473b2415fe
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-05 19:12:14 +0100

    Loops

--- Unit_05/Practice/UseDoWhile2.java
+++ Unit_05/Practice/UseDoWhile2.java
@@ -10,7 +10,7 @@ public class UseDoWhile2 {
             }
             else {
                 System.out.println("Numero impar introducido, finalizando bucle.");
-                    System.out.print("¿Quiere continuar? (s / n): ");
+                    System.out.print("¿Quiere continuar? (s / n): "); // Para requerir si quiere continuar
                     ans = System.console().readLine();
                     if ("s".equals(ans)){
                         do{
@@ -29,6 +29,6 @@ public class UseDoWhile2 {
         }while(n % 2 == 0);
         // Esto ocurre debido a que primero te ejecuta el trozo de código dentro del do, y luego te pide el requisito para que se cumpla, por lo tanto, hemos podido poner el mensaje de impares dentro del do debido a que cuando se ejecute la última vez mostrará antes de finalizar el mensaje de número impar, ya que el bucle se seguía cumpliendo, y después parará debido a que los valores del bucle no son los requeridos.
 
-        // Para requerir si quiere continuar
+        
     }
 }
