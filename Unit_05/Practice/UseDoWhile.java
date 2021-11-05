2097d3b8321b75c59eca63d16a52bc566c2e6e77
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-05 18:50:39 +0100

    Loops

---
+++ Unit_05/Practice/UseDoWhile.java
@@ -0,0 +1,16 @@
+/**
+ * @author David Antunez Perez
+ *  do {
+    sentencias
+    } while (expresion)
+ */
+public class UseDoWhile {
+    public static void main(String[] args) {
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        }while (n < 10);
+        
+    }
+}
