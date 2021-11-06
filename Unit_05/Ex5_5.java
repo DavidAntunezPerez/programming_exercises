d4c9433d7af6324600ab5f02516e5b7462a59398
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-06 12:09:32 +0100

    Loops

---
+++ Unit_05/Ex5_5.java
@@ -0,0 +1,14 @@
+/**
+ * @author David Antunez Perez
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle while.
+ */
+public class Ex5_5 {
+    public static void main(String[] args) {
+        int n = 320;
+        while (n >= 160){
+            System.out.println(n);
+            n--;
+        }
+    }
+}
