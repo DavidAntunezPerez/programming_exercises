ce9e6c68b915179683ec4d1f495da7ad0cd10a73
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-06 12:00:15 +0100

    Loops

--- Unit_05/Ex3_5.java
+++ Unit_05/Ex3_5.java
@@ -1,5 +1,6 @@
 /**
  * @author David Antunez Perez
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while
  */
 public class Ex3_5 {
     public static void main(String[] args) {
