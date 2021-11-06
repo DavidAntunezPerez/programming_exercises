ea314629df9db3d4e70c8c8a9ea9433ead395475
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-06 12:11:29 +0100

    Loops

--- Unit_05/Ex6_5.java
+++ Unit_05/Ex6_5.java
@@ -1,3 +1,8 @@
+/**
+ * @author David Antunez Perez
+ * Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle
+ * do-while.
+ */
 public class Ex6_5 {
     public static void main(String[] args) {
         
