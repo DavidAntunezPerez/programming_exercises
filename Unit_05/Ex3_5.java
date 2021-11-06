95f7fc0ca163ab55d044ad601860e2359f71a2ab
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-06 11:59:05 +0100

    Loops

--- Unit_05/Ex3_5.java
+++ Unit_05/Ex3_5.java
@@ -1,5 +1,9 @@
 public class Ex3_5 {
     public static void main(String[] args) {
-        
+        int n = 0;
+        do{
+            System.out.println(n);
+            n++;
+        }while(n <= 100);
     }    
 }
