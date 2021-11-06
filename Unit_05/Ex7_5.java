928f8a27585693a30d28f9863e4789b4c59feb12
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-06 12:13:43 +0100

    Loops

---
+++ Unit_05/Ex7_5.java
@@ -0,0 +1,13 @@
+/**
+ * @author David Antunez Perez
+ * Realiza el control de acceso a una caja fuerte. La combinación será un
+ * número de 4 cifras. El programa nos pedirá la combinación para abrirla. Si no
+ * acertamos, se nos mostrará el mensaje “Lo siento, esa no es la combinación”
+ * y si acertamos se nos dirá “La caja fuerte se ha abierto satisfactoriamente”.
+ * Tendremos cuatro oportunidades para abrir la caja fuerte.
+ */
+public class Ex7_5 {
+    public static void main(String[] args) {
+        
+    }
+}
