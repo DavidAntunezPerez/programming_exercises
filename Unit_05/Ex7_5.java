fc1408d17bbf0b3ce75111d5584254272eafe083
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-06 12:22:25 +0100

    Loops

--- Unit_05/Ex7_5.java
+++ Unit_05/Ex7_5.java
@@ -8,6 +8,21 @@
  */
 public class Ex7_5 {
     public static void main(String[] args) {
-        
+        int comb; // PIN ingresado en la caja fuerte
+        int cont = 0; // contador de intentos de la caja fuerte
+        do {
+            System.out.print("Por favor, ingrese el PIN para abrir la caja fuerte: ");
+            comb = Integer.parseInt(System.console().readLine());
+            if (comb == 2744){
+                System.out.println("La caja fuerte se ha abierto exitosamente.");
+                cont = 5;
+            }
+            else{
+                System.out.println("Lo siento, el PIN no es el correcto.");
+                cont++; 
+                int resting = 4 - cont; // intentos restantes
+                System.out.println("Quedan "+resting+" intentos.");
+            }
+        }while (cont < 4 );
     }
 }
