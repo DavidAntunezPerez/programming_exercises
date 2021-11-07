20251d8a4da4a78fbb2a93d21d2906457f9a24ac
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-07 19:37:12 +0100

    Loops

---
+++ Unit_05/Ex9_5.java
@@ -0,0 +1,14 @@
+/** 
+ * @author David Antúnez Pérez
+ *  Realiza un programa que nos diga cuántos dígitos tiene un número introducido
+    por teclado. Este ejercicio es equivalente a otro realizado anteriormente, con
+    la salvedad de que el anterior estaba limitado a números de 5 dígitos como
+    máximo. En esta ocasión, hay que realizar el ejercicio utilizando bucles; de
+    esta manera, la única limitación en el número de dígitos la establece el tipo de
+    dato que se utilice (int o long).
+*/
+public class Ex9_5 {
+    public static void main(String[] args) {
+        
+    }
+}
