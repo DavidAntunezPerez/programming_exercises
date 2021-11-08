091196df53a24e2f20bb9cb7ba223cf9d9e4619a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-08 08:59:23 +0100

    Loops

---
+++ Unit_05/Ex10_5.java
@@ -0,0 +1,12 @@
+/**
+ * @author David Antunez Perez
+ * Escribe un programa que calcule la media de un conjunto de números positivos
+    introducidos por teclado. A priori, el programa no sabe cuántos números se
+    introducirán. El usuario indicará que ha terminado de introducir los datos
+    cuando meta un número negativo.
+ */
+public class Ex10_5 {
+    public static void main(String[] args) {
+        
+    }
+}
