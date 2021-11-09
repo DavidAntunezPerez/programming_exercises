62999a589384306ad310dc895af663fa99137084
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 10:02:30 +0100

    Loops

--- Unit_05/Ex13_5.java
+++ Unit_05/Ex13_5.java
@@ -1,5 +1,25 @@
+/**
+ * Escribe un programa que lea una lista de diez números y determine cuántos
+ * son positivos, y cuántos son negativos.
+ * @author David Antunez Perez
+ */
+
 public class Ex13_5 {
     public static void main(String[] args) {
+        for (int n = 10;n > 0 ;n-- ){
+            double num;
+            System.out.println("Introduzca un número: ");
+            num = Double.parseDouble(System.console().readLine());
+            if( num > 0){
+                System.out.println(num + " es positivo.");
+            }
+            else if ( num < 0){
+                System.out.println(num + " es negativo.");
+            }
+            else {
+                System.out.println(num + " es neutro.");
+            }
+        }
         
-    }
+  }
 }
