73b5bb99052c3618b5c12ffe530f19e3a80e1ffb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 08:49:01 +0100

    Loops

--- Unit_05/Ex11_5.java
+++ Unit_05/Ex11_5.java
@@ -1,3 +1,5 @@
+import java.time.Period;
+
 /**
  * Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
  * los 5 primeros números enteros a partir de uno que se introduce por teclado.
@@ -5,12 +7,15 @@
  */
 public class Ex11_5 {
     public static void main(String[] args) {
-        int n = 0;
-        int i;
+        int n = 0; // contador de números consecutivos
+        int i; // número ingresado
+        int sq; // cuadrado del número
+        int cb; // cubo del número
         System.out.println("Introduce un número: ");
-            i = Integer.parseInt(System.console().readLine())
+            i = Integer.parseInt(System.console().readLine());
         while(n <= 5){
-            
+            i++;
+            System.out.println("El cuadrado de "+i+" es "++" y su cubo es "+);
             
         }
     }
