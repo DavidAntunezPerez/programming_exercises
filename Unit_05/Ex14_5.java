a33a9ca58cb5c29be9be20010bfe11fd7f0bfc6a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 10:07:10 +0100

    Loops

---
+++ Unit_05/Ex14_5.java
@@ -0,0 +1,10 @@
+/**
+ * Escribe un programa que pida una base y un exponente (entero positivo) y que
+ * calcule la potencia.
+ * @author David Antunez Perez
+ */
+public class Ex14_5 {
+    public static void main(String[] args) {
+        
+    }
+}
