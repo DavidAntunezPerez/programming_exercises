5bc528cf7da5e18209f3b7e98e8da99cf3a8579a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 08:40:03 +0100

    Loops

--- Unit_05/Ex11_5.java
+++ Unit_05/Ex11_5.java
@@ -1,7 +1,7 @@
 /**
  * Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
  * los 5 primeros números enteros a partir de uno que se introduce por teclado.
- * 
+ * @author David Antúnez Pérez
  */
 public class Ex11_5 {
     public static void main(String[] args) {
