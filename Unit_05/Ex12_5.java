82d1aacf5934cc39b91d17434625332161ba405c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 09:32:04 +0100

    Loops

--- Unit_05/Ex12_5.java
+++ Unit_05/Ex12_5.java
@@ -9,6 +9,26 @@
  */
 public class Ex12_5 {
     public static void main(String[] args) {
+        int n; // número de números introducidos
+        int f = 0; // primer número de la serie
+        int f2 = 1; // segundo número de la serie
+        int f3 = 1;
+        int sto = f3;
+        System.out.print("Introduce el número de números de la serie de Fibonacci que quieres ver: ");
+        n = Integer.parseInt(System.console().readLine());
+        if ( n == 1 ){
+            System.out.println(f);
+        }
+        else if (n >= 2){
+            System.out.println(f);
+            System.out.println(f2);
+        }
+       while (n > 3){
+        System.out.println(sto);
+        sto = 
         
+
+        n--;
+       }
     }
 }
