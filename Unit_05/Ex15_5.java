14a7e615a69655da9b7e3de17c8efd645e6abff4
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 10:26:37 +0100

    Loops

---
+++ Unit_05/Ex15_5.java
@@ -0,0 +1,12 @@
+/**
+ * Escribe un programa que dados dos números, uno real (base) y un entero
+ * positivo (exponente), saque por pantalla todas las potencias con base el
+ * numero dado y exponentes entre uno y el exponente introducido. No se deben
+ * utilizar funciones de exponenciación. P
+ * @author David Antunez Perez
+ */
+public class Ex15_5 {
+    public static void main(String[] args) {
+        
+    }
+}
