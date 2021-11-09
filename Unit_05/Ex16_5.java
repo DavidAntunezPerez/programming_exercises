e6d27de7f19890d28585fc42f41b9d3918d10076
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 11:16:19 +0100

    Loops

--- Unit_05/Ex16_5.java
+++ Unit_05/Ex16_5.java
@@ -2,28 +2,25 @@
  * Escribe un programa que diga si un número introducido por teclado es o no
  * primo. Un número primo es aquel que sólo es divisible entre él mismo y la
  * unidad
+ * 
  * @author David Antunez Perez
  */
 public class Ex16_5 {
-    public static void main(String[] args) {
-        System.out.print("Introduce un número: ");
-        int n = Integer.parseInt(System.console().readLine());
-        int c= n - 1;
-        boolean nprim = false;
-        do{
-            if (n % c == 0){
-                nprim = false;
-            }
-            else if (n % c != 0){
-                nprim = true;
-            }
-            c--;
-        }while (c > 1);
-            if ( nprim = true){
-                System.out.println(n + " es un número primo.");
-            }
-            else{
-                System.out.println(n + " NO es un número primo.");
-            }
+  public static void main(String[] args) {
+    System.out.print("Introduce un número: ");
+    int n = Integer.parseInt(System.console().readLine());
+    int c = n - 1;
+    boolean nPrim = true;
+    do {
+      if (n % c == 0) {
+        nPrim = false;
+      }
+      c--;
+    } while (c > 1);
+    if (nPrim = true) {
+      System.out.println(n + " es un número primo.");
+    } else {
+      System.out.println(n + " NO es un número primo.");
     }
+  }
 }
