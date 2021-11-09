b473d3f1698e74df24cc6849b12f4c78d2373096
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 10:41:08 +0100

    Loops

---
+++ Ex16_5.java
@@ -0,0 +1,11 @@
+/**
+ * Escribe un programa que diga si un número introducido por teclado es o no
+ * primo. Un número primo es aquel que sólo es divisible entre él mismo y la
+ * unidad
+ * @author David Antunez Perez
+ */
+public class Ex16_5 {
+    public static void main(String[] args) {
+        
+    }
+}
