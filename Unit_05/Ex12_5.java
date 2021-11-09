c2882f0a036ffff163327a65d34f6b122ebe7a13
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 08:54:57 +0100

    Loops

---
+++ Unit_05/Ex12_5.java
@@ -0,0 +1,14 @@
+/**
+ * Escribe un programa que muestre los n primeros términos de la serie de
+    Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1 y
+    el resto se calcula sumando los dos anteriores, por lo que tendríamos que los
+    términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144… El número n se debe
+    introducir por teclado.
+ *
+ * @author David Antúnez Pérez
+ */
+public class Ex12_5 {
+    public static void main(String[] args) {
+        
+    }
+}
