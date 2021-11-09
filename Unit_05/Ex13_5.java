fe3adc3b0374fac01e078eaee13028a91cbe73e5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 10:05:22 +0100

    Loops

--- Unit_05/Ex13_5.java
+++ Unit_05/Ex13_5.java
@@ -6,20 +6,24 @@
 
 public class Ex13_5 {
     public static void main(String[] args) {
+        int pos = 0 ; // contador de cuantos son positivos
+        int neg = 0; // contador de cuantos son negativos
         for (int n = 10;n > 0 ;n-- ){
             double num;
             System.out.println("Introduzca un número: ");
             num = Double.parseDouble(System.console().readLine());
             if( num > 0){
                 System.out.println(num + " es positivo.");
+                pos++;
             }
             else if ( num < 0){
                 System.out.println(num + " es negativo.");
+                neg++;
             }
             else {
                 System.out.println(num + " es neutro.");
             }
         }
-        
+        System.out.println("En total, hay "+pos+" nº positivos y "+neg+" nº negativos.");
   }
 }
