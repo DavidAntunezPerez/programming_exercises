9620e5d805cda72e8299e1092770608786ab1651
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-09 10:54:55 +0100

    Loops

--- Unit_05/Ex16_5.java
+++ Unit_05/Ex16_5.java
@@ -6,6 +6,21 @@
  */
 public class Ex16_5 {
     public static void main(String[] args) {
-        
+        System.out.print("Introduce un número: ");
+        int n = Integer.parseInt(System.console().readLine());
+        int c= n - 1;
+        boolean primo = false;
+        do{
+            if (n % c == 0){
+                primo = true;
+            }
+            c--;
+        }while (c > 1);
+        if ( primo = true){
+            System.out.println(n + " es un número primo.");
+        }
+        else{
+            System.out.println(n + " NO es un número primo.");
+        }
     }
 }
