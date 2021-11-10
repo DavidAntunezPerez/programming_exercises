f52f0c6bac8edd0a740ad2c6843fa6d0df68f46a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-10 23:41:51 +0100

    Loops

--- Unit_05/Ex17_5.java
+++ Unit_05/Ex17_5.java
@@ -8,10 +8,13 @@
  */
 public class Ex17_5 {
   public static void main(String[] args) {
+    int n= 0;
     do{
-      System.out.println("Escribe un número entero positivo: ");
-          int n = Integer.parseInt(System.console().readLine());
-      if (n < 0)
+      System.out.print("Escribe un número entero positivo: ");
+        n = Integer.parseInt(System.console().readLine());
+      if (n < 0){
+        System.out.println("ERROR, número no válido. Asegurese de que el número sea entero y positivo.");
+      }
     }while(n < 0);
     
     for(){
