7af00311cdad68d2d7da012505ed1ed387a098e3
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-10 23:57:46 +0100

    Loops

--- Unit_05/Ex17_5.java
+++ Unit_05/Ex17_5.java
@@ -8,18 +8,18 @@
  */
 public class Ex17_5 {
   public static void main(String[] args) {
-    int n= 0; // número introducido
-    do{
+    int n = 0; // número introducido
+    do {
       System.out.print("Escribe un número entero positivo: ");
-        n = Integer.parseInt(System.console().readLine());
-      if (n < 0){
+      n = Integer.parseInt(System.console().readLine());
+      if (n < 0) {
         System.out.println("ERROR, número no válido. Asegurese de que el número sea entero y positivo.");
       }
-    }while(n < 0);
+    } while (n < 0);
     int add = 0; // suma de todos los números
-    for(int i = n; i < n + 100; i++){
+    for (int i = n; i < n + 100; i++) {
       add += i;
     }
-    System.out.println("La suma de los 100 números posteriores a "+n+" es: "+add);
+    System.out.println("La suma de los 100 números posteriores a " + n + " es: " + add);
   }
 }
