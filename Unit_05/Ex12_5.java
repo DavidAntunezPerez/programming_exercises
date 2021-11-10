4d2adeac62227d78716bc5ca34fdb5290062345d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 00:14:26 +0100

    Loops

--- Unit_05/Ex12_5.java
+++ Unit_05/Ex12_5.java
@@ -1,41 +1,40 @@
 /**
  * Escribe un programa que muestre los n primeros términos de la serie de
-    Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1 y
-    el resto se calcula sumando los dos anteriores, por lo que tendríamos que los
-    términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144… El número n se debe
-    introducir por teclado.
+ * Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1 y
+ * el resto se calcula sumando los dos anteriores, por lo que tendríamos que los
+ * términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144… El número n se
+ * debe introducir por teclado.
  *
  * @author David Antúnez Pérez
  */
 public class Ex12_5 {
-    public static void main(String[] args) {
-        int n; // número de números introducidos
-        int f = 0; // primer número de la serie
-        int f2 = 1; // segundo número de la serie
-        int f3 = 1;
-        int sto;
-        int result = 2;
-        System.out.print("Introduce el número de números de la serie de Fibonacci que quieres ver: ");
-        n = Integer.parseInt(System.console().readLine());
-        switch (n){
-            case 1:
-                System.out.println(f);
-            break;
-            case 2:
-                System.out.println(f);
-                System.out.println(f2);
-            break;
-            default:
-                System.out.println(f);
-                System.out.println(f2);
-                System.out.println(f3);
-        }
-       while (n >= 4){
-        
-        System.out.println(result);
-        sto = result;
+	public static void main(String[] args) {
+		int n; // número de números introducidos
+		int f = 0; // primer número de la serie
+		int f2 = 1; // segundo número de la serie
+		int f3 = 1;
+		int sto;
+		int result = 2;
+		System.out.print("Introduce el número de números de la serie de Fibonacci que quieres ver: ");
+		n = Integer.parseInt(System.console().readLine());
+		switch (n) {
+		case 1:
+			System.out.println(f);
+			break;
+		case 2:
+			System.out.println(f);
+			System.out.println(f2);
+			break;
+		default:
+			System.out.println(f);
+			System.out.println(f2);
+			System.out.println(f3);
+			while (n > 2) {
+				System.out.println(result);
+				sto = result;
+				n--;
+			}
+		}
 
-        n--;
-       }
-    }
+	}
 }
