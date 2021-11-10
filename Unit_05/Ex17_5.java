99291225805d086c6c96def9b6ee00939505dba0
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-10 23:34:02 +0100

    Loops

--- Unit_05/Ex17_5.java
+++ Unit_05/Ex17_5.java
@@ -1,12 +1,13 @@
 /**
- * Realiza un programa que sume los 100 números siguientes a un número entero
- * y positivo introducido por teclado. Se debe comprobar que el dato introducido
+ * Realiza un programa que sume los 100 números siguientes a un número entero y
+ * positivo introducido por teclado. Se debe comprobar que el dato introducido
  * es correcto (que es un número positivo).
+ * 
  * @author David Antunez Perez
  * 
  */
 public class Ex17_5 {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+    
+  }
 }
