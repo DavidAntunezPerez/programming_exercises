7f38b2329189cd1fee3d6bb58807789ee573a63e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 08:19:49 +0100

    Loops

---
+++ Unit_05/Ex18_5.java
@@ -0,0 +1,13 @@
+/**
+ * Escribe un programa que obtenga los números enteros comprendidos entre dos
+ * números introducidos por teclado y validados como distintos, el programa debe
+ * empezar por el menor de los enteros introducidos e ir incrementando de 7 en
+ * 7.
+ * 
+ * @author David Antúnez Pérez
+ */
+public class Ex18_5 {
+  public static void main(String[] args) {
+    
+  }
+}
