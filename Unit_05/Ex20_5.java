30a11bcfb6ddd26918e694a5e9e10818a485f1ce
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 09:31:18 +0100

    Loops

--- Unit_05/Ex20_5.java
+++ Unit_05/Ex20_5.java
@@ -1,3 +1,9 @@
+/**
+ * Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca.
+ * @author David Antunez Perez
+ */
 public class Ex20_5 {
-  
+  public static void main(String[] args) {
+    
+  }
 }
