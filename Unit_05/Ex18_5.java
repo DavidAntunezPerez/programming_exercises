daa9e9bc86d5314c4b81c513bf0f67007ce1e593
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 08:24:02 +0100

    Loops

--- Unit_05/Ex18_5.java
+++ Unit_05/Ex18_5.java
@@ -8,6 +8,10 @@
  */
 public class Ex18_5 {
   public static void main(String[] args) {
+    System.out.print("Introduce el primer número: ");
+    int n1 = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduce el segundo número: ");
+    int n2 = Integer.parseInt(System.console().readLine());
     
   }
 }
