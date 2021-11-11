4d69119670f69a78f2cc36a2530b378c7e05a98a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 10:56:48 +0100

    Loops

---
+++ Unit_05/Ex21_5.java
@@ -0,0 +1,13 @@
+/**
+ * Realiza un programa que vaya pidiendo números hasta que se introduzca un
+ * numero negativo y nos diga cuantos números se han introducido, la media de
+ * los impares y el mayor de los pares. El número negativo sólo se utiliza para
+ * indicar el final de la introducción de datos pero no se incluye en el cómputo
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex21_5 {
+  public static void main(String[] args) {
+    
+  }
+}
