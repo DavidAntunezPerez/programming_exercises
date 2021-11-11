424ce511094acc1da696fc87252ba7aee6adca29
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 12:40:07 +0100

    Loops

--- Unit_05/Ex22_5.java
+++ Unit_05/Ex22_5.java
@@ -5,6 +5,9 @@
  */
 public class Ex22_5 {
   public static void main(String[] args) {
-  System.out.println("Los números primos entre 2 y 100 son: ");
+    System.out.println("Los números primos entre 2 y 100 son: ");
+    for (int i = 2; i % 2 != 0; i++) {
+      System.out.print(i + " ");
+    }
   }
 }
