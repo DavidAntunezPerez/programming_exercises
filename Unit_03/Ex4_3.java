7450b6946acaadaa510d96af16b7a9b807f25137
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 08:58:47 +0100

    Datalecture from Keyboard

--- Unit_03/Ex4_3.java
+++ Unit_03/Ex4_3.java
@@ -6,30 +6,28 @@
  * 
  */
 
-
 public class Ex4_3 {
- 
-  public static void main (String[] args) {
-     String line;
-     System.out.print(" Please, put a number: ");
-     line = System.console().readLine();
-     double number1;
-     number1 = Integer.parseInt(line);
-     System.out.print(" Put another number, please: ");
-     line = System.console().readLine();
-     double number2;
-     number2 = Integer.parseInt(line);
-     System.out.println( " First number is: " + number1 );
-     System.out.println( " Second number is: " + number2);
-     double plus = number1 + number2;
-     System.out.println( " First number plus second number is: " + plus);
-     double minus = number1 - number2;
-     System.out.println( " First number minus second number is: " + minus);
-     double mult = number1 * number2;
-     System.out.println( " First numer multiplied by second number is: " + mult);
-     double div = number1 / number2;
-     System.out.println( " First numer divided by second number is: " + div);
-  
+
+  public static void main(String[] args) {
+    String line;
+    System.out.print(" Please, put a number: ");
+    line = System.console().readLine();
+    double number1;
+    number1 = Integer.parseInt(line);
+    System.out.print(" Put another number, please: ");
+    line = System.console().readLine();
+    double number2;
+    number2 = Integer.parseInt(line);
+    System.out.println(" First number is: " + number1);
+    System.out.println(" Second number is: " + number2);
+    double plus = number1 + number2;
+    System.out.println(" First number plus second number is: " + plus);
+    double minus = number1 - number2;
+    System.out.println(" First number minus second number is: " + minus);
+    double mult = number1 * number2;
+    System.out.println(" First numer multiplied by second number is: " + mult);
+    double div = number1 / number2;
+    System.out.println(" First numer divided by second number is: " + div);
+
   }
 }
-
--- Unit_03/Ex5_3.java
+++ Unit_03/Ex5_3.java
@@ -4,25 +4,21 @@
  * 
  */
 
-
 public class Ex5_3 {
-  
-  public static void main (String[] args) {
-     String line;
-     System.out.print(" Rectangle height: ");
-     line = System.console().readLine();
-     double number1;
-     number1 = Integer.parseInt(line);
-     System.out.print(" Rectangle base: ");
-     line = System.console().readLine();
-     double number2;
-     number2 = Integer.parseInt(line);
-     System.out.println( " Rectangle height is: " + number1 );
-     System.out.println( " Rectangle base is: " + number2);
-     double result = (number1 * number2);
-     System.out.println( " The area of the rectangle is: " + result);
+
+  public static void main(String[] args) {
+    String line;
+    System.out.print(" Rectangle height: ");
+    line = System.console().readLine();
+    double number1;
+    number1 = Integer.parseInt(line);
+    System.out.print(" Rectangle base: ");
+    line = System.console().readLine();
+    double number2;
+    number2 = Integer.parseInt(line);
+    System.out.println(" Rectangle height is: " + number1);
+    System.out.println(" Rectangle base is: " + number2);
+    double result = (number1 * number2);
+    System.out.println(" The area of the rectangle is: " + result);
   }
 }
-
-
-
