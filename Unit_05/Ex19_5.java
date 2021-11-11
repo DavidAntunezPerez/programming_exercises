13f2144a7e3bc0bb445fa90f1aeeb27260f8f8ca
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 08:54:01 +0100

    Loops

--- Unit_05/Ex19_5.java
+++ Unit_05/Ex19_5.java
@@ -1,12 +1,16 @@
 /**
  * Realiza un programa que pinte una pirámide por pantalla. La altura se debe
- * pedir por teclado. El carácter con el que se pinta la pirámide también se debe
- * pedir por teclado
+ * pedir por teclado. El carácter con el que se pinta la pirámide también se
+ * debe pedir por teclado
  * 
  * @author David Antunez Perez
  */
 public class Ex19_5 {
   public static void main(String[] args) {
+    System.out.print("Indica la altura de la pirámide: ");
+      int n = Integer.parseInt(System.console().readLine());
+    System.out.println("Indica el carácter que quieres que lleve la pirámide: ");
+      String s = System.console().readLine();
     
   }
 }
