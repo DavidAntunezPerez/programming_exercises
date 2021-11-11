ffb701945def42df79effdd79963b205e422eaba
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 09:14:42 +0100

    Loops

--- Unit_05/Ex19_5.java
+++ Unit_05/Ex19_5.java
@@ -8,17 +8,25 @@
 public class Ex19_5 {
   public static void main(String[] args) {
     System.out.print("Indica la altura de la pirámide: ");
-      int n = Integer.parseInt(System.console().readLine());
+      int h = Integer.parseInt(System.console().readLine());
     System.out.print("Indica el carácter que quieres que lleve la pirámide: ");
       String ch = System.console().readLine();
-      // espacios de la pirámide
-    for(){
-
+    int floor = 1;
+    int lenght = 1;
+    int spa = h - 1; 
+    while(floor <= h){
+        // espacios de la pirámide
+      for(int i = 1; i <= spa; i++){
+        System.out.print(" ");
+      }
+      // carácteres de la pirámide
+      for(int i = 1; i <= lenght; i++){
+        System.out.print(ch);
+      }
+      System.out.println();
+      floor++;
+      spa--;
+      lenght +=2;
     }
-    // carácteres de la pirámide
-    for(){
-
-    }
-    
   }
 }
