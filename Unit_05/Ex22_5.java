01a1ad7d2dbb13f655f810f827e9b90073c25586
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-11 13:32:04 +0100

    Loops

--- Unit_05/Ex22_5.java
+++ Unit_05/Ex22_5.java
@@ -5,27 +5,12 @@
  */
 public class Ex22_5 {
   public static void main(String[] args) {
-  //   int n = 100;
-  //   System.out.println("Los números primos entre 2 y 100 son: ");
-  //   do {
-  //     int r = n;
-  //     n--;
-  //     if (r % n != 0) {
-  //       System.out.print(n + " ");
-  //     }
-  //   } while (n >= 2);
-  int n = 100;
-  int c = n - 1;
-  boolean nPrim = true;
-  System.out.println("Los números primos entre 2 y 100 son: ");
-  do {
-    if ((n % c) == 0) {
-      nPrim = false;
-    }
-    c--;
-  } while ((c > 2)&&(nPrim == true));
-  if ((nPrim == true)||(n <= 2)) {
-    System.out.println(n + " es un número primo.");
-  }
-  }
-}
+    int n = 100;
+    System.out.println("Los números primos entre 2 y 100 son: ");
+    do {
+      int r = n;
+      n--;
+      if (r % n != 0) {
+        System.out.print(n + " ");
+      }
+    } while (n >= 2);
