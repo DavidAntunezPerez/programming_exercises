f07b5fa1ec535b29725b5788e931c233172e4b8b
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-12 08:39:30 +0100

    Loops

--- Unit_05/Ex23_5.java
+++ Unit_05/Ex23_5.java
@@ -1,4 +1,9 @@
 /**
+ * Escribe un programa que permita ir introduciendo una serie indeterminada
+de números mientras su suma no supere el valor 10000. Cuando esto último
+ocurra, se debe mostrar el total acumulado, el contador de los números
+introducidos y la media.
+
  * @author David Antunez Perez
  */
 public class Ex23_5 {
