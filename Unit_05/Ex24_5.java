6ad146ea0ddc222b0792afd261ba3c1eef1d11f4
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-13 22:46:42 +0100

    Loops

---
+++ Unit_05/Ex24_5.java
@@ -0,0 +1,11 @@
+/**
+ * Escribe un programa que lea un número n e imprima una pirámide de números con
+ * n filas como en la siguiente figura:
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex24_5 {
+  public static void main(String[] args) {
+    
+  }
+}
