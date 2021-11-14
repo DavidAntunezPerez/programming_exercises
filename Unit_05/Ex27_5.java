31ce6c139af0ed2a888481ed971b37f93c79bc96
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-14 23:22:17 +0100

    Loops

--- Unit_05/Ex27_5.java
+++ Unit_05/Ex27_5.java
@@ -1,6 +1,7 @@
 /**
  * @author David Antunez Perez
  * 
+ * 
  *         Escribe un programa que muestre, cuente y sume los múltiplos de 3 que
  *         hay entre 1 y un número leído por teclado.
  * 
