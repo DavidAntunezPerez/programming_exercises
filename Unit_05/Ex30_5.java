4dd0f1a44b38a16a48150cc4baff51952947d345
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 10:01:25 +0100

    Loops

---
+++ Unit_05/Ex30_5.java
@@ -0,0 +1,15 @@
+/**
+ * @author David Antunez Perez
+ * 
+ *         Realiza una programa que calcule las horas transcurridas entre dos
+ *         horas de dos días de la semana. No se tendrán en cuenta los minutos
+ *         ni los segundos. El día de la semana se puede pedir como un número
+ *         (del 1 al 7) o como una cadena (de “lunes” a “domingo”). Se debe
+ *         comprobar que el usuario introduce los datos correctamente y que el
+ *         segundo día es posterior al primero.
+ */
+public class Ex30_5 {
+  public static void main(String[] args) {
+
+  }
+}
