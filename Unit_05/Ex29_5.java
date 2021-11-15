62ff58fce8656dfb15d7b8da59c0ad3198041964
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 09:02:30 +0100

    Loops

---
+++ Unit_05/Ex29_5.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author David Antunez Perez
+ * 
+ *         Escribe un programa que muestre por pantalla todos los números
+ *         enteros positivos menores a uno leído por teclado que no sean
+ *         divisibles entre otro también leído de igual forma.
+ */
+public class Ex29_5 {
+  public static void main(String[] args) {
+    
+  }
+}
