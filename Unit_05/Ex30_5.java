df51d78da927cfaf031744e12deb5d9e02cd9ef0
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 10:16:20 +0100

    Loops

--- Unit_05/Ex30_5.java
+++ Unit_05/Ex30_5.java
@@ -10,6 +10,11 @@
  */
 public class Ex30_5 {
   public static void main(String[] args) {
-
+    System.out.printf("Por favor, introduzca la primera hora.\n");
+    System.out.print("Dia: ");
+    String day = System.console().readLine();
+    System.out.print("Hora: ");
+    int hour = Integer.parseInt(System.console().readLine());
+    
   }
 }
