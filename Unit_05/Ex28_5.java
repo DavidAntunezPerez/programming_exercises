8d13960bff2deab6d009531727ed53f2ebcf8711
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 08:40:29 +0100

    Loops

--- Unit_05/Ex28_5.java
+++ Unit_05/Ex28_5.java
@@ -1,3 +1,9 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ *         Escribe un programa que calcule el factorial de un número entero
+ *         leído por teclado.
+ */
 public class Ex28_5 {
   public static void main(String[] args) {
     
