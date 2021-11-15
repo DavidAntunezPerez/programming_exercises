23b934c74db862f93059c8000b0f2cc68a91cdcb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 09:00:27 +0100

    Loops

--- Unit_05/Ex28_5.java
+++ Unit_05/Ex28_5.java
@@ -6,13 +6,20 @@
  */
 public class Ex28_5 {
   public static void main(String[] args) {
-    System.out.print("Introduzca un número entero: ");
-    int n = Integer.parseInt(System.console().readLine());
+    int n;
+    do {
+      System.out.print("Introduzca un número entero: ");
+      n = Integer.parseInt(System.console().readLine());
+      if ( n < 0){
+        System.out.print("ERROR. El número debe ser entero.");
+      }
+    } while (n < 0);
+
     int result = 1;
-    int add = 1;
+    int add;
     for (int i = n; i > 0; i--) {
-      add = n * i;
-      result += add;
+      add = i;
+      result *= add;
     }
     System.out.print(n + "! = " + result);
   }
