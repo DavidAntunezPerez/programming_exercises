ea9c0673bcf6a47fe69fcd56cca08ab4d4907905
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 09:04:29 +0100

    Loops

--- Unit_05/Ex29_5.java
+++ Unit_05/Ex29_5.java
@@ -8,6 +8,13 @@
  */
 public class Ex29_5 {
   public static void main(String[] args) {
-    
+    int n;
+    do {
+      System.out.print("Introduce un número positivo: ");
+      n = Integer.parseInt(System.console().readLine());
+      if ( n < 0){
+        System.out.println("ERROR. El número debe ser positivo.");
+      }
+    } while (n < 0);
   }
 }
