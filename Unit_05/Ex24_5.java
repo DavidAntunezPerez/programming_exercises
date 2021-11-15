44e4decc25448225a24098ad5fd36c916c1aff37
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-15 08:39:26 +0100

    Loops

--- Unit_05/Ex24_5.java
+++ Unit_05/Ex24_5.java
@@ -6,8 +6,26 @@
  */
 public class Ex24_5 {
   public static void main(String[] args) {
-    System.out.print("Selecciona la altura de la pirámide: ");
+    System.out.print("Indica la altura de la pirámide: ");
     int h = Integer.parseInt(System.console().readLine());
-
+    int ch = 1;
+    int floor = 1;
+    int lenght = 1;
+    int spa = h - 1;
+    while (floor <= h) {
+      // espacios de la pirámide
+      for (int i = 1; i <= spa; i++) {
+        System.out.print(" ");
+      }
+      // carácteres de la pirámide
+      for (int i = 1; i <= lenght; i++) {
+        System.out.print(ch);
+        ch++;
+      }
+      System.out.println();
+      floor++;
+      spa--;
+      lenght += 2;
+    }
   }
 }
