e306a2ec7c4cbf1329c462a3e2b1d4f27d6da681
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 10:07:26 +0100

    Loops

---
+++ Unit_05/Ex33_5.java
@@ -0,0 +1,17 @@
+/**
+ * Realiza un programa que pinte la letra U por pantalla hecha con asteriscos.
+ * El programa pedirá la altura. Fíjate que el programa inserta un espacio y
+ * pinta dos asteriscos menos en la base para simular la curvatura de las
+ * esquinas inferiores.
+ * 
+ * @author David Antúnez Pérez
+ */
+public class Ex33_5 {
+  public static void main(String[] args) {
+    System.out.print("Introduzca la altura de la U: ");
+    int h = Integer.parseInt(System.console().readLine());
+    for (;;) {
+
+    }
+  }
+}
