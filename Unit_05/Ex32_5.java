885be6e09f17db3f4f1f0d308bc9a3173e12c994
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 09:58:23 +0100

    Loops

--- Unit_05/Ex32_5.java
+++ Unit_05/Ex32_5.java
@@ -9,8 +9,22 @@
 public class Ex32_5 {
   public static void main(String[] args) {
     System.out.printf("Introduzca un número entero positivo: \n");
-    int n = Integer.parseInt(System.console().readLine());
-    System.out.printf("Dígitos pares: \n");
+    int numInt = Integer.parseInt(System.console().readLine());
+
+    int cifra = numInt;
+    int cogNum = 0;
+    int contDigit = 0;
+        
+    for ( int h = 1; h <= numInt; h *= 10 ) {
+      contDigit++;
+    }
+        System.out.print("Dígitos pares: ");
+    for ( int i = 1; i <= contDigit; i++ ) {
+      int potencia10 = (int)(Math.pow(10,(contDigit-i)));
+      cogNum = cifra / potencia10;
+      System.out.print(cogNum+" ");
+      cifra = cifra - (cogNum*potencia10);
+    }
     System.out.printf("Suma de dígitos pares: \n");
 
   }
