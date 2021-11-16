50effe649a97496283ee9734fb29db599c97c79e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 09:23:51 +0100

    Loops

---
+++ Ex32_5.java
@@ -0,0 +1,13 @@
+/**
+ * Escribe un programa que, dado un número entero positivo, diga cuáles son y
+ * cuánto suman los dígitos pares. Los dígitos pares se deben mostrar en orden,
+ * de izquierda a derecha. Usa long en lugar de int donde sea necesario para
+ * admitir números largos.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex32_5 {
+  public static void main(String[] args) {
+
+  }
+}
