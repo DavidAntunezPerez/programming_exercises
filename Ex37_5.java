8160a3fdb72ac1895ed908fb57b60bdf90087ac8
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 10:54:34 +0100

    Loops

---
+++ Ex37_5.java
@@ -0,0 +1,10 @@
+/**
+ * Realiza un conversor del sistema decimal al sistema de “palotes”.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex37_5 {
+  public static void main(String[] args) {
+    
+  }
+}
