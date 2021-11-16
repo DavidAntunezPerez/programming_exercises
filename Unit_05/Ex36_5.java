1ed546cf090ced5d3d7f68bbe94a2a49641065c0
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 10:50:14 +0100

    Loops

---
+++ Unit_05/Ex36_5.java
@@ -0,0 +1,14 @@
+/**
+ * Escribe un programa que diga si un número introducido por teclado es o no
+ * capicúa. Los números capicúa se leen igual hacia delante y hacia atrás. El
+ * programa debe aceptar números de cualquier longitud siempre que lo permita el
+ * tipo, en caso contrario el ejercicio no se dará por bueno. Se recomienda usar
+ * long en lugar de int ya que el primero admite números más largos.
+ * 
+ * @author David Antúnez Pérez
+ */
+public class Ex36_5 {
+  public static void main(String[] args) {
+    
+  }
+}
