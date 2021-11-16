825173e4f92d08911673e239dcf173c7a87d31af
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 10:49:18 +0100

    Loops

--- Unit_05/Ex35_5.java
+++ Unit_05/Ex35_5.java
@@ -1,5 +1,12 @@
+/**
+ * Realiza un programa que pinte una X hecha de asteriscos. El programa debe
+ * pedir la altura. Se debe comprobar que la altura sea un número impar mayor o
+ * igual a 3, en caso contrario se debe mostrar un mensaje de error.
+ * 
+ * @author David Antunez Perez
+ */
 public class Ex35_5 {
- public static void main(String[] args) {
-   
- } 
+  public static void main(String[] args) {
+
+  }
 }
