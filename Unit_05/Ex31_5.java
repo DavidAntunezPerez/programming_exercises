82c3a58c1956f4ef8a3df077f7ce7b5f840c19cc
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-16 09:14:26 +0100

    Loops

--- Unit_05/Ex31_5.java
+++ Unit_05/Ex31_5.java
@@ -1,5 +1,12 @@
+/**
+ * Realiza un programa que pinte la letra L por pantalla hecha con asteriscos.
+ * El programa pedirá la altura. El palo horizontal de la L tendrá una longitud
+ * de la mitad (división entera entre 2) de la altura más uno.
+ * 
+ * @author David Antunez Perez
+ */
 public class Ex31_5 {
   public static void main(String[] args) {
-    
+
   }
 }
