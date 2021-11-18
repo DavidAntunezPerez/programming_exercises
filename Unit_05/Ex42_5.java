efffea5c1a2580f9c0f7ab3bf2e029f63cacfdff
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 09:08:45 +0100

    Loops

---
+++ Unit_05/Ex42_5.java
@@ -0,0 +1,13 @@
+/**
+ * Escribe un programa que pida un número entero positivo por teclado y que
+ * muestre a continuación los 5 números consecutivos a partir del número
+ * introducido. Al lado de cada número se debe indicar si se trata de un primo o
+ * no.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex42_5 {
+  public static void main(String[] args) {
+    
+  }
+}
