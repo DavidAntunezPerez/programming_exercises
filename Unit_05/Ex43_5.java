21ab21ac2516026e6e96e7c15b8f109073ec5b59
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 09:35:16 +0100

    Loops

---
+++ Unit_05/Ex43_5.java
@@ -0,0 +1,17 @@
+/**
+ * Escribe un programa que permita partir un número introducido por teclado en
+ * dos partes. Las posiciones se cuentan de izquierda a derecha empezando por el
+ * 1. Suponemos que el usuario introduce correctamente los datos, es decir, el
+ * número introducido tiene dos dígitos como mínimo y la posición en la que se
+ * parte el número está entre 2 y la longitud del número. No se permite en este
+ * ejercicio el uso de funciones de manejo de String (por ej. para extraer
+ * subcadenas dentro de una cadena).
+ * 
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex43_5 {
+  public static void main(String[] args) {
+
+  }
+}
