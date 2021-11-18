efae13922ca19ec2e52996011250810635b7137a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 09:33:53 +0100

    Loops

--- Unit_05/Ex42_5.java
+++ Unit_05/Ex42_5.java
@@ -8,6 +8,26 @@
  */
 public class Ex42_5 {
   public static void main(String[] args) {
-    
+    System.out.print("Introduce un número entero positivo: ");
+    int numInt = Math.abs(Integer.parseInt(System.console().readLine())); // Número Introducido
+    int contDivisores = 2; // No puede ser 1
+    boolean pregPrimo = true; // Presupone que el número es primo
+    for (int i = numInt; i <= numInt + 5; i++) {
+      pregPrimo = true;
+      do {
+        if ((i % contDivisores == 0) && (i != contDivisores)) {
+          pregPrimo = false; // Si encuentra un divisor: Ya no es primo
+        }
+        contDivisores++;
+      } while ((contDivisores < i) && (pregPrimo == true));
+      // Es menor que "<" porque no puede ser el mismo número
+      contDivisores = 2;
+      if (pregPrimo == true) {
+        System.out.print(i + " es primo\n");
+      } 
+      else if (pregPrimo == false) {
+        System.out.print(i + " no es primo\n");
+      }
+    }
   }
 }
