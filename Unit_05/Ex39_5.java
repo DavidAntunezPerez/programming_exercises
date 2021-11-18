239f14fe6bd062d52d6e849f71eda0b1734f5a01
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 08:33:25 +0100

    Loops

---
+++ Unit_05/Ex39_5.java
@@ -0,0 +1,13 @@
+/**
+ * Escribe un programa que pida un número entero positivo por teclado y que
+ * muestre a continuación los números desde el 1 al número introducido junto con
+ * su factorial.
+ * 
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex39_5 {
+  public static void main(String[] args) {
+
+  }
+}
