109b6a9eb010d396b309d851cab62f477bd07bdb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 10:50:57 +0100

    Random Numbers

--- Unit_06/Practice/RandomFigure.java
+++ Unit_06/Practice/RandomFigure.java
@@ -1,5 +1,24 @@
 public class RandomFigure {
   public static void main(String[] args) {
-    
+    int figNum;
+    for (int i = 0; i <= 20; i++) {
+      figNum = (int) (Math.random() * 4 + 1);
+      switch (figNum) {
+      case 1:
+        System.out.print("_");
+        break;
+      case 2:
+        System.out.print("💩");
+        break;
+      case 3:
+        System.out.print("🤬");
+        break;
+      case 4:
+        System.out.print("🤟");
+        break;
+      default:
+        System.out.print("Introduce un valor válido.");
+      }
+    }
   }
 }
