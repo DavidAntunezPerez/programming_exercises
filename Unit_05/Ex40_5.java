83b7f610c9916a62f9805a63512d8afcee3e9d59
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 08:49:30 +0100

    Loops

---
+++ Unit_05/Ex40_5.java
@@ -0,0 +1,13 @@
+/**
+ * Realiza un programa que pinte por pantalla un rombo hueco hecho con
+ * asteriscos. El programa debe pedir la altura. Se debe comprobar que la altura
+ * sea un número impar mayor o igual a 3, en caso contrario se debe mostrar un
+ * mensaje de error.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex40_5 {
+  public static void main(String[] args) {
+
+  }
+}
