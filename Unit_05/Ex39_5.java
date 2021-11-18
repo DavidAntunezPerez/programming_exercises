b1f91ddd797eaa8c56de0b7fbcc06af126d29f7c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 08:43:19 +0100

    Loops

--- Unit_05/Ex39_5.java
+++ Unit_05/Ex39_5.java
@@ -8,6 +8,21 @@
  */
 public class Ex39_5 {
   public static void main(String[] args) {
-
+    int n;
+    do {
+      System.out.print("Introduzca un número entero: ");
+      n = Integer.parseInt(System.console().readLine());
+      if (n < 0) {
+        System.out.print("ERROR. El número debe ser entero.");
+      }
+    } while (n < 0);
+    System.out.print("Del 1 al " + n + " , los factoriales son: ");
+    int result = 1;
+    int add;
+    for (int i = n; i > 0; i--) {
+      System.out.print(i + "! = " + result+ ", ");
+      add = i;
+      result *= add;
+    }
   }
 }
