5d1a26e383d308cdb76200c78c252be20ea6ed6a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 08:57:31 +0100

    Loops

--- Unit_05/Ex41_5.java
+++ Unit_05/Ex41_5.java
@@ -7,6 +7,24 @@
  */
 public class Ex41_5 {
   public static void main(String[] args) {
-    
+    // Separa número del último al primero
+    System.out.print("Por favor, introduzca número: ");
+    int numInt = Integer.parseInt(System.console().readLine());
+    int cifra = numInt;
+    int cogNum = 0;
+    int contPos = 0;
+    int contNeg = 0;
+    for ( int i = 1; i <= numInt; i *= 10 ) {
+      cogNum = cifra % 10;
+      System.out.print(cogNum+"\n");
+      if(cogNum % 2 == 0){
+        contPos++;
+      }
+      if(cogNum % 2 != 0){
+        contNeg++;
+      }
+      cifra = (cifra - cogNum)/10;
+    }
+    System.out.printf("En el número "+numInt+" , hay "+contPos+" números positivos y "+contNeg+" números negativos.");
   }
 }
