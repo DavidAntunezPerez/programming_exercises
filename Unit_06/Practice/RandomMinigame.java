88768d859a460470c676ff2878f7425b607c3f79
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 10:44:46 +0100

    Random Numbers

--- Unit_06/Practice/RandomMinigame.java
+++ Unit_06/Practice/RandomMinigame.java
@@ -1,17 +1,17 @@
 public class RandomMinigame {
   public static void main(String[] args) {
-    int figNum = (int)(Math.random()*3);
-    switch(figNum){
-      case 0:
+    int figNum = (int) (Math.random() * 3+1);
+    switch (figNum) {
+    case 1:
       System.out.print("Piedra");
       break;
-      case 1:
+    case 2:
       System.out.print("Papel");
       break;
-      case 2:
+    case 3:
       System.out.print("Tijeras");
       break;
-      default:
+    default:
       System.out.print("Introduce un valor válido.");
     }
   }
