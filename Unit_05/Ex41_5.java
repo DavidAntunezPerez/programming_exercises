a4d8b0d0338279c8920f113b7fff7386e8d2eddd
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-18 08:50:17 +0100

    Loops

---
+++ Unit_05/Ex41_5.java
@@ -0,0 +1,12 @@
+/**
+ * Escribe un programa que diga cuántos dígitos pares y cuántos dígitos impares
+ * hay dentro de un número. Se recomienda usar long en lugar de int ya que el
+ * primero admite números más largos.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex41_5 {
+  public static void main(String[] args) {
+
+  }
+}
