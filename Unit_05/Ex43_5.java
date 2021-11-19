66e6d0a2831383ff0e7e6b29e901e5221ee0a666
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-19 16:47:21 +0100

    Loops

--- Unit_05/Ex43_5.java
+++ Unit_05/Ex43_5.java
@@ -15,6 +15,8 @@ public class Ex43_5 {
     // Separa número del último al primero
     System.out.print("Por favor, introduzca número: ");
     int numInt = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca la posición desde la que partir el número: ");
+    int pos = Integer.parseInt(System.console().readLine());
 
     int cifra = numInt;
     int cogNum = 0;
