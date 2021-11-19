dbfdd0bb8e4af524c2cf161065a70f38946324b8
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-19 11:10:40 +0100

    Loops

--- Unit_05/Ex43_5.java
+++ Unit_05/Ex43_5.java
@@ -12,7 +12,23 @@
  */
 public class Ex43_5 {
   public static void main(String[] args) {
-    System.out.print("Introduce un número");
-    int n = Integer.parseInt(System.console().readLine());
+    // Separa número del último al primero
+    System.out.print("Por favor, introduzca número: ");
+    int numInt = Integer.parseInt(System.console().readLine());
+
+    int cifra = numInt;
+    int cogNum = 0;
+    int contDigit = 0;
+
+    for (int h = 1; h <= numInt; h *= 10) {
+      contDigit++;
+    }
+
+    for (int i = 1; i <= contDigit; i++) {
+      int potencia10 = (int) (Math.pow(10, (contDigit - i)));
+      cogNum = cifra / potencia10;
+      System.out.print(cogNum + "\n");
+      cifra = cifra - (cogNum * potencia10);
+    }
   }
 }
