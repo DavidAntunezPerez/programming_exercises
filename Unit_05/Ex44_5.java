512f1f5905f15c278f2196e41dc5ce35319cb1b6
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-20 15:34:28 +0100

    Loops

--- Unit_05/Ex44_5.java
+++ Unit_05/Ex44_5.java
@@ -1,3 +1,17 @@
+/**
+ * 
+ * Escribe un programa que sea capaz de insertar un dígito dentro de un número
+ * indicando la posición. El nuevo dígito se colocará en la posición indicada y
+ * el resto de dígitos se desplazará hacia la derecha. Las posiciones se cuentan
+ * de izquierda a derecha empezando por el 1. Suponemos que el usuario introduce
+ * correctamente los datos. Se recomienda usar long en lugar de int ya que el
+ * primero admite números más largos.
+ * 
+ * @author David Antunez Perez
+ * 
+ */
 public class Ex44_5 {
-  
+  public static void main(String[] args) {
+
+  }
 }
