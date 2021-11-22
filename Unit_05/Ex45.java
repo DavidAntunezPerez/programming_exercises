45af42f2e3433b056db97fff0607eeae093e53b6
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 08:45:16 +0100

    Loops

--- Unit_05/Ex45.java
+++ Unit_05/Ex45.java
@@ -10,6 +10,24 @@
  */
 public class Ex45 {
   public static void main(String[] args) {
-    
+    System.out.print("Introduzca un número entero positivo: ");
+    int nIntr = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca el dígito que quieres insertar: ");
+    int digit = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca la posicion donde quieres insertar: ");
+    int pos = Integer.parseInt(System.console().readLine());
+    int n = nIntr;
+    // contar digitos
+    int contDig = 0;
+    do {
+      n /= 10;
+      contDig++;
+    } while (n > 0);
+    // num a la izquierda del dígito
+    int left = nIntr / (int) (Math.pow(10, contDig - pos));
+    // num a la derecha del dígito
+    int right = nIntr % (int) (Math.pow(10, contDig - pos));
+    // mostramos resultado:
+    System.out.print(left + "" + digit + "" + right + "");
   }
 }
