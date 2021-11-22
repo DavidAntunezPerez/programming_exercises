ea2738793dbb2a5fcf050ed5af96d7d80bad751f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 10:06:07 +0100

    Loops

--- Unit_05/Ex49_5.java
+++ Unit_05/Ex49_5.java
@@ -9,6 +9,20 @@
  */
 public class Ex49_5 {
   public static void main(String[] args) {
-    
+    System.out.print("Por favor, introduzca varios números enteros, para terminar, introduzca un número primo: ");
+    int contDivisores = 2; // No puede ser 1
+    boolean pregPrimo = true; // Presupone que el número es primo
+    do {
+      int numInt = Integer.parseInt(System.console().readLine());
+      //comprobación de número primo
+      pregPrimo = true;
+      do {
+        pregPrimo = true;
+        if (numInt % contDivisores == 0 && numInt != contDivisores) {
+          pregPrimo = false; // Si encuentra un divisor: Ya no es primo
+        }
+        contDivisores++;
+      } while (contDivisores < numInt && pregPrimo == true);
+    } while (pregPrimo == false);
   }
 }
