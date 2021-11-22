07115df93c4f9e580f0ea92e7326a90c877ef452
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 08:53:52 +0100

    Loops

---
+++ Unit_05/Ex47_5.java
@@ -0,0 +1,15 @@
+/**
+ * Con motivo de la celebración del día de la mujer, el 8 de marzo, nos han
+ * encargado realizar un programa que pinte un 8 por pantalla usando la letra M.
+ * Se pide al usuario la altura, que debe ser un número entero impar mayor o
+ * igual que 5. Si el número introducido no es correcto, el programa deberá
+ * mostrar un mensaje de error. A continuación se muestran algunos ejemplos. La
+ * anchura de la figura siempre será de 6 caracteres.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex47_5 {
+  public static void main(String[] args) {
+
+  }
+}
