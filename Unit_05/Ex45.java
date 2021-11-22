c545bc6756ecef8ce8fc7dba51ee9dd6b9730e26
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 08:40:55 +0100

    Loops

--- Unit_05/Ex45.java
+++ Unit_05/Ex45.java
@@ -1,9 +1,15 @@
 /**
+ * Escribe un programa que cambie un dígito dentro de un número dando la
+ * posición y el valor nuevo. Las posiciones se cuentan de izquierda a derecha
+ * empezando por el 1. Se recomienda usar long en lugar de int ya que el primero
+ * admite números más largos. Suponemos que el usuario introduce correctamente
+ * los datos.
+ * 
  * 
  * @author David Antunez Perez
  */
 public class Ex45 {
   public static void main(String[] args) {
-    
+
   }
 }
