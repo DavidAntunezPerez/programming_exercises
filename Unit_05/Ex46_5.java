930f083d0782a69441524b1ef1454b64b9a26f69
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 08:51:42 +0100

    Loops

---
+++ Unit_05/Ex46_5.java
@@ -0,0 +1,14 @@
+/**
+ * Realiza un programa que pinte por pantalla un rectángulo hueco hecho con
+ * asteriscos. Se debe pedir al usuario la anchura y la altura. Hay que
+ * comprobar que tanto la anchura como la altura sean mayores o iguales que 2,
+ * en caso contrario se debe mostrar un mensaje de error.
+ * 
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex46_5 {
+  public static void main(String[] args) {
+    
+  }
+}
