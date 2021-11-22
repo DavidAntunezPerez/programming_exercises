f8567b3728e6a60faa1fbb768760bcef84726697
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 08:54:48 +0100

    Loops

---
+++ Unit_05/Ex48_5.java
@@ -0,0 +1,13 @@
+/**
+ * Realiza un programa que diga los dígitos que aparecen y los que no aparecen
+ * en un número entero introducido por teclado. El orden es el que se muestra en
+ * los ejemplos. Utiliza el tipo long para que el usuario pueda introducir
+ * números largos.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex48_5 {
+  public static void main(String[] args) {
+
+  }
+}
