1d22e0a840155243a0bbf76b2bc3db756a6df095
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 09:02:29 +0100

    Loops

--- Unit_05/Ex48_5.java
+++ Unit_05/Ex48_5.java
@@ -9,8 +9,17 @@
 public class Ex48_5 {
   public static void main(String[] args) {
     System.out.print("Introduzca un número entero: ");
-    int n = Integer.parseInt(System.console().readLine());
+    int numInt = Integer.parseInt(System.console().readLine());
+    // contamos los dígitos
+    int n = numInt;
+    int contDig = 1;
+    do {
+      n /= 10;
+      contDig++;
+    } while (n > 0);
+    
     System.out.print("Dígitos que aparecen en el número: ");
+
     System.out.print("Dígitos que no aparecen: ");
   }
 }
