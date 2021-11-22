695e584f7852fb5a89ba6843d21dfcc712102b75
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-22 09:56:42 +0100

    Loops

--- Unit_05/Ex49_5.java
+++ Unit_05/Ex49_5.java
@@ -1,3 +1,12 @@
+/**
+ * Realiza un programa que calcule el máximo, el mínimo y la media de una serie
+ * de números enteros positivos introducidos por teclado. El programa terminará
+ * cuando el usuario introduzca un número primo. Este último número no se tendrá
+ * en cuenta en los cálculos. El programa debe indicar también cuántos números
+ * ha introducido el usuario (sin contar el primo que sirve para salir).
+ * 
+ * @author David Antunez Perez
+ */
 public class Ex49_5 {
   public static void main(String[] args) {
     
