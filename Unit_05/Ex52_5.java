ce909c20b8fd6f2339465c095a4c03b0b9c5396a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-23 09:10:29 +0100

    Loops

---
+++ Unit_05/Ex52_5.java
@@ -0,0 +1,13 @@
+/**
+ * Realiza un programa que sea capaz de desplazar todos los dígitos de un número
+ * de derecha a izquierda una posición. El dígito de más a la izquierda, pasaría
+ * a dar la vuelta y se colocaría a la derecha. Si el número tiene un solo
+ * dígito, se queda igual.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex52_5 {
+  public static void main(String[] args) {
+
+  }
+}
