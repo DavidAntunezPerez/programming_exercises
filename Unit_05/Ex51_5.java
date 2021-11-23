f44f1d5f6fa1b61a65619f217c5d64ec014d4114
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-23 08:46:34 +0100

    Loops

---
+++ Unit_05/Ex51_5.java
@@ -0,0 +1,14 @@
+/**
+ * El gusano numérico se come los dígitos con forma de rosquilla, o sea, el 0 y
+ * el 8 (todos los que encuentre). Realiza un programa que muestre un número
+ * antes y después de haber sido comido por el gusano. Si el animalito no se ha
+ * comido ningún dígito, el programa debe indicarlo.
+ * 
+ * @author David Antunez Perez
+ * 
+ */
+public class Ex51_5 {
+  public static void main(String[] args) {
+
+  }
+}
