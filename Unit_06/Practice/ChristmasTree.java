00914d8cc35b902ad4f5e7cd1cabeea3666bb4dd
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-23 10:12:30 +0100

    Random Numbers

--- Unit_06/Practice/ChristmasTree.java
+++ Unit_06/Practice/ChristmasTree.java
@@ -3,10 +3,21 @@ public class ChristmasTree {
     System.out.print("Este programa pinta un árbol de Navidad.");
     System.out.print("Por favor, introduzca la altura: ");
     int h = Integer.parseInt(System.console().readLine());
+    int spa = h - 1;
+    int floor = 1;
     System.out.print("✵\n");
+    while(floor <= h - 1){
+    // espacios externos
+    for (int i = 1; i <= spa; i++) {
+      System.out.print(" ");
+    }
+    // hojas del árbol
     for (int i = h - 3; i > 0; i--) {
       System.out.print("/\n");
     }
+    floor++;
+    spa--;
+  }
     System.out.print("#\n#");
   }
 }
