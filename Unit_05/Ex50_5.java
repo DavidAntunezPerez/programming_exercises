97513294641172a56ff6c246167372f2668d008c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-23 08:26:50 +0100

    Loops

--- Unit_05/Ex50_5.java
+++ Unit_05/Ex50_5.java
@@ -1,9 +1,17 @@
 /**
+ * Una empresa de cartelería nos ha encargado un programa para realizar uno de
+ * sus diseños. Debido a los acontecimientos que han tenido lugar en Cataluña
+ * durante el 2018, han recibido muchos pedidos del cartel que muestra el número
+ * 155. Realiza un programa que pinte el número 155 mediante asteriscos. Al
+ * usuario se le pedirán dos datos, la altura del cartel y el número de espacios
+ * que habrá entre los números. La altura mínima es 5. La anchura de los números
+ * siempre es la misma. La parte superior de los cincos también es siempre
+ * igual. La parte inferior del 5 sí que varía en función de la altura.
  * 
  * @author David Antunez Perez
  */
 public class Ex50_5 {
   public static void main(String[] args) {
-    
+
   }
 }
