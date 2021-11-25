269dcff65b84e940d3e4dfa73388add7f8772575
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-25 11:03:22 +0100

    Random Numbers

---
+++ Unit_06/Ex12_6.java
@@ -0,0 +1,12 @@
+/**
+ * Realiza un programa que llene la pantalla de caracteres aleatorios (a lo
+ * Matrix) con el código ascii entre el 32 y el 126. Puedes hacer casting con
+ * (char) para convertir un entero en un carácter.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex12_6 {
+  public static void main(String[] args) {
+    
+  }
+}
