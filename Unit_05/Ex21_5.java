4203d444a9357a5adfbe427a13bbccad43de1391
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-25 09:55:47 +0100

    Loops

--- Unit_05/Ex21_5.java
+++ Unit_05/Ex21_5.java
@@ -12,6 +12,7 @@ public class Ex21_5 {
     int cont = 0; // Número de números introducidos.
     int imp = -1; // Número de números impares
     int par = 0; // Mayor número par
+    int add = 0; // suma de impares
     while (i > 0) {
       System.out.print("Escriba un número: ");
       i = Integer.parseInt(System.console().readLine());
@@ -20,7 +21,8 @@ public class Ex21_5 {
         System.out.println("(Para finalizar la introducción de datos escriba un número negativo o 0)");
       }
       // contador de impar
-      if (i % 2 != 0) {
+      if ((i % 2 != 0) && (i >= 0)) {
+        add += i;
         imp++;
       }
       // detectar mayor número par
@@ -29,7 +31,8 @@ public class Ex21_5 {
       }
 
     }
+    int avImp = add / (imp + 1); // media de los impares
     System.out.printf("Se han introducido " + cont + " números.\nDe los cuales " + imp + " son impares y " + par
-        + " es el mayor número par.");
+        + " es el mayor número par. La media de los impares es " + avImp);
   }
 }
