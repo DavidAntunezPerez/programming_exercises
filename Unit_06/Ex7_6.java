254dea535195fbadb30b465433511ebd27c0d86f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-25 09:59:36 +0100

    Random Numbers

--- Unit_06/Ex7_6.java
+++ Unit_06/Ex7_6.java
@@ -1,8 +1,11 @@
 /**
+ * Escribe un programa que muestre tres apuestas de la quiniela en tres columnas
+ * para los 14 partidos y el pleno al quince (15 filas).
+ * 
  * @author David Antunez Perez
  */
 public class Ex7_6 {
   public static void main(String[] args) {
-    
+
   }
 }
