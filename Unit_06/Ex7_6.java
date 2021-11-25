de2ee2406209b1fb6d21c1d12a734a51e4fb5302
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-25 10:12:51 +0100

    Random Numbers

--- Unit_06/Ex7_6.java
+++ Unit_06/Ex7_6.java
@@ -6,22 +6,50 @@
  */
 public class Ex7_6 {
   public static void main(String[] args) {
-    String result = "?  ";
-    int n;
-    for (int i = 0; i > 14; i++) {
-      n = (int) (Math.random() * 3);
-      switch (n) {
+    String result1 = "?  ";
+    String result2 = "?  ";
+    String result3 = "?  ";
+    int n1;
+    int n2;
+    int n3;
+    for (int i = 0; i < 14; i++) {
+      n1 = (int) (Math.random() * 3);
+      switch (n1) {
       case 0:
-        result = "1  ";
+        result1 = "1  ";
         break;
       case 1:
-        result = " X ";
+        result1 = " X ";
         break;
       case 2:
-        result = "  2";
+        result1 = "  2";
         break;
       }
-      System.out.println("|" + result + "|" + result + "|" + result + "|");
+      n2 = (int) (Math.random() * 3);
+      switch (n2) {
+      case 0:
+        result2 = "1  ";
+        break;
+      case 1:
+        result2 = " X ";
+        break;
+      case 2:
+        result2 = "  2";
+        break;
+      }
+      n3 = (int) (Math.random() * 3);
+      switch (n3) {
+      case 0:
+        result3 = "1  ";
+        break;
+      case 1:
+        result3 = " X ";
+        break;
+      case 2:
+        result3 = "  2";
+        break;
+      }
+      System.out.println("|" + result1 + "|" + result2 + "|" + result3 + "|");
     }
   }
 }
