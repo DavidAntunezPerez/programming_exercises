eea0ca0fe4c1a562e1ccc02d5895ee4027c3e45e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-25 08:41:40 +0100

    Random Numbers

--- Unit_06/Ex1_6.java
+++ Unit_06/Ex1_6.java
@@ -1,5 +1,13 @@
+/**
+ * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+ * también la suma total (los puntos que suman entre los tres dados).
+ * 
+ * @author David Antunez Perez
+ */
 public class Ex1_6 {
   public static void main(String[] args) {
-    
+    for (int i = 0; i < 3; i++) {
+      System.out.print((int) (Math.random() * 6) + " ");
+    }
   }
 }
