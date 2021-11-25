2fd4bcbf81ad131878e354e91b0c45103dcdfbdc
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-25 09:00:13 +0100

    Random Numbers

---
+++ Unit_06/Ex2_6.java
@@ -0,0 +1,36 @@
+/**
+ * Realiza un programa que muestre al azar el nombre de una carta de la baraja
+ * francesa. Esta baraja está dividida en cuatro palos: picas, corazones,
+ * diamantes y tréboles. Cada palo está formado por 13 cartas, de las cuales 9
+ * cartas son numerales y 4 literales: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K y A
+ * (que sería el 1). Para convertir un número en una cadena de caracteres
+ * podemos usar String.valueOf(n).
+ * 
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex2_6 {
+  public static void main(String[] args) {
+    String number = "?";
+    int n = (int) (Math.random() * 13 + 2);
+    switch (n) {
+    case 11:
+      number = "J";
+      break;
+    case 12:
+      number = "Q";
+      break;
+    case 13:
+      number = "K";
+      break;
+    case 14:
+      number = "A";
+    }
+
+    if (n < 11) {
+      System.out.print("La carta obtenida es el " + n + "\n");
+    } else if (n >= 11) {
+      System.out.print("La carta obtenida es el " + number + "\n");
+    }
+  }
+}
