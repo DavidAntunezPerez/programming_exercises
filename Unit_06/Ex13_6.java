9af7cf2ca9addd4259d7eb56d64daacca4a5f325
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-26 16:26:31 +0100

    Random Numbers

--- Unit_06/Ex13_6.java
+++ Unit_06/Ex13_6.java
@@ -1,9 +1,12 @@
 /**
+ * Escribe un programa que simule la tirada de dos dados. El programa deberá
+ * continuar tirando los dados una y otra vez hasta que en alguna tirada los dos
+ * dados tengan el mismo valor
  * 
  * @author David Antunez Perez
  */
 public class Ex13_6 {
   public static void main(String[] args) {
-    
+
   }
 }
