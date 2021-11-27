56e4ae23dc5d76ab1fe19e0daf9ce9186356a465
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-27 12:38:36 +0100

    Random Numbers

--- Unit_06/Ex6_6.java
+++ Unit_06/Ex6_6.java
@@ -1,6 +1,6 @@
 /**
  * Escribe un programa que piense un número al azar entre 0 y 100. El usuario
- * decrbe adivinarlo y tiene para ello 5 oportunidades. Después de cada intento
+ * debe adivinarlo y tiene para ello 5 oportunidades. Después de cada intento
  * fallido, el programa dirá cuántas oportunidades quedan y si el número
  * introducido es menor o mayor que el número secreto.
  * 
@@ -11,10 +11,10 @@ public class Ex6_6 {
     System.out.println("ADIVINA EL NÚMERO");
     int cont = 5;
     boolean guess = false;
+    int random = (int) (Math.random() * 100);
     do {
       System.out.println("Tienes " + cont + " intentos");
       System.out.print("Por favor, introduce un número del 0 al 100: ");
-      int random = (int) (Math.random() * 100);
       int n = Integer.parseInt(System.console().readLine());
       if (n == random) {
         guess = true;
