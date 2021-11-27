b0046cb3f7cc1882911a8fd04365d65ab2d1ffb0
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-27 12:33:32 +0100

    Random Numbers

---
+++ Unit_06/Ex14_6.java
@@ -0,0 +1,15 @@
+/**
+ * Realiza un programa que haga justo lo contrario a lo que hace el ejercicio 6.
+ * El programa intentará adivinar el número que estás pensando - un número entre
+ * 0 y 100 - teniendo para ello 5 oportunidades. En cada intento fallido, el
+ * programa debe preguntar si el número que estás pensando es mayor o menor que
+ * el que te acaba de decir.
+ * 
+ * @author David Antunez Perez
+ * 
+ */
+public class Ex14_6 {
+  public static void main(String[] args) {
+
+  }
+}
