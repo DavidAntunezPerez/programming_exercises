5e6ee0b349c32988324cc3d44fac12cd514bb6af
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-29 09:13:57 +0100

    Arrays

--- Unit_7/Practice/ExampleArray2.java
+++ Unit_7/Practice/ExampleArray2.java
@@ -2,7 +2,7 @@ public class ExampleArray2 {
   public static void main(String[] args) {
     String[] fruits = { "manzana", "pera", "piña", "chirimoya" };
     for (int i = 0; i < fruits.length; i++) {
-      System.out.println(fruits[i]);
+      System.out.println("La fruta número " + (i + 1) + " es " + fruits[i]);
     }
   }
 }
