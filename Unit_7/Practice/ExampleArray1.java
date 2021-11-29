bcb09d0319062be0f23917c53cab89238fdbea00
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-29 09:05:01 +0100

    Arrays

--- Unit_7/Practice/ExampleArray1.java
+++ Unit_7/Practice/ExampleArray1.java
@@ -4,9 +4,9 @@ public class ExampleArray1 {
 
     // Para definir como array:
     // Definir Estructura
-    double[] prices;
+    double[] price;
     // Reservar memoria
-    prices = new double[5];
+    price = new double[6];
 
     /**
      * new nos servirá para crear objetos pero sirve para llamar al constructor,
@@ -14,15 +14,24 @@ public class ExampleArray1 {
      */
 
     /**
-     *LA FORMA CORTA SERÍA: 
-     double[] prices = new double[5];
-      */ 
+     * LA FORMA CORTA SERÍA:
+     * double[] prices = new double[5];
+     */
+    /**
+     * TAMBIEN SE PODRIA PONER:
+     * double[] price = {25.127, 500, 4.16, 350, 400, 20};
+     */
+    // Para darle un valor
 
-    //Para darle un valor
-    prices[2]= 4.16;
-    prices[0]= 25.127;
-    
-    System.out.printf("%.2f\n",prices[0]);
-    System.out.printf("%.2f$\n", prices[2]);
+    price[0] = 25.127;
+    price[1] = 500;
+    price[2] = 4.16;
+    price[3] = 350;
+    price[4] = 400;
+    price[5] = 20;
+
+    for (int i = 0; i < price.length; i++) {
+      System.out.printf("%.2f\n", price[i]);
+    }
   }
 }
