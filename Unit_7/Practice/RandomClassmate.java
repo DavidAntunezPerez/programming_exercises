f27e6302ac03fede6d1aa4ac174cca08b59a71dc
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-29 09:56:10 +0100

    Arrays

---
+++ Unit_7/Practice/RandomClassmate.java
@@ -0,0 +1,11 @@
+public class RandomClassmate {
+  public static void main(String[] args) {
+    String[] classmates = { "Aguilera Martín, Diego", "Andrés Domínguez, Javier", "Antúnez Pérez, David",
+        "Benítez Ruíz, Jose Antonio", "Castillo Muñoz, Natalia", "Cueto Jiménez, Alejandro", "Dominguez Gomez, Sergio",
+        "Egea Hermoso, Adrián", "García Campoy, Daniel", "González Pons, Verónica", "Linero Fernández, Álvaro",
+        "Morales García, Sergio", "Moreno González, Alberto", "Moreno Rodríguez, Javier", "Parra Toval, Alberto",
+        "Rodríguez Barcos, Diego", "Servia Morales, David", "San Juan Thomnmpson, Joseph Luca",
+        "Sicilia Pérez, Francisco Javier" };
+    
+  }
+}
