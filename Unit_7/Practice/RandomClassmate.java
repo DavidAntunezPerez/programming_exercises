645ecc04d03fe20619e22d67c7ce7cb5a9518a78
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-29 10:04:34 +0100

    Arrays

--- Unit_7/Practice/RandomClassmate.java
+++ Unit_7/Practice/RandomClassmate.java
@@ -1,11 +1,13 @@
 public class RandomClassmate {
   public static void main(String[] args) {
+    // Hacemos un Array con la lista de la clase
     String[] classmates = { "Aguilera Martín, Diego", "Andrés Domínguez, Javier", "Antúnez Pérez, David",
         "Benítez Ruíz, Jose Antonio", "Castillo Muñoz, Natalia", "Cueto Jiménez, Alejandro", "Dominguez Gomez, Sergio",
         "Egea Hermoso, Adrián", "García Campoy, Daniel", "González Pons, Verónica", "Linero Fernández, Álvaro",
         "Morales García, Sergio", "Moreno González, Alberto", "Moreno Rodríguez, Javier", "Parra Toval, Alberto",
         "Rodríguez Barcos, Diego", "Servia Morales, David", "San Juan Thomnmpson, Joseph Luca",
         "Sicilia Pérez, Francisco Javier" };
-    
+    // Hacemos que se muestre en pantalla el alumno de forma aleatoria
+    System.out.print("El chaval mas subnormal es " + classmates[(int) (Math.random() * classmates.length)]);
   }
 }
