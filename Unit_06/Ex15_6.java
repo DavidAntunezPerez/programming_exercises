143e436b01fc28d5a0a01d8b439cf8903e25db1a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-29 11:37:07 +0100

    Random Numbers

---
+++ Unit_06/Ex15_6.java
@@ -0,0 +1,30 @@
+/**
+ * @author David Antunez Perez
+ *         Realiza un generador de melodía con las siguientes condiciones:
+ *         a) Las notas deben estar generadas al azar. Las 7 notas son do, re,
+ *         mi, fa, sol,
+ *         la y si.
+ *         b) Una melodía está formada por un número aleatorio de notas mayor o
+ *         igual
+ *         a 4, menor o igual a 28 y siempre múltiplo de 4 (4, 8, 12…).
+ *         c) Cada grupo de 4 notas será un compás y estará separado del
+ *         siguiente
+ *         compás mediante la barra vertical “|” (Alt + 1). El final de la
+ *         melodía se marca
+ *         con dos barras.
+ *         d) La última nota de la melodía debe coincidir con la primera.
+ */
+public class Ex15_6 {
+  public static void main(String[] args) {
+    // Creamos variables para cada nota de la melodía
+    String not1 = "do";
+    String not2 = "re";
+    String not3 = "mi";
+    String not4 = "fa";
+    String not5 = "sol";
+    String not6 = "la";
+    String not7 = "si";
+    // 
+
+  }
+}
