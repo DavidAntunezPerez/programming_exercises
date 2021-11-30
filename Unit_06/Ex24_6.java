4fe3bb6d071d69c54d9821d3caeca5ceeaa3ff5c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 12:29:55 +0100

    Random Numbers

--- Unit_06/Ex24_6.java
+++ Unit_06/Ex24_6.java
@@ -1,5 +1,13 @@
+/**
+ * Escribe un programa que, dado un número introducido por teclado, elija al
+ * azar
+ * uno de sus dígitos.
+ * 
+ * @author David Antunez Perez
+ */
+
 public class Ex24_6 {
   public static void main(String[] args) {
-    
+
   }
 }
