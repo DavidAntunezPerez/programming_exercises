ecec216e3c7d0aa7a194e9a8f00ec46b38e7f125
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 12:32:11 +0100

    Random Numbers

--- Unit_06/Ex24_6.java
+++ Unit_06/Ex24_6.java
@@ -5,9 +5,17 @@
  * 
  * @author David Antunez Perez
  */
-
 public class Ex24_6 {
   public static void main(String[] args) {
-
+    // Separa número del último al primero
+    System.out.print("Por favor, introduzca número: ");
+    int numInt = Integer.parseInt(System.console().readLine());
+    int cifra = numInt;
+    int cogNum = 0;
+    for (int i = 1; i <= numInt; i *= 10) {
+      cogNum = cifra % 10;
+      System.out.print(cogNum + "\n");
+      cifra = (cifra - cogNum) / 10;
+    }
   }
 }
