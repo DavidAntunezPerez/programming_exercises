e4cb71bc91f736855be2a20827258f2e741e03e5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 11:08:09 +0100

    Random Numbers

---
+++ Unit_06/Ex21_6.java
@@ -0,0 +1,21 @@
+/**
+ * Realiza un programa que genere una secuencia de cinco monedas de curso
+ * legal lanzadas al aire. Las monedas disponibles son de 1 céntimo, 2 céntimos,
+ * 5 céntimos, 10 céntimos, 20 céntimos, 50 céntimos, 1 euro y 2 euros. Las dos
+ * posiciones posibles son cara y cruz.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex21_6 {
+  public static void main(String[] args) {
+    // inicializamos las variables aleatorias
+    int c = 0;
+    // mostramos resultado por pantalla
+    c = (int) (Math.random() * 2 + 1);
+    System.out.println("2 cents - " + c);
+    System.out.print("20 cents - " + c);
+    System.out.println("50 cents - " + c);
+    System.out.println("1 eur - " + c);
+    System.out.println(" 2 eur - " + c);
+  }
+}
