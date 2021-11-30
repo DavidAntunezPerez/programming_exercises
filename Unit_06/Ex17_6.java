46dd322401f9121cab06daa9584f336d0268b182
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 08:55:06 +0100

    Random Numbers

---
+++ Unit_06/Ex17_6.java
@@ -0,0 +1,16 @@
+/**
+ * Realiza un programa que pinte por pantalla una pecera con un pececito dentro.
+ * Se debe pedir al usuario el ancho y el alto de la pecera, que como mínimo
+ * serán de 4 unidades. No hay que comprobar que los datos se introducen
+ * correctamente; podemos suponer que el usuario los introduce bien. Dentro de
+ * la pecera hay que colocar de forma aleatoria un pececito, que puede estar
+ * situado en cualquiera de las posiciones que quedan en el hueco que forma el
+ * rectángulo
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex17_6 {
+  public static void main(String[] args) {
+
+  }
+}
