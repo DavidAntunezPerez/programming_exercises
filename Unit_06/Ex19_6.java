4efe11629b57a1d4b5da32791bc0bbcede2887ad
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 10:59:42 +0100

    Random Numbers

--- Unit_06/Ex19_6.java
+++ Unit_06/Ex19_6.java
@@ -8,10 +8,23 @@
  */
 public class Ex19_6 {
   public static void main(String[] args) {
-
+    int add = 0;
+    int max = -100;
+    int min = 200;
     for (int i = 0; i <= 50; i++) {
       int n = (int) (Math.random() * 300 - 100);
       System.out.print(n + " ");
+      add += n;
+      if ((n % 2 == 0) && (n > max)) {
+        max = n;
+      }
+      if ((n % 2 != 0) && (n < min)) {
+        min = n;
+      }
     }
+    System.out.println();
+    System.out.println("La suma de todos los números es " + add);
+    System.out.println("El mayor de los pares es " + max);
+    System.out.println("El menor de los impares es " + min);
   }
 }
