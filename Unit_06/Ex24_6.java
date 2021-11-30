1cf39525e8d6150701f1a28cc7e9fbad0eb1db9b
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 16:55:11 +0100

    Random Numbers

--- Unit_06/Ex24_6.java
+++ Unit_06/Ex24_6.java
@@ -7,15 +7,23 @@
  */
 public class Ex24_6 {
   public static void main(String[] args) {
-    // Separa número del último al primero
-    System.out.print("Por favor, introduzca número: ");
-    int numInt = Integer.parseInt(System.console().readLine());
-    int cifra = numInt;
-    int cogNum = 0;
-    for (int i = 1; i <= numInt; i *= 10) {
-      cogNum = cifra % 10;
-      System.out.print(cogNum + "\n");
-      cifra = (cifra - cogNum) / 10;
-    }
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    long numeroIntroducido = Long.parseLong(System.console().readLine());
+
+    long numero = numeroIntroducido;
+
+    // calcula la longitud del número
+    int longitud = 0;
+
+    do {
+      numero /= 10;
+      longitud++;
+    } while (numero > 0);
+
+    // elige una posición al azar dentro del número
+    int posicion = (int) (Math.random() * longitud) + 1;
+
+    // extrae el dígito de esa posición
+    System.out.println((numeroIntroducido / (long) (Math.pow(10, longitud - posicion))) % 10);
   }
 }
