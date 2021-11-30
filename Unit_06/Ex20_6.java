c76cd7b3f26cad0eb47c0838a1ab48271876dcc5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 11:01:21 +0100

    Random Numbers

---
+++ Unit_06/Ex20_6.java
@@ -0,0 +1,13 @@
+/**
+ * Realiza un programa que pinte por pantalla una cuba con cierta cantidad de
+ * agua. La capacidad será indicada por el usuario. La cuba se llenará con una
+ * cantidad aleatoria de agua que puede ir entre 0 y la capacidad máxima que
+ * pueda admitir. El ancho de la cuba no varía.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex20_6 {
+  public static void main(String[] args) {
+
+  }
+}
