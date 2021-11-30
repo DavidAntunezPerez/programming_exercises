7d8e1d312c806105787f1ae32a8b6245fd411b60
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-30 12:16:19 +0100

    Random Numbers

---
+++ Unit_06/Ex22_6.java
@@ -0,0 +1,17 @@
+/**
+ * Realiza un programa que pinte por pantalla una serpiente con un “serpenteo”
+ * aleatorio. La cabeza se representará con el carácter @ y se debe colocar
+ * exactamente en la posición 13 (con 12 espacios delante). A partir de ahí,
+ * el cuerpo irá serpenteando de la siguiente manera: se generará de forma
+ * aleatoria un valor entre tres posibles que hará que el siguiente carácter se
+ * coloque una posición a la izquierda del anterior, alineado con el anterior o
+ * una posición a la derecha del anterior. La longitud de la serpiente se pedirá
+ * por teclado y se supone que el usuario introducirá un dato correcto.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex22_6 {
+  public static void main(String[] args) {
+    
+  }
+}
