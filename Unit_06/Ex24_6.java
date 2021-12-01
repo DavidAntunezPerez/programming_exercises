12826b2db49717a98a1fbe393e92b07db563d60d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-01 18:54:06 +0100

    Random Numbers

--- Unit_06/Ex24_6.java
+++ Unit_06/Ex24_6.java
@@ -2,9 +2,9 @@
  * Escribe un programa que, dado un número introducido por teclado, elija al
  * azar
  * uno de sus dígitos.
- *
+ * 
  * @author David Antunez Perez
- */
+ */ 
 public class Ex24_6 {
   public static void main(String[] args) {
     System.out.print("Por favor, introduzca un número entero positivo: ");
