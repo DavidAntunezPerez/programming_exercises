77e6f8e9a74ebc1ba4c0bf725a13a4d3a6916981
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-01 19:07:26 +0100

    Random Numbers

--- Unit_06/Ex25_6.java
+++ Unit_06/Ex25_6.java
@@ -1,8 +1,14 @@
 /**
+ * Escribe un programa que muestre por pantalla 100 números enteros separados
+ * por un espacio. Los números deben estar generados de forma aleatoria en
+ * un rango entre 10 y 200 ambos incluidos. Los primos deben aparecer entre
+ * almohadillas (p. ej. #19#) y los múltiplos de 5 entre corchetes (p. ej.
+ * [25]).
+ * 
  * @author David Antunez Perez
  */
 public class Ex25_6 {
   public static void main(String[] args) {
-    
+
   }
 }
