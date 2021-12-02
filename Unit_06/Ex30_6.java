776ad1b2d214db04726390347458b11b1511eeb9
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-02 10:12:56 +0100

    Random Numbers

---
+++ Unit_06/Ex30_6.java
@@ -0,0 +1,19 @@
+/**
+ * El pequeño Roberto tenía como mascota un pececillo dentro de una pecera. Los
+ * Reyes Magos le han traído un caballito de mar ($) y una caracola (@) para que
+ * le hagan compañía al pez. Realiza un programa que pinte por pantalla la
+ * pecera
+ * con los tres animalitos acuáticos colocados dentro en posiciones aleatorias.
+ * Por
+ * una cuestión de física elemental, ninguno de los animales puede coincidir
+ * en la misma posición. Se debe pedir al usuario el ancho y el alto de la
+ * pecera,
+ * que como mínimo serán de 4 unidades.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex30_6 {
+  public static void main(String[] args) {
+    
+  }
+}
