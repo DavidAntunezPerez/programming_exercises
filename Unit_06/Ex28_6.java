2be7c2a75de79b072a3295a87b67f4c2d43fa63f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-02 09:17:24 +0100

    Random Numbers

---
+++ Unit_06/Ex28_6.java
@@ -0,0 +1,21 @@
+/**
+ * Realiza una versión del programa “Hola mundo”. Se debe mostrar la palabra
+ * HOLA (todo en mayúsculas) hecha con asteriscos. La altura de la palabra debe
+ * ser un número impar generado al azar entre el 3 y el 13 ambos incluidos. Los
+ * palos horizontales de la H y de la A están justo a la mitad de la altura. La
+ * anchura
+ * de las letras siempre es la misma. Aunque, en este caso, sólo hay 6 posibles
+ * HOLAs de distinta altura, el programa debe hacerse de tal forma que sea muy
+ * fácil adaptarlo a otros rangos de altura, por ej. entre 7 y 33. Por tanto, no
+ * se da
+ * por válido escribir 6 sentencias if, cada una pintando la correspondiente
+ * figura
+ * completa.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex28_6 {
+  public static void main(String[] args) {
+
+  }
+}
