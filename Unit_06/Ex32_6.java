db4bfc0c3af59d676c5487a2401ccbb62f230729
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-02 11:02:16 +0100

    Random Numbers

---
+++ Unit_06/Ex32_6.java
@@ -0,0 +1,27 @@
+/**
+ * Realiza un programa que pinte un sendero aleatorio. Los bordes se pintan con
+ * el carácter “|”. La anchura del sendero siempre es la misma, los dos
+ * caracteres
+ * del borde más cuatro caracteres en medio, en total 6 caracteres (incluyendo
+ * espacios). A cada metro, el sendero puede continuar recto, girar un carácter
+ * a
+ * la izquierda o girar un carácter a la derecha, por supuesto de forma
+ * aleatoria.
+ * Por cada metro de sendero - representado por una línea - puede que haya un
+ * obstáculo o puede que no, la probabilidad es del 50%. La posición del
+ * obstáculo
+ * es aleatoria dentro de la línea. En caso de existir un obstáculo en un metro
+ * de
+ * sendero (en una línea), puede ser una planta (carácter *) o una piedra
+ * (carácter
+ * O), la probabilidad de que salga uno u otro es la misma. Recuerda que nunca
+ * habrá más de un obstáculo por metro de sendero, habrá uno o ninguno.
+ * 
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex32_6 {
+  public static void main(String[] args) {
+
+  }
+}
