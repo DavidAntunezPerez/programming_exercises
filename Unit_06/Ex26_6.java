0aa53b8e868842f91242320c5b6170e2203fe5db
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-02 08:22:58 +0100

    Random Numbers

--- Unit_06/Ex26_6.java
+++ Unit_06/Ex26_6.java
@@ -1,5 +1,14 @@
+/**
+ * Realiza un programa que pinte una tableta de turrón con un bocado realizado
+ * de forma aleatoria. El ancho y el alto de la tableta se pide por teclado. El
+ * bocado se da alrededor del turrón, obviamente no se puede dar un bocado por
+ * en medio
+ * de la tableta.
+ * 
+ * @author David Antunez Perez
+ */
 public class Ex26_6 {
   public static void main(String[] args) {
-    
+
   }
 }
