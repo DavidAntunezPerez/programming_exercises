78bf07862ef9c46c3a69ad9830f42e552e5f0e1d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-06 20:53:47 +0100

    Arrays

--- Unit_07/Ex3_7.java
+++ Unit_07/Ex3_7.java
@@ -1,8 +1,13 @@
 /**
+ * Escribe un programa que lea 10 números por teclado y que luego los muestre
+ * en orden inverso, es decir, el primero que se introduce es el último en
+ * mostrarse
+ * y viceversa.
+ * 
  * @author David Antunez Perez
  */
 public class Ex3_7 {
   public static void main(String[] args) {
-    
+
   }
 }
