260eeb7aa9e7bfa40add41457364f6fb3de8246b
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-07 00:04:44 +0100

    Arrays

---
+++ Unit_07/Ex5_7.java
@@ -0,0 +1,15 @@
+/**
+ * Escribe un programa que pida 10 números por teclado y que luego muestre los
+ * números introducidos junto con las palabras “máximo” y “mínimo” al lado del
+ * máximo y del mínimo respectivamente.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex5_7 {
+  public static void main(String[] args) {
+    
+    for(int i = 0; i < 10; i++){
+      
+    }
+  }
+}
