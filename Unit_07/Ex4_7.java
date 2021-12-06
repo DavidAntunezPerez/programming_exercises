595cec0e78660aa8fdfe7434b266a2442ca427b1
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-06 23:34:45 +0100

    Arrays

---
+++ Unit_07/Ex4_7.java
@@ -0,0 +1,22 @@
+/**
+ * Define tres arrays de 20 números enteros cada una, con nombres numero,
+ * cuadrado
+ * y cubo. Carga el array numero con valores aleatorios entre 0 y 100. En el
+ * array
+ * cuadrado se deben almacenar los cuadrados de los valores que hay en el array
+ * numero. En el array cubo se deben almacenar los cubos de los valores que hay
+ * en
+ * numero. A continuación, muestra el contenido de los tres arrays dispuesto en
+ * tres
+ * columnas.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex4_7 {
+  public static void main(String[] args) {
+    int[] numero = new int[20];
+    int[] cuadrado = new int[20];
+    int[] cubo = new int[20];
+
+  }
+}
