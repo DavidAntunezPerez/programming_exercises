aaaee1a1177672e582d78457dac425ef6719d8d4
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-07 23:55:17 +0100

    Arrays

--- Unit_07/Ex5_7.java
+++ Unit_07/Ex5_7.java
@@ -9,10 +9,11 @@ public class Ex5_7 {
   public static void main(String[] args) {
     int max;
     int min;
+    int[] n = new int[10];
     System.out.print("Introduce un número por teclado: ");
-    int n = Integer.parseInt(System.console().readLine());
-    max = n;
-    min = n;
+    n[0] = Integer.parseInt(System.console().readLine());
+    max = n[0];
+    min = n[0];
     for(int i = 0; i < 10; i++){
       System.out.print("Introduce otro número por teclado: ");
     }
