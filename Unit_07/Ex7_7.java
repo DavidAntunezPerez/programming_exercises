ad0886f9aa7fc57645e0fdad9da77f4818c40dbb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-09 08:44:55 +0100

    Arrays

--- Unit_07/Ex7_7.java
+++ Unit_07/Ex7_7.java
@@ -1,4 +1,11 @@
 /**
+ * Escribe un programa que genere 100 números aleatorios del 0 al 20 y que los
+ * muestre por pantalla separados por espacios. El programa pedirá entonces por
+ * teclado dos valores y a continuación cambiará todas las ocurrencias del
+ * primer
+ * valor por el segundo en la lista generada anteriormente. Los números que se
+ * han cambiado deben aparecer entrecomillados.
+ * 
  * @author David Antunez Perez
  */
 public class Ex7_7 {
