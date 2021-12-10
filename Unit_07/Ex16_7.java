459469a7fdbe10dbb22a1cfb41700ba227acbb05
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-10 20:37:57 +0100

    Arrays

--- Unit_07/Ex16_7.java
+++ Unit_07/Ex16_7.java
@@ -10,6 +10,44 @@
  */
 public class Ex16_7 {
   public static void main(String[] args) {
+    // Declaramos el array
+    int[] n = new int[20];
+    // Aleatorizamos el array
+    for (int i = 0; i < n.length; i++) {
+      n[i] = (int) (Math.random() * 400);
+    }
+    // Mostramos el array
+    for (int i = 0; i < n.length; i++) {
+      System.out.print(n[i] + " ");
+    }
+    // Pregunta al usuario
+    System.out.println();
+    int res;
+    do {
+      System.out.print("¿Quieres resaltar multiplos de 5 o de 7? (5 / 7): ");
+      res = Integer.parseInt(System.console().readLine());
+      if ((res != 5) && (res != 7)) {
+        System.out.println("Escribe un valor válido (5 o 7).");
+      }
+    } while ((res != 5) && (res != 7));
+    // Remarcar según lo elegido
+    if (res == 5) {
+      for (int i = 0; i < n.length; i++) {
+        if (n[i] % 5 == 0) {
+          System.out.print("[" + n[i] + "]" + " ");
+        } else {
+          System.out.print(n[i] + " ");
+        }
+      }
+    } else if (res == 7) {
 
+      for (int i = 0; i < n.length; i++) {
+        if (n[i] % 7 == 0) {
+          System.out.print("[" + n[i] + "]" + " ");
+        } else {
+          System.out.print(n[i] + " ");
+        }
+      }
+    }
   }
 }
