cba40106e3f6d01198d45e932b20e98d2f30ebc5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-10 12:38:53 +0100

    Arrays

---
+++ Unit_07/Ex14_7.java
@@ -0,0 +1,17 @@
+/**
+ * Escribe un programa que pida 8 palabras y las almacene en un array. A
+ * continuación, las palabras correspondientes a colores se deben almacenar al
+ * comienzo y las que no son colores a continuación. Puedes utilizar tantos
+ * arrays
+ * auxiliares como quieras. Los colores que conoce el programa deben estar en
+ * otro array y son los siguientes: verde, rojo, azul, amarillo, naranja, rosa,
+ * negro,
+ * blanco y morado.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex14_7 {
+  public static void main(String[] args) {
+    
+  }
+}
