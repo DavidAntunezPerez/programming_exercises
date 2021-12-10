d961b3261cb0f2ea58e7dc6ba3ea0953684b4a09
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-10 12:37:57 +0100

    Arrays

--- Unit_07/Ex13_7.java
+++ Unit_07/Ex13_7.java
@@ -11,29 +11,44 @@
 public class Ex13_7 {
   public static void main(String[] args) {
     int[] n = new int[100];
+    int max = 0;
+    int min = 500;
     for (int i = 0; i < n.length; i++) {
       n[i] = (int) (Math.random() * 500);
       System.out.print(n[i] + "  ");
+      if (n[i] > max) {
+        max = n[i];
+      }
+      if (n[i] < min) {
+        min = n[i];
+      }
     }
     System.out.println();
     int dest;
-    do{
-    System.out.print("¿Qué quieres destacar? (1 - max ; 2 - min) : ");
-    dest = Integer.parseInt(System.console().readLine());
-  }while((dest < 1)||( dest > 2));
+    do {
+      System.out.print("¿Qué quieres destacar? (1 - max ; 2 - min) : ");
+      dest = Integer.parseInt(System.console().readLine());
+    } while ((dest < 1) || (dest > 2));
     System.out.println();
-    switch (dest){
+    switch (dest) {
       case 1:
-      for (int i = 0; i < n.length; i++) {
-        
-        System.out.print(n[i] + "  ");
-      }
-      break;
+        for (int i = 0; i < n.length; i++) {
+          if (n[i] == min) {
+            System.out.print("***" + n[i] + "***" + "  ");
+          } else {
+            System.out.print(n[i] + "  ");
+          }
+        }
+        break;
       case 2:
-      for (int i = 0; i < n.length; i++) {
-        System.out.print(n[i] + "  ");
-      }
-      break;
+        for (int i = 0; i < n.length; i++) {
+          if (n[i] == max) {
+            System.out.print("***" + n[i] + "***" + "  ");
+          } else {
+            System.out.print(n[i] + "  ");
+          }
+        }
+        break;
     }
   }
 }
