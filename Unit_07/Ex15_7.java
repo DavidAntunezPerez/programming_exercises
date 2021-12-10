d08b1dd173dc16444e4b661b4a75fae78744053e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-10 13:57:14 +0100

    Arrays

---
+++ Unit_07/Ex15_7.java
@@ -0,0 +1,34 @@
+/**
+ * Un restaurante nos ha encargado una aplicación para colocar a los clientes en
+ * sus mesas. En una mesa se pueden sentar de 0 (mesa vacía) a 4 comensales
+ * (mesa llena). Cuando llega un cliente se le pregunta cuántos son. De momento
+ * el programa no está preparado para colocar a grupos mayores a 4, por tanto,
+ * si
+ * un cliente dice por ejemplo que son un grupo de 6, el programa dará el
+ * mensaje
+ * “Lo siento, no admitimos grupos de 6, haga grupos de 4 personas como
+ * máximo e intente de nuevo”. Para el grupo que llega, se busca siempre
+ * la primera mesa libre (con 0 personas). Si no quedan mesas libres, se busca
+ * donde haya un hueco para todo el grupo, por ejemplo si el grupo es de dos
+ * personas, se podrá colocar donde haya una o dos personas. Inicialmente, las
+ * mesas se cargan con valores aleatorios entre 0 y 4. Cada vez que se sientan
+ * nuevos clientes se debe mostrar el estado de las mesas. Los grupos no se
+ * pueden romper aunque haya huecos sueltos suficientes.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex15_7 {
+  public static void main(String[] args) {
+    int people;
+    do {
+      System.out.print("¿Cuántos son? : ");
+      people = Integer.parseInt(System.console().readLine());
+      if (people > 4) {
+        System.out.println();
+        System.out
+            .print("Lo siento, no admitimos grupos de 6, haga grupos de 4 personas como máximo e intente de nuevo.");
+      }
+    } while (people > 4);
+
+  }
+}
