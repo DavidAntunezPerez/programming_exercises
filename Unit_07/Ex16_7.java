0f97a1a58a18c7fbeb19623fb18abb52acab83a1
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-10 20:19:06 +0100

    Arrays

---
+++ Unit_07/Ex16_7.java
@@ -0,0 +1,15 @@
+/**
+ * Escribe un programa que rellene un array de 20 elementos con números enteros
+ * aleatorios comprendidos entre 0 y 400 (ambos incluidos). A continuación el
+ * programa mostrará el array y preguntará si el usuario quiere resaltar los
+ * múltiplos de 5 o los múltiplos de 7. Seguidamente se volverá a mostrar el
+ * array
+ * escribiendo los números que se quieren resaltar entre corchetes.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex16_7 {
+  public static void main(String[] args) {
+
+  }
+}
