06c12fd536ec325187aac174b4e95d4b9d4d291f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-11 19:01:39 +0100

    Arrays

---
+++ Unit_07/Ex19_7.java
@@ -0,0 +1,18 @@
+/**
+ * Realiza un programa que sea capaz de insertar un número en una posición
+ * concreta de un array. En primer lugar, el programa generará un array de 12
+ * números enteros aleatorios entre 0 y 200 ambos incluidos. A continuación se
+ * debe mostrar el contenido de ese array junto al índice (0 – 11). Seguidamente
+ * el programa preguntará por el número que se quiere insertar y por la posición
+ * donde será insertado. Los números del array se desplazan a la derecha para
+ * dejar sitio al nuevo. El último número (el que se encuentra en la posición
+ * 11)
+ * siempre se perderá.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex19_7 {
+  public static void main(String[] args) {
+    
+  }
+}
