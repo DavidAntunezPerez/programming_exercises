ee7f5ed464c8f610aa7bc50d61f73856c47f5c46
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-11 21:02:42 +0100

    Arrays

--- Unit_07/Ex20_7.java
+++ Unit_07/Ex20_7.java
@@ -13,6 +13,23 @@
  */
 public class Ex20_7 {
   public static void main(String[] args) {
-    
+    System.out.print("Introduzca el número de reyes:  ");
+    int n = Integer.parseInt(System.console().readLine());
+    String[] name = new String[n];
+    System.out.println();
+    System.out.print("Vaya introduciendo nombres de reyes y pulsando ENTER: \n");
+    for (int i = 0; i < name.length; i++) {
+      name[i] = System.console().readLine();
+    }
+    for (int i = 0; i < n; i++) {
+      int orden = 1;
+      
+      for (int j = 0; j < i; j++) {
+        if (name[i].equals(name[j])) {
+          orden++;
+        }
+      }
+      System.out.println(name[i] + " " + orden + "º");
+    }
   }
 }
