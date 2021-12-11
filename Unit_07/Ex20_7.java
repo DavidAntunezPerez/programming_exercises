7fff9cca7603cd35c85e22016006ca5b78e9cc80
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-11 19:08:13 +0100

    Arrays

---
+++ Unit_07/Ex20_7.java
@@ -0,0 +1,18 @@
+/**
+ * Implementa un programa que calcule la denominación ordinal de los reyes de
+ * una secuencia histórica. El programa solicitará la cantidad de reyes que se
+ * van a introducir, y a continuación recibirá los nombres de los reyes.
+ * Presentará
+ * por pantalla dichos nombres, pero colocándoles el ordinal correspondiente.
+ * Así,
+ * por ejemplo, si hay dos Felipes en los nombres de los reyes, el primero
+ * debería
+ * aparecer como Felipe 1º y el segundo como Felipe 2º.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex20_7 {
+  public static void main(String[] args) {
+    
+  }
+}
