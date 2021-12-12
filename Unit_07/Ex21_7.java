f1140b33ff4a6713037fcca8ffc72166a86f2fef
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-12 20:59:44 +0100

    Arrays

--- Unit_07/Ex21_7.java
+++ Unit_07/Ex21_7.java
@@ -2,8 +2,7 @@
  * Escribe un programa que rellene un array de 15 elementos con números enteros
  * comprendidos entre 0 y 500 (ambos incluidos). A continuación, se mostrará el
  * array “cincuerizado”, según el siguiente criterio: si el número que hay en
- * una
- * posición del array es múltiplo de 5, se deja igual, y si no, se cambia por el
+ * una posición del array es múltiplo de 5, se deja igual, y si no, se cambia por el
  * siguiente múltiplo de 5 que exista a partir de él.
  * 
  * @author David Antunez Perez
@@ -14,6 +13,24 @@ public class Ex21_7 {
     for(int i = 0; i < n.length; i++) {
       n[i] = (int) (Math.random() * 501);
     }
-    
+    // Mostrar Indice + Array
+    System.out.println("ARRAY ORIGINAL");
+    for (int i = 0; i < n.length; i++) {
+      System.out.printf(" %-4d", i);
+    }
+    System.out.println();
+    for (int i = 0; i < n.length; i++) {
+      System.out.printf("%-5s", n[i]);
+    }
+    // Mostrar array ordenado
+    System.out.println();
+    System.out.print("ARRAY CINCUERIZADO\n");
+    for (int i = 0; i < n.length; i++) {
+      System.out.printf(" %-4d", i);
+    }
+    System.out.println();
+    for (int i = 0; i < n.length; i++) {
+      System.out.printf("%-5s", n[i]);
+    }
   }
 }
