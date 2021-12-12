8db6cbcd4d458f8484055112b232c2eba7a993e8
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-12 20:27:20 +0100

    Arrays

--- Unit_07/Ex21_7.java
+++ Unit_07/Ex21_7.java
@@ -1,4 +1,10 @@
 /**
+ * Escribe un programa que rellene un array de 15 elementos con números enteros
+ * comprendidos entre 0 y 500 (ambos incluidos). A continuación, se mostrará el
+ * array “cincuerizado”, según el siguiente criterio: si el número que hay en
+ * una
+ * posición del array es múltiplo de 5, se deja igual, y si no, se cambia por el
+ * siguiente múltiplo de 5 que exista a partir de él.
  * 
  * @author David Antunez Perez
  */
