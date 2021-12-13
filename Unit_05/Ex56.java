f80970dd90e2d73817ee1d232cf091e20809c660
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 19:24:55 +0100

    Loops

---
+++ Unit_05/Ex56.java
@@ -0,0 +1,11 @@
+/**
+ * Realiza un programa que pinte un triángulo relleno tal como se muestra en los
+ * ejemplos. El usuario debe introducir la altura de la figura.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex56 {
+  public static void main(String[] args) {
+
+  }
+}
