2fedd8e6eb7d1d0880983049e49cdee1293eb31a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 20:11:40 +0100

    Loops

---
+++ Unit_05/Ex64_5.java
@@ -0,0 +1,17 @@
+/**
+ * Escribe un programa que pinte por pantalla un rectángulo hueco de 6
+ * caracteres de ancho por 3 de alto y, a continuación, un menú que permita
+ * agrandarlo,
+ * achicarlo o cambiar su orientación. Cada vez que el rectángulo se agranda,
+ * se incrementa en 1 tanto su anchura como su altura. Cuando se achica,
+ * se decrementa en 1 su anchura y altura. Por último, cuando se cambia la
+ * orientación, los valores de anchura y altura se intercambian. El valor mínimo
+ * de la altura o la anchura es 2.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex64_5 {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ Unit_05/Ex65_5.java
@@ -0,0 +1,17 @@
+/**
+ * Escribe un programa que pinte por pantalla la letra A. El usuario debe
+ * introducir
+ * la altura total y la fila en la que debe aparecer el palito horizontal
+ * (contando
+ * desde el vértice). La altura mínima es de 3 pisos. La fila donde va el palito
+ * horizontal debe ser mayor que 1 y menor que la altura total. Si el usuario
+ * introduce algún dato incorrecto, el programa debe mostrar un mensaje de
+ * error.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex65_5 {
+  public static void main(String[] args) {
+
+  }
+}
