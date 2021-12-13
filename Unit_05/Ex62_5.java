95f228057ee4dc47c526a1bc3dfbd00212bda096
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 19:59:17 +0100

    Loops

--- Unit_05/Ex62_5.java
+++ Unit_05/Ex62_5.java
@@ -11,6 +11,31 @@
  */
 public class Ex62_5 {
   public static void main(String[] args) {
-    
+    int lucky = 0;
+    int unlucky = 0;
+    // Separa número del último al primero
+    System.out.print("Por favor, introduzca número: ");
+    int numInt = Integer.parseInt(System.console().readLine());
+    int cifra = numInt;
+    int cogNum = 0;
+    for (int i = 1; i <= numInt; i *= 10) {
+      cogNum = cifra % 10;
+      if ((cogNum == 3) || (cogNum == 7) || (cogNum == 8)) {
+        lucky++;
+      } else {
+        unlucky++;
+      }
+      cifra = (cifra - cogNum) / 10;
+    }
+    // Comparar las cifras afortunadas con las desafortunadas, en caso de empate, el
+    // sistema dirá que es afortunado
+    String result;
+    if (lucky >= unlucky) {
+      result = "afortunado";
+    } else {
+      result = "desafortunado";
+    }
+    // Mostramos el resultado
+    System.out.println("El número " + numInt + " es un número " + result);
   }
 }
