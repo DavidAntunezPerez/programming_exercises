5d19ff26da24e1905ef752281cf7c092d0814a45
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 18:44:45 +0100

    Loops

--- Unit_05/Ex55_5.java
+++ Unit_05/Ex55_5.java
@@ -1,8 +1,24 @@
 /**
+ * Realiza un programa que sea capaz de desplazar todos los dígitos de un número
+ * de izquierda a derecha una posición. El dígito de más a la derecha, pasaría a
+ * dar la vuelta y se colocaría a la izquierda. Si el número tiene un solo
+ * dígito, se
+ * queda igual.
+ * 
  * @author David Antunez Perez
  */
 public class Ex55_5 {
   public static void main(String[] args) {
+    System.out.print("Introduzca un número: ");
+    int n = Integer.parseInt(System.console().readLine());
+    int cont = 0;
+    for (int i = n; i > 0;) {
+      i /= 10;
+      cont++;
+    }
+    int aux = n % 10;
     
+    System.out.print(" "+aux);
+
   }
 }
