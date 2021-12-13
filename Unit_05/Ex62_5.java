17c751b5ccee17bc7e53e3abafe84d411037bcc8
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 19:36:31 +0100

    Loops

---
+++ Unit_05/Ex62_5.java
@@ -0,0 +1,16 @@
+/**
+ * Según cierta cultura oriental, los números de la suerte son el 3, el 7, el 8
+ * y el 9.
+ * Los números de la mala suerte son el resto: el 0, el 1, el 2, el 4, el 5 y el
+ * 6. Un
+ * número es afortunado si contiene más números de la suerte que de la mala
+ * suerte. Realiza un programa que diga si un número introducido por el usuario
+ * es afortunado o no
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex62_5 {
+  public static void main(String[] args) {
+
+  }
+}
