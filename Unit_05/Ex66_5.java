c13874fc45f06569679f5e2f2de2c8b8a860e22d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 20:12:22 +0100

    Loops

---
+++ Unit_05/Ex66_5.java
@@ -0,0 +1,18 @@
+/**
+ * La Guardia Civil de Tráfico nos ha encargado un programa que pinte una señal
+ * para desviar el tráfico hacia la derecha. La señal es una doble flecha con el
+ * vértice apuntando a la derecha. Se pide al usuario la altura de la figura,
+ * que
+ * debe ser un número impar mayor o igual que 3. La distancia entre cada flecha
+ * de asteriscos es siempre de 4 espacios. Si la altura introducida por el
+ * usuario
+ * no es un número impar mayor o igual que 3, el programa debe mostrar un
+ * mensaje de error.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex66_5 {
+  public static void main(String[] args) {
+    
+  }
+}
