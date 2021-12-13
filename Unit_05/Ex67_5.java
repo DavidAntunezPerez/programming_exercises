749c32fb900809b69c60fa16a6e89d54daba0df9
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 20:13:00 +0100

    Loops

---
+++ Unit_05/Ex67_5.java
@@ -0,0 +1,15 @@
+/**
+ * @author David Antunez Perez
+ *         Realiza un programa que pinte una escalera que va descendiendo de
+ *         izquierda
+ *         a derecha. El programa pedirá el número de escalones y la altura de
+ *         cada
+ *         escalón. La anchura de los escalones siempre es la misma: 4
+ *         asteriscos.
+ * 
+ */
+public class Ex67_5 {
+  public static void main(String[] args) {
+
+  }
+}
