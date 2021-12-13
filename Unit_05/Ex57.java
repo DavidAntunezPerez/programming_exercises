c9c7eeb8aca7629ab5a9d2540e0a45f2ba6a53f5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 19:29:39 +0100

    Loops

---
+++ Unit_05/Ex57.java
@@ -0,0 +1,11 @@
+/**
+ * Realiza un programa que pinte un triángulo hueco tal como se muestra en los
+ * ejemplos. El usuario debe introducir la altura de la figura.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex57 {
+  public static void main(String[] args) {
+
+  }
+}
