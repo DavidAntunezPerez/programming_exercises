2cd0dc16ba674ec4bb700994eec8b1a52035db4c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 20:01:40 +0100

    Loops

---
+++ Unit_05/Ex63_5.java
@@ -0,0 +1,11 @@
+/**
+ * Realiza un programa que pinte dos pirámides rellenas hechas con asteriscos,
+ * una al lado de la otra y separadas por un espacio en su base.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex63_5 {
+  public static void main(String[] args) {
+
+  }
+}
