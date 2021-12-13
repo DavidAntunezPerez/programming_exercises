b78c5881cebd7557d6d7cb208459712ad799b4de
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-12-13 20:13:31 +0100

    Loops

---
+++ Unit_05/Ex69_5.java
@@ -0,0 +1,19 @@
+/**
+ * Realiza un programa que pinte una pirámide maya. Por los lados, se trata
+ * de una pirámide normal y corriente. Por el centro se van pintando líneas de
+ * asteriscos de forma alterna (empezando por la superior): la primera se pinta,
+ * la segunda no, la tercera sí, la cuarta no, etc. La terraza de la pirámide
+ * siempre
+ * tiene 6 asteriscos, por tanto, las líneas centrales que se añaden a la
+ * pirámide
+ * normal tienen 4 asteriscos. El programa pedirá la altura. Se supone que el
+ * usuario introducirá un número entero mayor o igual a 3; no es necesario
+ * comprobar los datos de entrada.
+ * 
+ * @author David Antunez Perez
+ */
+public class Ex69_5 {
+  public static void main(String[] args) {
+
+  }
+}
