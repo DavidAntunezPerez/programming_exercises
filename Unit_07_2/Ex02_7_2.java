1e9a7bbeff4243e2726272874224130144543f87
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-10 10:21:16 +0100

    Bidimensional Arrays

--- Unit_07_2/Ex02_7_2.java
+++ Unit_07_2/Ex02_7_2.java
@@ -11,6 +11,26 @@
  */
 public class Ex02_7_2 {
   public static void main(String[] args) {
-    
+    int[][] n = new int[5][6]; // aumentamos el tamaño del array puesto que queremos meter las sumas
+    int fila, columna;
+    // asignamos valores aleatorios al array
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 5; columna++) {
+        n[fila][columna] = (int) (Math.random() * 101);
+        n[4][columna] += n[fila][columna]; // suma columna
+        n[fila][5] += n[fila][columna]; // suma filas
+        n[4][5] += n[fila][5];
+        n[4][5] += n[4][columna];
+      }
+    }
+
+    for (fila = 0; fila < 5; fila++) {
+      System.out.print("Fila: " + fila);
+      for (columna = 0; columna < 6; columna++) {
+        System.out.printf("%10d ", n[fila][columna]);
+      }
+      System.out.println();
+    }
+
   }
 }
