08cfec1c7c779d27ca4475ed977128c5ddc6734f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-10 10:46:13 +0100

    Bidimensional Arrays

--- Unit_07_2/Ex03_7_2.java
+++ Unit_07_2/Ex03_7_2.java
@@ -1,24 +1,20 @@
 /**
  * @author David Antunez Perez
  * 
- *         Escribe un programa que pida 20 números enteros. Estos números se
- *         deben
- *         introducir en un array de 4 filas por 5 columnas. El programa
- *         mostrará las
- *         sumas parciales de filas y columnas igual que si de una hoja de
- *         cálculo se
- *         tratara. La suma total debe aparecer en la esquina inferior derecha
+ *         Modifica el programa anterior de tal forma que los números que se
+ *         introducen
+ *         en el array se generen de forma aleatoria (valores entre 100 y 999).
+ * 
  */
-public class Ex02_7_2 {
+public class Ex03_7_2 {
   public static void main(String[] args) {
     int[][] n = new int[5][6]; // aumentamos el tamaño del array puesto que queremos meter las sumas
     int fila, columna;
-    // asignamos valores al array
+    // asignamos valores aleatorios al array
     for (fila = 0; fila < 4; fila++) {
       for (columna = 0; columna < 5; columna++) {
         // introducir número
-        System.out.print("Introduce un número:");
-        n[fila][columna] = Integer.parseInt(System.console().readLine());
+        n[fila][columna] = (int) (Math.random() * 900 + 100);
         n[4][columna] += n[fila][columna]; // suma columna
         n[fila][5] += n[fila][columna]; // suma filas
         n[4][5] += n[fila][columna]; // suma total
