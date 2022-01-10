33b457b06601edc8b5238d3ca234c46067fc4e78
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-10 09:14:13 +0100

    Bidimensional Arrays

---
+++ Unit_07_2/Practice/ExampleArray5.java
@@ -0,0 +1,15 @@
+public class ExampleArray5 {
+  public static void main(String[] args)
+      throws InterruptedException { // Se añade esta línea para poder usar sleep
+    int fila, columna;
+    int[][] n = { { 20, 4 }, { 67, 33 }, { 0, 7 } };
+    for (fila = 0; fila < 3; fila++) {
+      System.out.print("Fila: " + fila);
+      for (columna = 0; columna < 2; columna++) {
+        System.out.printf("%10d ", n[fila][columna]);
+        Thread.sleep(1000); // retardo de un segundo
+      }
+      System.out.println();
+    }
+  }
+}
