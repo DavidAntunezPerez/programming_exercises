9f8ac1632a5fd13b9029ea9fba9effeb199c22ef
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-10 08:59:09 +0100

    Bidimensional Arrays

---
+++ Unit_07_2/Practice/ExampleArray4.java
@@ -0,0 +1,18 @@
+public class ExampleArray4 {
+  public static void main(String[] args) throws InterruptedException {
+    int[][] n = new int[3][2]; // array de 3 filas y 2 columnas
+    n[0][0] = 25;
+    n[1][0] = 73;
+    n[1][1] = 22;
+    n[2][1] = 5;
+    int fila, columna;
+    for(fila = 0; fila < 3; fila++){
+      System.out.print("Fila: "+ fila);
+      for(columna = 0; columna < 2; columna++){
+        System.out.printf("%10d ",n[fila][columna]);
+        Thread.sleep(1000); // retardo de un segundo
+      }
+      System.out.println();
+    }
+  }
+}
