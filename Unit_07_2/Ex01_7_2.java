fc01859f68189276f7b8e4a942a3211f78620056
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-10 09:19:56 +0100

    Bidimensional Arrays

---
+++ Unit_07_2/Ex01_7_2.java
@@ -0,0 +1,15 @@
+public class Ex01_7_2 {
+  public static void main(String[] args) {
+    int[][] n = new int[3][6];
+    n[0][0] = 0;
+    n[0][1] = 30;
+    n[0][2] = 2;
+    n[0][5] = 5;
+    n[1][0] = 75;
+    n[1][4] = 0;
+    n[2][2] = -2;
+    n[2][3] = 9;
+    n[2][5] = 11;
+
+  }
+}
