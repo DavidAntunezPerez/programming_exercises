ed7d5286fe588c7201daa13899285d3d786827a6
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-12 13:54:10 +0100

    Multidimensional Arrays

--- Unit_07_2/Ex07_7_2.java
+++ Unit_07_2/Ex07_7_2.java
@@ -4,7 +4,7 @@
  *         Mejora el juego “Busca el tesoro” de tal forma que si hay una mina a
  *         una casilla
  *         de distancia, el programa avise diciendo ¡Cuidado! ¡Hay una mina
- *         cerca!
+ *         cerca! 
  * 
  * 
  */
