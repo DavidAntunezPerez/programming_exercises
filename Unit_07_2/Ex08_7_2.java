2b07d75dba6684c3d577c397e82a6978a2a3767c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-13 08:33:18 +0100

    Multidimensional Arrays

---
+++ Unit_07_2/Ex08_7_2.java
@@ -0,0 +1,21 @@
+/**
+ * @author David Antunez Perez
+ * 
+ *         Escribe un programa que, dada una posición en un tablero de ajedrez,
+ *         nos diga
+ *         a qué casillas podría saltar un alfil que se encuentra en esa
+ *         posición. Como se
+ *         indica en la figura, el alfil se mueve siempre en diagonal. El
+ *         tablero cuenta con
+ *         64 casillas. Las columnas se indican con las letras de la “a” a la
+ *         “h” y las filas
+ *         se indican del 1 al 8
+ */
+public class Ex08_7_2 {
+  public static void main(String[] args) {
+    int fila, columna;
+    int[][] n = new int[8][8]; // array principal
+    String[][] gArray = new String[8][8]; // array gráfico
+    
+  }
+}
