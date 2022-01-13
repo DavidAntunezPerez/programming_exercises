bdc66a32ba42184345429390e5750e07bf02f87e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-13 10:22:43 +0100

    Multidimensional Arrays

--- Unit_07_2/Ex09_7_2.java
+++ Unit_07_2/Ex09_7_2.java
@@ -14,6 +14,11 @@ public class Ex09_7_2 {
   public static void main(String[] args) {
     int[][] m = new int[12][12]; // matriz cuadradada de 12 x 12
     int fila, columna;
+    int capa;
+    int i;
+    int j;
+    int aux1;
+    int aux2;
     // hacemos que la matriz contenga números generados al azar
     System.out.println("                               MATRIZ ORIGINAL");
     System.out.println("                               ---------------");
@@ -29,12 +34,38 @@ public class Ex09_7_2 {
     System.out.println("                               MATRIZ RESULTADO");
     System.out.println("                               ----------------");
     int aux = m[11][11];
-    for (int i = 11; i > 0; i--) {
-      for(int j = 11; j > 0 ; j--)
-      m[i][j] = m[i - 1][j - 1];
-    }
+    for (capa = 0; capa < 6; capa++) {
+
+      // rota por arriba
+      aux1 = m[capa][11 - capa];
+      for (i = 11 - capa; i > capa; i--) {
+        m[capa][i] = m[capa][i - 1];
+      }
+
+      // rota por la derecha
+      aux2 = m[11 - capa][11 - capa];
+      for (i = 11 - capa; i > capa + 1; i--) {
+        m[i][11 - capa] = m[i - 1][11 - capa];
+      }
+      m[capa + 1][11 - capa] = aux1;
+
+      // rota por abajo
+      aux1 = m[11 - capa][capa];
+      for (i = capa; i < 11 - capa - 1; i++) {
+        m[11 - capa][i] = m[11 - capa][i + 1];
+      }
+      m[11 - capa][11 - capa - 1] = aux2;
+
+      // rota por la izquierda
+      for (i = capa; i < 11 - capa - 1; i++) {
+        m[i][capa] = m[i + 1][capa];
+      }
+      m[11 - capa - 1][capa] = aux1;
+
+    } // for capa
+
     m[0][0] = aux;
-    // Mostrar por pantalla
+    // mostrar por pantalla
     for (fila = 0; fila < 12; fila++) {
       for (columna = 0; columna < 12; columna++) {
         System.out.printf("%6d ", m[fila][columna]); // lo mostramos por pantalla
