4960db7f8955b889e269178a1906cdef946dae79
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-13 10:32:58 +0100

    Multidimensional Arrays

--- Unit_07_2/Ex10_7_2.java
+++ Unit_07_2/Ex10_7_2.java
@@ -1,12 +1,21 @@
 /**
  * @author David Antunez Perez
- * Realiza el juego de las tres en raya.
-
+ *         Realiza el juego de las tres en raya.
+ * 
  */
 public class Ex10_7_2 {
   public static void main(String[] args) {
-    int fila,columna;
+    int fila, columna;
     int[][] n = new int[3][3];
-    
+    System.out.println(" _________ ");
+    for (fila = 0; fila < 3; fila++) {
+      System.out.print("|");
+      for (columna = 0; columna < 3; columna++) {
+        System.out.printf("%2d ", n[fila][columna]);
+      }
+      System.out.print("|");
+      System.out.println();
+    }
+    System.out.println(" --------- ");
   }
 }
