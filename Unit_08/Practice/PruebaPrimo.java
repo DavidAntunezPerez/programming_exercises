47f0e2a1706f489c1599bc0d9470599f1aaeabab
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-17 09:57:33 +0100

    Functions

--- Unit_08/Practice/PruebaPrimo.java
+++ Unit_08/Practice/PruebaPrimo.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class PruebaPrimo {
   public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    int[] n = new int[200];
+    int[] n = new int[100];
     for (int i = 0; i < n.length; i++) {
       n[i] = random(1, 100);
     }
