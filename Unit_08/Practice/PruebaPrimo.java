ae552f31457c504e1c90760b47adb1630fca7a99
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-17 10:03:52 +0100

    Functions

--- Unit_08/Practice/PruebaPrimo.java
+++ Unit_08/Practice/PruebaPrimo.java
@@ -8,9 +8,31 @@ public class PruebaPrimo {
     for (int num : n) {
       System.out.printf("%3d", num);
     }
-  }
+    System.out.println();
+    System.out.print("Primos: ");
+    for (int num : n) {
+      if (esPrimo(num)) {
+        System.out.printf("%3d", num);
+      }
+    }
+  } // main
 
   public static int random(int start, int end) {
     return (int) (Math.random() * (end - start + 1) + start);
-  }
-}
+  } // random
+
+  public static boolean esPrimo(int numInt) {
+    // Comrpueba Primo
+    int contDivisores = 2; // No puede ser 1
+    boolean pregPrimo = true; // Presupone que el número es primo
+    do {
+      if (numInt % contDivisores == 0 && numInt != contDivisores) {
+        pregPrimo = false; // Si encuentra un divisor: Ya no es primo
+      }
+      contDivisores++;
+    } while (contDivisores < numInt && pregPrimo == true);
+    // Es menor que "<" porque np ùede ser el misom número
+    return pregPrimo;
+
+  }// esPrimo
+} // class
