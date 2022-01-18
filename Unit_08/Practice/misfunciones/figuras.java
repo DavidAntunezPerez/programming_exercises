5a78704d501f45e0c5687679d3e1c59106a599d9
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-18 09:24:23 +0100

    Functions

---
+++ Unit_08/Practice/misfunciones/figuras.java
@@ -0,0 +1,21 @@
+package misfunciones;
+
+/**
+ * Realiza figuras
+ */
+
+public class figuras {
+  /**
+   * Crea una linea de asteriscos de una longitud determinada
+   * 
+   * @param longitud tamaño de la línea
+   * @return una línea de asteriscos
+   */
+  public static String line(int longitud) {
+    String result = "";
+    for (int i = 0; i < longitud; i++) {
+      result += "*";
+    }
+    return result;
+  }
+}// class
