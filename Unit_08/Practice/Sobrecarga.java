e0a9943cb3ef266367063e8c03a54aae2a499cf9
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-18 08:43:43 +0100

    Functions

---
+++ Unit_08/Practice/Sobrecarga.java
@@ -0,0 +1,22 @@
+public class Sobrecarga {
+  public static void main(String[] args) {
+    greet();
+    greet("Juan");
+    greet(7);
+  }
+
+  // Functions
+  public static void greet() {
+    System.out.println("Hello");
+  }
+
+  public static void greet(String name) {
+    System.out.println("Hello " + name);
+  }
+
+  public static void greet(int times) {
+    for (int i = 0; i < times; i++) {
+      System.out.println("Hello(" + i + ") ");
+    }
+  }
+}
