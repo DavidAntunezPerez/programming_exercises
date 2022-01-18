d6a962be423f90a96ec591717561fb2c7da42fe3
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-18 09:43:14 +0100

    Functions

--- Unit_08/Practice/PruebaPaquete.java
+++ Unit_08/Practice/PruebaPaquete.java
@@ -3,6 +3,6 @@ import misfunciones.Matematicas;
 
 public class PruebaPaquete {
   public static void main(String[] args) {
-    System.out.println(Matematicas.random(10,20));
+    System.out.println(Matematicas.random(10, 20));
   }
 }
