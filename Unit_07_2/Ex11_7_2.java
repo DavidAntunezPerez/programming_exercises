eb92fd81769c8d8fc2af37ef948199b35166e618
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-19 22:33:17 +0100

    Multidimensional Arrays

--- Unit_07_2/Ex11_7_2.java
+++ Unit_07_2/Ex11_7_2.java
@@ -1,5 +1,18 @@
+/**
+ * @author David Antúnez Pérez
+ * 
+ *         Realiza un programa que muestre por pantalla un array de 10 filas por
+ *         10
+ *         columnas relleno con números aleatorios entre 200 y 300. A
+ *         continuación, el
+ *         programa debe mostrar los números de la diagonal que va desde la
+ *         esquina
+ *         superior izquierda a la esquina inferior derecha, así como el máximo,
+ *         el mínimo
+ *         y la media de los números que hay en esa diagonal.
+ */
 public class Ex11_7_2 {
   public static void main(String[] args) {
-    
+
   }
 }
