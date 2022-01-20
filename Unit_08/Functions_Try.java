df8044b9d7c0a193bbbd9fd1d01ef6cf12426f70
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-20 10:07:02 +0100

    Functions

---
+++ Unit_08/Functions_Try.java
@@ -0,0 +1,12 @@
+import functions.Ex_1to14_8;
+
+/**
+ * For trying the functions in this Unit
+ */
+public class Functions_Try {
+  public static void main(String[] args) {
+    System.out.println(Ex_1to14_8.esCapicua(2012));
+    System.out.println(Ex_1to14_8.esPrimo(5));
+    System.out.println(Ex_1to14_8.siguientePrimo(10));
+  }
+}
