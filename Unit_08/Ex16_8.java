3bf39e92f4ac56fe122e0fb9a54d7466dbd8fdcf
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-21 14:07:20 +0100

    Functions

---
+++ Unit_08/Ex16_8.java
@@ -0,0 +1,10 @@
+import functions.Functions_1to14_8;
+public class Ex16_8 {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 99999; i++) {
+      if (Functions_1to14_8.esCapicua(i)) {
+        System.out.printf("%10d", i);
+      }
+    }
+  }
+}
