8394321f0ff2aad1a93fd3f86c0ab857e8754c16
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-21 12:24:34 +0100

    Functions

--- Unit_08/functions/Ex_1to14_8.java
+++ Unit_08/functions/Ex_1to14_8.java
@@ -235,9 +235,10 @@ public class Ex_1to14_8 {
   public static int trozoDeNumero(int n, int quitarDerecha, int quitarIzquierda){
     int result = n;
     // Quitar Derecha
-    n /= potencia(10, quitarDerecha);
+    result /= potencia(10, quitarDerecha);
     // Quitar Izquierda
-    return n;
+    
+    return result;
   }
   /**
    * 14.
