6b8e949a5daa25ca597148be65f557990575b7c9
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-21 14:24:02 +0100

    Functions

--- Unit_08/Ex17_8.java
+++ Unit_08/Ex17_8.java
@@ -2,21 +2,19 @@
 import functions.Functions_1to14_8;
 
 public class Ex17_8 {
-  public static int binarioDecimal (int n){
-    int numDig = Functions_1to14_8.digitos(n); // numero de dígitos
-    int cifra = n;
-int cogNum = 0;
-int contDigit = 0;
-    
-for ( int h = 1; h <= n; h *= 10 ) {
-	contDigit++;
-}
-    
-for ( int i = 1; i <= contDigit; i++ ) {
-  int potencia10 = (int)(Math.pow(10,(contDigit-i)));
-  cogNum = cifra / potencia10;
-  System.out.print(cogNum+"\n");
- 
+  public static void main(String[] args) {
+    System.out.println(binarioDecimal(101));
   }
-  return 
+
+  public static int binarioDecimal(int numInt) {
+    int contDigit = Functions_1to14_8.digitos(numInt); // numero de dígitos
+    int cifra = numInt;
+    int cogNum = 0;
+    for (int i = 1; i <= numInt; i *= 10) {
+      cogNum = cifra % 10;
+      System.out.print(cogNum + "\n");
+      cifra = (cifra - cogNum) / 10;
+    }
+    return result;
+  }
 }
