cb7677cd3e6cbd28b915bbd94c26b5f31c2e1073
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-25 12:10:05 +0100

    Functions

--- Unit_08/Ex18_8.java
+++ Unit_08/Ex18_8.java
@@ -2,5 +2,10 @@
  * Escribe un programa que pase de decimal a binario.
  */
 public class Ex18_8 {
-  
+  public static void main(String[] args) {
+    
+  }
+  public static int decimalBinario(int nIntr){
+    
+  }
 }
