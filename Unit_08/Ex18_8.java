69c545e6d35b65eb0fa4b0ed6ea21e2fdd1d3588
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-26 20:17:25 +0100

    Functions

--- Unit_08/Ex18_8.java
+++ Unit_08/Ex18_8.java
@@ -7,6 +7,12 @@ public class Ex18_8 {
   }
   public static int decimalBinario(int nIntr){
     int result = 0;
+    int aux;
+    do{
+      aux= nIntr;
+      aux %= 2;
+      nIntr /= 2;
+    }while(nIntr > 0);
     return result;
   }
 }
