5f6414d4e9cceb885771baf623a22e86f4d08771
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-27 08:32:29 +0100

    Functions

--- Unit_08/Ex19_8.java
+++ Unit_08/Ex19_8.java
@@ -1,5 +1,11 @@
+/**
+ * Une y amplía los dos programas anteriores de tal forma que se permita
+ * convertir un número entre cualquiera de las siguientes bases: decimal,
+ * binario,
+ * hexadecimal y octal.
+ */
 public class Ex19_8 {
   public static void main(String[] args) {
-    
+
   }
 }
