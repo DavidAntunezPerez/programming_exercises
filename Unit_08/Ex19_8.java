913b7ec14b08625b2beab49bd384c38d8b5df85e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-27 10:19:56 +0100

    Functions

--- Unit_08/Ex19_8.java
+++ Unit_08/Ex19_8.java
@@ -1,3 +1,4 @@
+import functions.Functions_1to14_8;
 /**
  * Une y amplía los dos programas anteriores de tal forma que se permita
  * convertir un número entre cualquiera de las siguientes bases: decimal,
@@ -7,5 +8,49 @@
 public class Ex19_8 {
   public static void main(String[] args) {
 
+  }
+
+  /**
+   * De decimal a binario
+   * 
+   * @param nIntr Numero introducido en decimal
+   * @return Numero pasado a binario
+   */
+  public static long decimalBinario(int nIntr) {
+    String binario = "";
+    while (nIntr > 0) {
+      if (nIntr % 2 == 0) {
+        binario = "0" + binario;
+      } else {
+        binario = "1" + binario;
+      }
+      nIntr = nIntr / 2;
+    }
+    long result = Long.parseLong(binario);
+    return result;
+  }
+
+  /**
+   * Pasa un número de binario a decimal
+   * 
+   * @param numInt Número introducido en binario
+   * @return Valor en decimal
+   */
+  public static int binarioDecimal(int numInt) {
+    int result = 0; // resultado final
+    // Separa número del último al primero
+    int cifra = numInt;
+    int cogNum = 0;
+    int elevado = 0;
+    int posicion = 0;
+    for (int i = 1; i <= numInt; i *= 10) {
+      elevado = 0;
+      cogNum = cifra % 10;
+      elevado = Functions_1to14_8.potencia(2, posicion);
+      result += elevado * cogNum;
+      cifra = (cifra - cogNum) / 10;
+      posicion++;
+    }
+    return result; // devuelve el resultado de las operaciones
   }
 }
