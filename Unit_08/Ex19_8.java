1dedb454ae4401333070ceea5bbf0e85eb4dea12
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-28 17:53:59 +0100

    Functions

--- Unit_08/Ex19_8.java
+++ Unit_08/Ex19_8.java
@@ -8,10 +8,71 @@ import functions.Functions_1to14_8;
  */
 public class Ex19_8 {
   public static void main(String[] args) {
-    System.out.println(binarioOctal(101));
-    System.out.println(octalBinario(5));
-    System.out.println(octalDecimal(20));
-    System.out.println(binarioDecimal(11));
+    String resultado = "";
+    long numeroIntroducido = 666;
+    
+    System.out.println(" 1) Binario -> Octal");
+    System.out.println(" 2) Binario -> Decimal");
+    System.out.println(" 3) Binario -> Hexadecimal");
+    System.out.println(" 4) Octal -> Binario");
+    System.out.println(" 5) Octal -> Decimal");
+    System.out.println(" 6) Octal -> Hexadecimal");
+    System.out.println(" 7) Decimal -> Binario");
+    System.out.println(" 8) Decimal -> Octal");
+    System.out.println(" 9) Decimal -> Hexadecimal");
+    System.out.println("10) Hexadecimal -> Binario");
+    System.out.println("11) Hexadecimal -> Octal");
+    System.out.println("12) Hexadecimal -> Decimal");
+    System.out.print("Elija una opción: ");
+    int opcion = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduzca el número que quiere convertir: ");
+    String numeroIntroducidoString = System.console().readLine();
+    
+    if (opcion < 10) {
+      numeroIntroducido = Long.parseLong(numeroIntroducidoString);
+    }
+    
+    switch (opcion) {
+      case 1:
+        resultado = Long.toString(binarioOctal(numeroIntroducido));
+        break;
+      case 2:
+        resultado = Long.toString(binarioDecimal(numeroIntroducido));
+        break;
+      // case 3:
+      //   resultado = binarioHexadecimal(numeroIntroducido);
+      //   break;
+      case 4:
+        resultado = Long.toString(octalBinario(numeroIntroducido));
+        break;
+      case  5:
+        resultado = Long.toString(binarioDecimal(octalBinario(numeroIntroducido)));
+        break;
+      // case  6:
+      //   resultado = binarioHexadecimal(octalBinario(numeroIntroducido));
+      //   break;
+      case  7:
+        resultado = Long.toString(decimalBinario(numeroIntroducido));
+        break;
+      case  8:
+        resultado = Long.toString(decimalOctal(numeroIntroducido));
+        break;
+      // case  9:
+      //   resultado = binarioHexadecimal(decimalBinario(numeroIntroducido));
+      //   break;
+      // case 10:
+      //   resultado = Long.toString(hexadecimalBinario(numeroIntroducidoString));
+      //   break;
+      // case 11:
+      //   resultado = Long.toString(binarioAOctal(hexadecimalBinario(numeroIntroducidoString)));
+      //   break;
+      // case 12:
+      //   resultado = Long.toString(binarioDecimal(hexadecimalBinario(numeroIntroducidoString)));
+      //   break;
+    }
+    
+    System.out.println(resultado);
   }
 
   /**
@@ -149,21 +210,6 @@ public class Ex19_8 {
       cifra = cifra - (cogNum * potencia10);
     }
     result = Long.parseLong(stringResult);
-    return result;
-  }
-
-  /**
-   * Octal a Decimal
-   * 
-   * @param numInt número octal
-   * @return número decimal
-   */
-  public static long octalDecimal(long numIntr) {
-    long result = numIntr;
-    // pasamos primero a binario
-    result = octalBinario(numIntr);
-    // después a decimal
-    result = binarioDecimal(result);
     return result;
   }
 }
