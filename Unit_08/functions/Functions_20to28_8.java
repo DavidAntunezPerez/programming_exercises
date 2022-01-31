271ebbe4e03807be0d540fd483147ac598172ec5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-31 19:49:23 +0100

    Functions

--- Unit_08/functions/Functions_20to28_8.java
+++ Unit_08/functions/Functions_20to28_8.java
@@ -160,4 +160,25 @@ public class Functions_20to28_8 {
     }
     return a;
   }
+
+  /**
+   * Rota a la izquierda un array n veces
+   * 
+   * @param x array introducido
+   * @param n número de rotaciones
+   * @return array rotado
+   */
+  public static int[] rotaIzquierdaArrayInt(int[] x, int n) {
+    int[] a = x.clone(); // clona en a el contenido de x
+    int i;
+    int aux;
+    while (n-- > 0) {
+      aux = a[0];
+      for (i = 0; i < a.length - 1; i++) {
+        a[i] = a[i + 1];
+      }
+      a[a.length - 1] = aux;
+    }
+    return a;
+  }
 }
