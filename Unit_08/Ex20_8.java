728a9e447ab2cffc5d4b36bcd572d225338bb12d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-01-31 11:02:12 +0100

    Functions

--- Unit_08/Ex20_8.java
+++
@@ -1,3 +0,0 @@
-public class Ex20_8 {
-  
-}
---
+++ Unit_08/functions/Functions_20to28_8.java
@@ -0,0 +1,8 @@
+package functions;
+/**
+ * Crea una biblioteca de funciones para arrays (de una dimensión) de números
+enteros que contenga las siguientes funciones:
+ */
+public class Functions_20to28_8 {
+  
+}
