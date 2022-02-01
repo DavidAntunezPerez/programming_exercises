46dcd1a1f1057944b186b7f5ca681ee9a215727c
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-01 20:13:03 +0100

    Functions

--- Unit_08/functions/Functions_29to34_8.java
+++ Unit_08/functions/Functions_29to34_8.java
@@ -5,7 +5,7 @@ public class Functions_29to34_8 {
 
   }
 
-  public static int[][] generaArrayBiInt(int xlength, int ylength, int min, int max) {
+  public static int[][] generaArrayBiInt(int xlength, int ylength, int min, int max) {       
     int[][] x = new int[xlength][ylength];
     for (int i = 0; i < xlength; i++) {
       for (int j = 0; j < ylength; j++) {
