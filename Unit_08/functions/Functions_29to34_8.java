62e242aed4ab17098e25e89550dcb108a8e9796b
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-01 19:51:33 +0100

    Functions

--- Unit_08/functions/Functions_29to34_8.java
+++ Unit_08/functions/Functions_29to34_8.java
@@ -1,5 +1,15 @@
 package functions;
 
 public class Functions_29to34_8 {
-  
+  public static int[][] generaArrayBiInt(int min, int max) {
+    int[][] x = new int[min][max];
+    int filas, columnas;
+    for(filas = 0; filas < x.length; filas++){
+      for(columnas = 0; columnas < x[0].length; columnas++){
+        x[filas][columnas] = (int) (Math.random() * (max + min) + min);
+      }
+      System.out.println();
+    }
+    return x;
+  }
 }
