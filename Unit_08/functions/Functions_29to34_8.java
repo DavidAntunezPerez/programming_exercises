1d12b6e13b863cf4309a1e0e69826c9247d66e89
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-01 20:04:59 +0100

    Functions

--- Unit_08/functions/Functions_29to34_8.java
+++ Unit_08/functions/Functions_29to34_8.java
@@ -2,23 +2,18 @@ package functions;
 
 public class Functions_29to34_8 {
   public static void main(String[] args) {
-    int filas, columnas;
-    int[][]x = generaArrayBiInt(5, 10);
-    for(filas = 0; filas < x.length; filas++){
-      for(columnas = 0; columnas < x[0].length; columnas++){
-        System.out.print(x[filas][columnas]+ " ");
-      }
-      System.out.println();
-    }
+
   }
-  public static int[][] generaArrayBiInt(int min, int max) {
-    int[][] x = new int[min][max];
-    int filas, columnas;
-    for(filas = 0; filas < x.length; filas++){
-      for(columnas = 0; columnas < x[0].length; columnas++){
-        x[filas][columnas] = (int) (Math.random() * (max + min) + min);
+
+  public static int[][] generaArrayBiInt(int xlength, int ylength, int min, int max) {
+    int[][] array = new int[xlength][ylength];
+
+    for (int i = 0; i < xlength; i++) {
+      for (int j = 0; j < ylength; j++) {
+        array[i][j] = (int) (Math.random() * (max - min)) + min;
       }
+
     }
-    return x;
+    return array;
   }
 }
