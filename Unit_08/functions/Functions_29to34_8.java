eb80c9ff93b529e9a463b2d0646ffe7f497d0496
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-01 21:12:58 +0100

    Functions

--- Unit_08/functions/Functions_29to34_8.java
+++ Unit_08/functions/Functions_29to34_8.java
@@ -4,7 +4,14 @@ public class Functions_29to34_8 {
   public static void main(String[] args) {
 
   }
-
+  /**
+   * Genera un array bidimensional 
+   * @param xlength longitud filas
+   * @param ylength longitud columnas
+   * @param min minimo número intervalo
+   * @param max maximo número intervalo
+   * @return array bidimensional
+   */
   public static int[][] generaArrayBiInt(int xlength, int ylength, int min, int max) {
     int[][] x = new int[xlength][ylength];
     for (int i = 0; i < xlength; i++) {
@@ -14,8 +21,17 @@ public class Functions_29to34_8 {
     }
     return x;
   }
-
-  public static int filaDeArrayBiInt(int[][] x) {
-    
+  /**
+   * Devuelve una fila indicada en un array bidimensional
+   * @param x array bidimensional
+   * @param fila fila indicada
+   * @return array de una dimensión de la fila
+   */
+  public static int[] filaDeArrayBiInt(int[][] x, int fila) {
+    int [] f = new int [x[0].length];
+    for (int i = 0; i < x[0].length;i++){
+      f[i] = x[fila][i];
+    }
+    return f;
   }
 }
