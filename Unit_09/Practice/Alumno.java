63f7db289b355525e32c92963be4ee59d72fb26e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-03 09:26:50 +0100

    OOP

--- Unit_09/Practice/Alumno.java
+++ Unit_09/Practice/Alumno.java
@@ -1,4 +1,11 @@
+import java.util.Date;
+
 public class Alumno {
   int numeroExpediente;
-  
+  String nombre;
+  String apellidos;
+  String telefono;
+  String estudiosPrevios = "Bachillerato";
+  Date fechaNacimiento;
+
 }
