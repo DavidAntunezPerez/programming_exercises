c0862f58ab48a8c7414176effc517c00cbc7ccec
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-03 10:14:57 +0100

    OOP

--- Unit_09/Practice/Alumno.java
+++ Unit_09/Practice/Alumno.java
@@ -8,10 +8,11 @@ public class Alumno {
   String estudiosPrevios = "Bachillerato";
   Date fechaNacimiento;
   boolean esRepetidor;
-  
+
   public Alumno(int numeroExpediente, String nombre, String apellidos, String telefono, String estudiosPrevios,
       Date fechaNacimiento, boolean esRepetidor) {
-    this.numeroExpediente = numeroExpediente;
+        // this significa el objeto al que hace referencia el método
+    this.numeroExpediente = numeroExpediente; // lo de a la derecha del punt es el atributo
     this.nombre = nombre;
     this.apellidos = apellidos;
     this.telefono = telefono;
