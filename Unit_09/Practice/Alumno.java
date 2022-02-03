bba5288d73772fb3f954ffe1a24cfb79b01dabb8
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-03 10:09:01 +0100

    OOP

--- Unit_09/Practice/Alumno.java
+++ Unit_09/Practice/Alumno.java
@@ -8,4 +8,17 @@ public class Alumno {
   String estudiosPrevios = "Bachillerato";
   Date fechaNacimiento;
   boolean esRepetidor;
+  
+  public Alumno(int numeroExpediente, String nombre, String apellidos, String telefono, String estudiosPrevios,
+      Date fechaNacimiento, boolean esRepetidor) {
+    this.numeroExpediente = numeroExpediente;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.telefono = telefono;
+    this.estudiosPrevios = estudiosPrevios;
+    this.fechaNacimiento = fechaNacimiento;
+    this.esRepetidor = esRepetidor;
+  }
+
+  
 }
