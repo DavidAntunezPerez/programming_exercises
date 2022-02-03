1d0b02d9efb41ae159c015cb7bd7d5d5655659e3
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-03 22:32:47 +0100

    Functions

--- Unit_08/Ex35_8.java
+++ Unit_08/Ex35_8.java
@@ -1,3 +1,21 @@
 public class Ex35_8 {
-  
+  public static int decimalBinario(int nIntr) {
+    String binario = "";
+    while (nIntr > 0) {
+      if (nIntr % 2 == 0) {
+        binario = "0" + binario;
+      } else {
+        binario = "1" + binario;
+      }
+      nIntr = nIntr / 2;
+    }
+    int result = Integer.parseInt(binario);
+    return result;
+  }
+
+  public static String convierteEnPalotes(int n) {
+    int Binario = decimalBinario(n);
+    String palotes = "";
+    
+  }
 }
