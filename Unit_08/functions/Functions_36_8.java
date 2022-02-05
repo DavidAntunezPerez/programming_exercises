3ff1e2417be78cbe87c40cfb1a2e31db68efa6c9
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-05 01:09:17 +0100

    Functions

--- Unit_08/functions/Functions_36_8.java
+++ Unit_08/functions/Functions_36_8.java
@@ -2,14 +2,14 @@ package functions;
 
 public class Functions_36_8 {
   public static void main(String[] args) {
-    int[] numeros = Functions_20to28_8.generaArrayInt(20, 1, 100);
+    int[] numeros = Functions_20to28_8.generaArrayInt(1, 20);
 
     System.out.println("Array original: ");
     Functions_20to28_8.muestraArrayInt(numeros);
     System.out.println("Primos: ");
     Functions_20to28_8.muestraArrayInt(filtraPrimos(numeros));
   }
-  }
+
   public static int[] filtraPrimos(int x[]) {
     int[] primos = new int[x.length];
 
