0c927af781b924d8a7e7ee0b108c63aa6634c5df
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-07 08:44:27 +0100

    OOP

--- Unit_09/Ex2_9.java
+++ Unit_09/Ex2_9.java
@@ -1,3 +1,3 @@
 /**
- * Clases - Animal [Subclase Perro(), Caballo(), Gato()], Mineral[] , Persona[]
+ * Clases - Animal [Subclase Perro(Atributos(Laika, Snoopy, Pluto, Goofy, Ayudante_de_santa_claus)), Caballo(Atributo(Bucefalo, Pegaso, Rocinante)), Gato(Atributos(Gardfiel, Silvestre, Milu))], Mineral[Atributos(Pirita, Cuarzo)] , Persona[Atributos(Paula, Javier, Tom) Atributos(Pato_Lucas)]
  */
