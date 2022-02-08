13547a6f1ebdf535a6bf90fd218ddb1500eaec73
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-08 13:13:25 +0100

    OOP

--- Unit_09/Ex5_9/Pizza.java
+++ Unit_09/Ex5_9/Pizza.java
@@ -4,48 +4,70 @@ public class Pizza {
   private static String estado = "pedida";
   private static int TotalPedidas = 0;
   private static int TotalServidas = 0;
+
   public static int getTotalPedidas() {
     return TotalPedidas;
   }
+
   public void setTotalPedidas(int totalPedidas) {
     TotalPedidas = totalPedidas;
   }
+
   public static int getTotalServidas() {
     return TotalServidas;
   }
+
   public void setTotalServidas(int totalServidas) {
     TotalServidas = totalServidas;
   }
+
   public String getTamaño() {
     return tamaño;
   }
+
   public void setTamaño(String tamaño) {
     this.tamaño = tamaño;
   }
+
   public String getTipo() {
     return tipo;
   }
+
   public void setTipo(String tipo) {
     this.tipo = tipo;
   }
+
   public String getEstado() {
     return estado;
   }
+
   public void setEstado(String estado) {
     this.estado = estado;
   }
+
   public Pizza(String tamaño, String tipo, String estado) {
     this.tamaño = tamaño;
     this.tipo = tipo;
     this.estado = estado;
     TotalPedidas++;
-    if(estado == "servida"){
+    if (estado == "servida") {
       TotalServidas++;
     }
   }
+
   public Pizza(String tamaño, String tipo) {
     this.tamaño = tamaño;
     this.tipo = tipo;
     TotalPedidas++;
+  }
+
+  public void sirve() {
+    if (this.estado == "servida") {
+      System.out.println("La pizza ya ha sido servida antes.");
+    } else {
+      System.out.println("La pizza se ha servido.");
+      this.estado = "servida";
+      TotalServidas++;
+    }
   }
 }
