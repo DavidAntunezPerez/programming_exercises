fe89b739dca9b3c00d171e4a2c1ad5b6853b98fb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-08 08:45:20 +0100

    OOP

---
+++ Unit_09/Caballo.java
@@ -0,0 +1,33 @@
+/**
+ * Implementa la clase Caballo vista en un ejercicio anterior. Pruébala creando
+ * instancias y aplicándole algunos métodos
+ */
+public class Caballo {
+  private String nombre;
+  private String raza;
+  private String fechNac;
+
+  public void Review() {
+    System.out
+        .println("Hola, soy el caballo " + this.nombre + ", soy de raza" + this.raza + " y nací el " + this.fechNac);
+  }
+
+  public void darCarrera() {
+    // El caballo camina una distancia aleatoria
+    System.out.println("El caballo ha caminado " + (int) (Math.random() * 1000 + 1) + "m");
+  }
+  
+  public Caballo(String nombre, String raza, String fechNac) {
+    this.nombre = nombre;
+    this.raza = raza;
+    this.fechNac = fechNac;
+  }
+  public Caballo(){
+
+  }
+  public static void main(String[] args) {
+    Caballo Juan = new Caballo();
+    Juan.darCarrera();
+    Caballo Pepe = new Caballo("Pepito", "puro", "2018");
+  }
+}
