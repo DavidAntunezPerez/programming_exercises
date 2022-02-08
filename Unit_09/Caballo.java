f09c2e355bce7b4d7368e0695181231a79f81947
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-08 08:46:20 +0100

    OOP

--- Unit_09/Caballo.java
+++ Unit_09/Caballo.java
@@ -7,27 +7,30 @@ public class Caballo {
   private String raza;
   private String fechNac;
 
-  public void Review() {
+  public void review() {
     System.out
-        .println("Hola, soy el caballo " + this.nombre + ", soy de raza" + this.raza + " y nací el " + this.fechNac);
+        .println("Hola, soy el caballo " + this.nombre + ", soy de raza " + this.raza + " y nací el " + this.fechNac);
   }
 
   public void darCarrera() {
     // El caballo camina una distancia aleatoria
     System.out.println("El caballo ha caminado " + (int) (Math.random() * 1000 + 1) + "m");
   }
-  
+
   public Caballo(String nombre, String raza, String fechNac) {
     this.nombre = nombre;
     this.raza = raza;
     this.fechNac = fechNac;
   }
-  public Caballo(){
+
+  public Caballo() {
 
   }
+
   public static void main(String[] args) {
     Caballo Juan = new Caballo();
     Juan.darCarrera();
     Caballo Pepe = new Caballo("Pepito", "puro", "2018");
+    Pepe.review();
   }
 }
