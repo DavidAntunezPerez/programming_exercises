88c70387658a17ed44046764f1655d825a706631
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-08 13:05:58 +0100

    OOP

--- Unit_09/Ex5_9/Pizza.java
+++ Unit_09/Ex5_9/Pizza.java
@@ -1,6 +1,32 @@
 public class Pizza {
-  String tamaño;
-  String tipo;
-  String estado = "pedida";
-  
+  private String tamaño;
+  private String tipo;
+  private String estado = "pedida";
+  public String getTamaño() {
+    return tamaño;
+  }
+  public void setTamaño(String tamaño) {
+    this.tamaño = tamaño;
+  }
+  public String getTipo() {
+    return tipo;
+  }
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+  public String getEstado() {
+    return estado;
+  }
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+  public Pizza(String tamaño, String tipo, String estado) {
+    this.tamaño = tamaño;
+    this.tipo = tipo;
+    this.estado = estado;
+  }
+  public Pizza(String tamaño, String tipo) {
+    this.tamaño = tamaño;
+    this.tipo = tipo;
+  }
 }
