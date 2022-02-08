62fcc6fc36a51418d88c9a18d90f1082f33ddb5e
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-08 14:04:06 +0100

    Functions

---
+++ Unit_08/Ex40_8.java
@@ -0,0 +1,35 @@
+public class Ex40_8 {
+  public static int[] filtraCon7(int x[]) {
+    // Devuelve el array con todos los numeros que contengan 7
+    int[] y = new int[x.length];
+    int cont = 0;
+    for (int i = 0; i < x.length; i++) {
+      // Separa número del último al primero
+      int cifra = x[i];
+      int cogNum = 0;
+      for (int j = 1; j <= x[i]; j *= 10) {
+        cogNum = cifra % 10;
+        if(cogNum == 7){
+          x[i] = y[i];
+          cont++;
+        }
+        cifra = (cifra - cogNum) / 10;
+      }
+    }
+    int[] result = new int[cont];
+    cont = 0;
+    for (int i = 0; i < result.length; i++) {
+      result[cont] = y[i];
+      cont++;
+    }
+    return result;
+  }
+  public static void main(String[] args) {
+    int[] x = functions.Functions_20to28_8.generaArrayInt(0, 100);
+    System.out.println("ARRAY ORIGINAL:");
+    functions.Functions_20to28_8.muestraArrayInt(x);
+    System.out.println("ARRAY CON 7:");
+    functions.Functions_20to28_8.muestraArrayInt(filtraCon7(x));
+
+  }
+}
