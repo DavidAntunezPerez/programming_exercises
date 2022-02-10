39e6a7369f87efbc1b50b8c3bb5be4cc156ff64a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-10 11:17:26 +0100

    OOP

--- Unit_09/Ex7_9/Entrada.java
+++ Unit_09/Ex7_9/Entrada.java
@@ -7,8 +7,10 @@ public class Entrada {
     Zona vip = new Zona(25);
     boolean salir = false;
     do {
+      salir = false;
       System.out.println("1. Mostrar número de entradas libres \n2. Vender entradas \n3. Salir");
       int option = Integer.parseInt(System.console().readLine());
+      System.out.println();
       switch (option) {
         case 1:
           System.out.println("Sala principal: " + principal.getEntradasPorVender());
@@ -16,6 +18,22 @@ public class Entrada {
           System.out.println("Sala VIP: " + vip.getEntradasPorVender());
           break;
         case 2:
+          System.out.print("¿Cuántas entradas quieres comprar?");
+          int n = Integer.parseInt(System.console().readLine());
+          System.out.println("¿De que zona quieres comprar?");
+          System.out.println("1. Principal\n2. Compra-Venta\n3. VIP");
+          int zona = Integer.parseInt(System.console().readLine());
+          switch (zona) {
+            case 1:
+              principal.vender(n);
+              break;
+            case 2:
+              compraventa.vender(n);
+              break;
+            case 3:
+              vip.vender(n);
+              break;
+          }
           break;
         case 3:
           System.out.println("Nos vemos!");
