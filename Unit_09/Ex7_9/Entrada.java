0518d45860fe9b89dd6ea823b8e52706a1286c52
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-10 10:50:07 +0100

    OOP

---
+++ Unit_09/Ex7_9/Entrada.java
@@ -0,0 +1,16 @@
+package Ex7_9;
+
+public class Entrada {
+  public static void main(String[] args) {
+    System.out.println("1. Mostrar número de entradas libres \n2. Vender entradas \n3. Salir");
+    int option = Integer.parseInt(System.console().readLine());
+    switch (option) {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+}
