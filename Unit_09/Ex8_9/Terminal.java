31d2499b0f20612affaa2a979b720d857403ae1f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-11 12:34:16 +0100

    OOP

--- Unit_09/Ex8_9/Terminal.java
+++ Unit_09/Ex8_9/Terminal.java
@@ -1,8 +1,23 @@
 public class Terminal {
   private String id;
+  private int tiempo;
 
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public int getTiempo() {
+    return tiempo;
+  }
+  public void setTiempo(int tiempo) {
+    this.tiempo = tiempo;
+  }
   public Terminal(String id) {
     this.id = id;
   }
-  
+  public void llama(){
+
+  }
 }
