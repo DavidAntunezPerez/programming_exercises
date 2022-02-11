96a680d338167db9f1bae4da5ce6447126515866
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-11 12:45:39 +0100

    OOP

--- Unit_09/Ex8_9/Terminal.java
+++ Unit_09/Ex8_9/Terminal.java
@@ -1,24 +1,29 @@
 public class Terminal {
   private String id;
-  private int tiempo;
+  private static int tiempo;
 
   public String getId() {
     return id;
   }
+
   public void setId(String id) {
     this.id = id;
   }
-  public int getTiempo() {
+
+  public static int getTiempo() {
     return tiempo;
   }
-  public void setTiempo(int tiempo) {
-    this.tiempo = tiempo;
+
+  public static void setTiempo(int tiempo) {
+    Terminal.tiempo = tiempo;
   }
+
   public Terminal(String id) {
     this.id = id;
   }
-  public void llama(Terminal t , int time){
-    System.out.println("Nº "+t.getId()+ " - "+(getTiempo()+time)+"s de conversación.");
+
+  public void llama(Terminal t, int time) {
+    System.out.println("Nº " + t.getId() + " - " + (getTiempo() + time) + "s de conversación.");
     Terminal.tiempo += time;
   }
 }
