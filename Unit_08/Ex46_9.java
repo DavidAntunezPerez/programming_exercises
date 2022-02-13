29529163effeb61d2f10324ecbf3412acd8050f6
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-13 19:42:36 +0100

    Functions

--- Unit_08/Ex46_9.java
+++ Unit_08/Ex46_9.java
@@ -1,23 +1,35 @@
 public class Ex46_9 {
   public static void main(String[] args) {
-    System.out.print("Introduce la altura:");
+    System.out.print("Introduzca la altura de la figura: ");
     int altura = Integer.parseInt(System.console().readLine());
+
     for (int i = 1; i < altura; i++) {
-      System.out.println();
-      System.out.print(linea('*',"*", i));
-      System.out.print(linea(' '," ",altura-i));
-      System.out.print(linea(' '," ",altura-i));
-      System.out.print(linea('*',"*", i));
+      System.out.print(vertices('*', i));
+      System.out.print(linea(' ', altura * 2 - i * 2 - 1));
+      System.out.println(vertices('*', i));
     }
+
+    System.out.println(linea('*', altura * 2 - 1));
+
   }
 
-  public static String linea(char lados, String caracter, int repeticiones) {
-    String result = "";
-    result += String.valueOf(lados);
-    for (int i = 0; i < repeticiones - 2; i++) {
-      result += String.valueOf(caracter);
+  public static String linea(char caracter, int longitud) {
+
+    String resultado = "";
+
+    for (int i = 0; i < longitud; i++) {
+      resultado += caracter;
     }
-    result += String.valueOf(lados);
-    return result;
+
+    return resultado;
+  }
+
+  public static String vertices(char caracter, int longitud) {
+
+    if (longitud == 1) {
+      return "*";
+    }
+
+    return "*" + linea(' ', longitud - 2) + "*";
   }
 }
