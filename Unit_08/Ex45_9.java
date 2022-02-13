9d3c3e2c07bfbe6fb18c01bd9d2417a205fe0741
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-13 19:30:59 +0100

    Functions

---
+++ Unit_08/Ex45_9.java
@@ -0,0 +1,24 @@
+public class Ex45_9 {
+  public static void main(String[] args) {
+    System.out.print("Introduce la altura:");
+    int altura = Integer.parseInt(System.console().readLine());
+    for (int i = 1; i < altura; i++) {
+      System.out.println();
+      System.out.print(linea('*', i));
+      System.out.print(linea(' ',altura-i));
+      System.out.print(linea(' ',altura-i));
+      System.out.print(linea('*', i));
+    }
+  }
+
+  public static String linea(char caracter, int repeticiones) {
+
+    String resultado = "";
+
+    for (int i = 0; i < repeticiones; i++) {
+      resultado += caracter;
+    }
+
+    return resultado;
+  }
+}
