6b0d7e0bd93a20a3ed1f7c8701bdfccd53eb9b33
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-13 20:21:20 +0100

    Functions

--- Unit_08/Ex48_8.java
+++ Unit_08/Ex48_8.java
@@ -1,3 +1,25 @@
+import java.util.Arrays;
+
 public class Ex48_8 {
-  
+  public static void main(String[] args) {
+    int[] x = {1, 2, 3, 4, 5, 6, 7};
+    int[] y = {1, 2, 3, 4, 5, 6, 7};
+    System.out.println(
+    "Integer Array: "+ Arrays.toString(concatena(x, y)));
+  }
+
+  public static int[] concatena(int[] a, int[] b) {
+    int[] result = new int[a.length + b.length];
+    int cont = 0;
+    for (int i = 0; i < result.length; i++) {
+      if (i < a.length) {
+        result[i] = a[i];
+      } else if((i >= a.length)) {
+        result[i] = b[cont];
+        cont++;
+      }
+    }
+    return result;
+  }
+
 }
