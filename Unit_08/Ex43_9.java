6877462c63d07734d8906d89f4a8bc9113d77a0a
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-13 18:53:23 +0100

    Functions

--- Unit_08/Ex43_9.java
+++ Unit_08/Ex43_9.java
@@ -1,5 +1,21 @@
 public class Ex43_9 {
   public static void main(String[] args) {
-    
+    System.out.print("Introduce la altura de la pirámide:");
+    int altura = Integer.parseInt(System.console().readLine());
+    for (int i = 0; i < altura; i++) {
+      System.out.print(linea(' ', i));
+      System.out.println(linea('*', altura - i));
+    }
+  }
+
+  public static String linea(char caracter, int repeticiones) {
+
+    String result = "";
+
+    for (int i = 0; i < repeticiones; i++) {
+      result += caracter;
+    }
+
+    return result;
   }
 }
