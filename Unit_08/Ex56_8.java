faf1f85e5d92b78950215a6a01b38fee9226d41d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-14 10:13:46 +0100

    Functions

--- Unit_08/Ex56_8.java
+++ Unit_08/Ex56_8.java
@@ -1,10 +1,18 @@
 public class Ex56_8 {
   public static void main(String[] args) {
     int[][] x = generaArrayBiInt(10, 10, 100, 100);
+    int fila, columna;
+    for (fila = 0; fila < x.length; fila++) {
+      System.out.print("Fila: " + fila);
+      for (columna = 0; columna < x[0].length; columna++) {
+        System.out.printf("%10d ", x[fila][columna]);
+      }
+      System.out.println();
+    }
   }
 
   public static int[] corteza(int[][] n) {
-
+    
   }
 
   // GENERA ARRAY BIDIMENSIONAL
