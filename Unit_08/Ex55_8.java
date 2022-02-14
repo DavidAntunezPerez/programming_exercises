f18e9d8ba1ce5bd087fa20db2fdd16af52f2f348
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-14 10:00:01 +0100

    Functions

--- Unit_08/Ex55_8.java
+++ Unit_08/Ex55_8.java
@@ -2,16 +2,31 @@ public class Ex55_8 {
   public static void main(String[] args) {
     String[] a = { "casa", "coche", "sol", "mesa", "mesa", "coche", "ordenador", "sol",
         "CASA" };
-    for (int i = 0; i < a.length; i++) {
+    for (int i = 0; i < sinRepetir(a).length; i++) {
       System.out.print(sinRepetir(a)[i] + " ");
     }
+
   }
 
   public static String[] sinRepetir(String[] s) {
-    String[] result = new String[s.length];
-    
-    
-    return result;
+    String[] arraySinRepeticionesAux = new String[s.length];
+    int k = 0;
+    for (int i = 0; i < s.length; i++) {
+      boolean repetido = false;
+      for (int j = 0; ((j < s.length) && (!repetido)); j++) {
+        if (s[i].equals(arraySinRepeticionesAux[j])) {
+          repetido = true;
+        }
+      }
+      if (!repetido) {
+        arraySinRepeticionesAux[k] = s[i];
+        k++;
+      }
+    }
+    String[] arraySinRepeticiones = new String[k];
+    for (int i = 0; i < k; i++) {
+      arraySinRepeticiones[i] = arraySinRepeticionesAux[i];
+    }
+    return arraySinRepeticiones;
   }
-
 }
