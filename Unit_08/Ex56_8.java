daddf879edadfb1611fecfbd9af05793273546b2
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-14 10:53:51 +0100

    Functions

--- Unit_08/Ex56_8.java
+++ Unit_08/Ex56_8.java
@@ -21,12 +21,11 @@ public class Ex56_8 {
     int cont = 0;
     for (fila = 0; fila < n.length; fila++) {
       for (columna = 0; columna < n[0].length; columna++) {
-        if ((fila == 0) || (columna == 0) || (fila == n.length )||(columna == n[0].length)) {
+        if ((fila == 0) || (columna == 0) || (fila == n.length) || (columna == n[0].length)) {
           result[cont] = n[fila][columna];
           cont++;
         }
       }
-      System.out.println();
     }
     return result;
   }
