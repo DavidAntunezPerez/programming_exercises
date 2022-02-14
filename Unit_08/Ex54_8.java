f2a3138e94e9b4146f8dede72f40224f26a21483
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-14 09:00:23 +0100

    Functions

---
+++ Unit_08/Ex54_8.java
@@ -0,0 +1,13 @@
+public class Ex54_8 {
+  public static void main(String[] args) {
+    
+  }
+  public static int ocurrencias(int digito, int n) {
+
+  }
+
+  public static int ocurrencias(int digito, int[] a) {
+
+  }
+
+}
