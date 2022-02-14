16feef778825ddd9380fd73fbc448e634997a57d
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-14 09:09:15 +0100

    Functions

--- Unit_08/Ex54_8.java
+++ Unit_08/Ex54_8.java
@@ -1,6 +1,8 @@
 public class Ex54_8 {
   public static void main(String[] args) {
-
+    int[] a = { 714, 81, 9, 11 };
+    System.out.println(ocurrencias(5, 25153));
+    System.out.println(ocurrencias(6, a));
   }
 
   public static int ocurrencias(int digito, int n) {
@@ -19,6 +21,20 @@ public class Ex54_8 {
   }
 
   public static int ocurrencias(int digito, int[] a) {
-
+    int repeticiones = 0;
+    for (int j = 0; j < a.length; j++) {
+      // Separa número del último al primero
+      int numInt = a[j];
+      int cifra = numInt;
+      int cogNum = 0;
+      for (int i = 1; i <= numInt; i *= 10) {
+        cogNum = cifra % 10;
+        if (cogNum == digito) {
+          repeticiones++;
+        }
+        cifra = (cifra - cogNum) / 10;
+      }
+    }
+    return repeticiones;
   }
 }
