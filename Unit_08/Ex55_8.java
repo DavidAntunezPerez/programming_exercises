280da551a1678ad4e05a0ebee9ca019c4f02d4eb
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-14 09:37:25 +0100

    Functions

---
+++ Unit_08/Ex55_8.java
@@ -0,0 +1,17 @@
+public class Ex55_8 {
+  public static void main(String[] args) {
+    String[] a = { "casa", "coche", "sol", "mesa", "mesa", "coche", "ordenador", "sol",
+        "CASA" };
+    for (int i = 0; i < a.length; i++) {
+      System.out.print(sinRepetir(a)[i] + " ");
+    }
+  }
+
+  public static String[] sinRepetir(String[] s) {
+    String[] result = new String[s.length];
+    
+    
+    return result;
+  }
+
+}
