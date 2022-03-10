65047d87cba0c4e0d5e1dac54a3ca28d281735de
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-03-10 10:10:59 +0100

    Collections and Dictionaries

---
+++ Unit_10/Ex3_10.java
@@ -0,0 +1,25 @@
+
+/**
+ * @author David Antunez Perez
+ * Escribe un programa que ordene 10 números enteros introducidos por teclado
+ * y almacenados en un objeto de la clase ArrayList
+ */
+
+import java.util.ArrayList;
+
+public class Ex3_10 {
+  public static void main(String[] args) {
+    ArrayList<Integer> a = new ArrayList<Integer>(10);
+    System.out.print("Introduce un número: ");
+    int n = Integer.parseInt(System.console().readLine());
+    a.add(n);
+    for (int i = 0; i < 9; i++) {
+      System.out.print("Introduce otro número: ");
+      n = Integer.parseInt(System.console().readLine());
+      a.add(n);
+    }
+    for (int num : a) {
+      System.out.print(num + " ");
+    }
+  }
+}
