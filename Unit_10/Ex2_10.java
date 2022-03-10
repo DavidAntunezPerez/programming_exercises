5c1280bcaee04e1488729a987ad0ea42856fd024
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-03-10 10:01:06 +0100

    Collections and Dictionaries

--- Unit_10/Ex2_10.java
+++ Unit_10/Ex2_10.java
@@ -12,12 +12,36 @@ public class Ex2_10 {
   public static void main(String[] args) {
     ArrayList<Integer> a = new ArrayList<Integer>(10);
     int random = 0;
-    for (int i = 0; i < a.size(); i++) {
+    int max = 0;
+    int min = 100;
+    int sum = 0;
+    int rNum = (int) (Math.random() * 2);
+    int rSize = 0;
+    switch (rNum) {
+      case 0:
+        rSize = 10;
+        break;
+      case 1:
+        rSize = 20;
+        break;
+    }
+    for (int i = 0; i < rSize; i++) {
       random = (int) (Math.random() * 101);
       a.add(random);
+      sum += random;
+      if (min > random) {
+        min = random;
+      }
+      if (max < random) {
+        max = random;
+      }
       System.out.print(a.get(i) + " ");
     }
+    System.out.println();
     System.out.println("Nº de elementos: " + a.size());
+    System.out.println("Nº mayor: " + max);
+    System.out.println("Nº menor: " + min);
+    System.out.println("Media: " + (sum / rSize));
 
   }
 }
