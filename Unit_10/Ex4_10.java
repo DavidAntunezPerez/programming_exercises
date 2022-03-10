e08732dccae9209029cfe99e45cac55759fa8efc
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-03-10 10:18:22 +0100

    Collections and Dictionaries

--- Unit_10/Ex4_10.java
+++ Unit_10/Ex4_10.java
@@ -5,9 +5,24 @@
  * debe ordenar palabras en lugar de números.
  */
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ex4_10 {
   public static void main(String[] args) {
+    ArrayList<String> a = new ArrayList<String>(10);
+    System.out.print("Introduce una letra: ");
+    String n = System.console().readLine();
+    a.add(n);
+    for (int i = 0; i < 9; i++) {
+      System.out.print("Introduce otra letra: ");
+      n = System.console().readLine();
+      a.add(n);
+    }
+    System.out.println("Array Ordenado de menor a mayor:");
+    Collections.sort(a);
+    for (String num : a) {
+      System.out.print(num + " ");
+    }
 
   }
 }
